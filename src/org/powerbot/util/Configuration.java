1e12a529760cdbbcf2e5f880c721c1d376143cd1
A. Ahmed <paris@powerbot.org>
2012-02-26 21:38:43 +0000

    Added resources.

--- src/org/powerbot/util/Configuration.java
+++ src/org/powerbot/util/Configuration.java
@@ -1,8 +1,14 @@
 package org.powerbot.util;
 
+import java.net.MalformedURLException;
+
+import org.powerbot.util.io.IOHelper;
+import org.powerbot.util.io.Resources;
+
 public class Configuration {
 	public static final String NAME = "RSBot";
 	private static final int VERSION = 4000;
+	public static final boolean FROMJAR;
 
 	public interface Paths {
 		public interface URLs {
@@ -12,6 +18,10 @@ public class Configuration {
 			public static final String GAME = "runescape.com";
 			public static final String CLIENT_PATCH = BASE + "/modscript/";
 		}
+	}
+
+	static {
+		FROMJAR = Configuration.class.getClassLoader().getResource(Resources.Paths.VERSION) != null;
 	}
 
 	public static int getVersion() {
