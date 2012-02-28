7ef6024b5796a5acf87929903ab45ad7ee96553e
A. Ahmed <paris@powerbot.org>
2012-02-28 00:14:29 +0000

    Added crude implementation of read/write operations.

--- src/org/powerbot/util/io/SecureStore.java
+++ src/org/powerbot/util/io/SecureStore.java
@@ -1,12 +1,15 @@
 package org.powerbot.util.io;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.RandomAccessFile;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
 import java.util.logging.Logger;
+
 import org.powerbot.util.Configuration;
 import org.powerbot.util.StringUtil;
 
@@ -18,6 +21,7 @@ public final class SecureStore {
 	private final static SecureStore instance = new SecureStore();
 	private final static int MAGIC = 0x00525354, VERSION = 1, BLOCKSIZE = 512, MAXBLOCKS = 2048;
 	private final File store;
+	private long offset;
 	private byte[] key;
 
 	private SecureStore() {
@@ -81,7 +85,66 @@ public final class SecureStore {
 			md.update(payload);
 			raf.skipBytes(payload.length);
 		}
+		offset = raf.getFilePointer();
 		raf.close();
 		key = md.digest();
+	}
+
+	public InputStream read(final String name) throws IOException {
+		final RandomAccessFile raf = new RandomAccessFile(store, "r");
+		raf.seek(offset);
+		final byte[] header = new byte[TarEntry.BLOCKSIZE];
+		while (raf.read(header) != -1) {
+			final TarEntry entry = TarEntry.read(header);
+			int l = (int) Math.ceil((double) entry.length / TarEntry.BLOCKSIZE) * TarEntry.BLOCKSIZE;
+			if (name.equals(entry.name)) {
+				final byte[] data = new byte[(int) entry.length];
+				raf.read(data);
+				raf.close();
+				return new ByteArrayInputStream(data);
+			} else {
+				raf.skipBytes(l);
+			}
+		}
+		raf.close();
+		return null;
+	}
+
+	public void write(final String name, final InputStream is) throws IOException {
+		final RandomAccessFile raf = new RandomAccessFile(store, "rw");
+		raf.seek(offset);
+		final byte[] header = new byte[TarEntry.BLOCKSIZE];
+		while (raf.read(header) != -1) {
+			if (header[0] == 0) {
+				continue;
+			}
+			final TarEntry entry = TarEntry.read(header);
+			int l = (int) Math.ceil((double) entry.length / TarEntry.BLOCKSIZE) * TarEntry.BLOCKSIZE;
+			if (name.equals(entry.name)) {
+				// TODO: delete (replace) existing file
+			} else {
+				raf.skipBytes(l);
+			}
+		}
+		if (is != null) {
+			final byte[] empty = new byte[TarEntry.BLOCKSIZE];
+			final long z = raf.getFilePointer();
+			raf.write(empty);
+			int l = 0, b;
+			while ((b = is.read()) != -1) {
+				raf.write(b);
+				l++;
+			}
+			final int p = l < TarEntry.BLOCKSIZE ? TarEntry.BLOCKSIZE - l : l % TarEntry.BLOCKSIZE;
+			for (int i = 0; i < p; i++) {
+				raf.write(0);
+			}
+			raf.seek(z);
+			final TarEntry entry = new TarEntry();
+			entry.name = name;
+			entry.length = l;
+			raf.write(entry.getBytes());
+		}
+		raf.close();
 	}
 }
