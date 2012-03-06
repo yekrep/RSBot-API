d0d79107f252c4018f8aa9679516f805817722b0
Joe Haddad <timer@powerbot.org>
2012-03-05 21:20:48 -0500

    Created PaintEvent and PaintListener.

---
+++ src/org/powerbot/game/event/PaintEvent.java
@@ -0,0 +1,61 @@
+package org.powerbot.game.event;
+
+import java.awt.Color;
+import java.awt.Composite;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Paint;
+import java.awt.RenderingHints;
+import java.awt.Shape;
+import java.awt.Stroke;
+import java.awt.geom.AffineTransform;
+import java.util.EventListener;
+
+import org.powerbot.event.EventDispatcher;
+import org.powerbot.event.GameEvent;
+import org.powerbot.game.event.listener.PaintListener;
+
+public class PaintEvent extends GameEvent {
+	private static final long serialVersionUID = 1L;
+	public Graphics graphics;
+
+	public PaintEvent() {
+		setType(EventDispatcher.PAINT_EVENT);
+		this.graphics = null;
+	}
+
+	@Override
+	public void dispatch(final EventListener eventListener) {
+		if (graphics == null) {
+			try {
+				((PaintListener) eventListener).onRepaint(null);
+			} catch (NullPointerException ignored) {
+			}
+			return;
+		}
+		final Graphics2D graphics2D = (Graphics2D) graphics;
+
+		final Color s_background = graphics2D.getBackground();
+		final Shape s_clip = graphics2D.getClip();
+		final Color s_color = graphics2D.getColor();
+		final Composite s_composite = graphics2D.getComposite();
+		final Font s_font = graphics2D.getFont();
+		final Paint s_paint = graphics2D.getPaint();
+		final RenderingHints s_renderingHints = graphics2D.getRenderingHints();
+		final Stroke s_stroke = graphics2D.getStroke();
+		final AffineTransform s_transform = graphics2D.getTransform();
+
+		((PaintListener) eventListener).onRepaint(graphics);
+
+		graphics2D.setBackground(s_background);
+		graphics2D.setClip(s_clip);
+		graphics2D.setColor(s_color);
+		graphics2D.setComposite(s_composite);
+		graphics2D.setFont(s_font);
+		graphics2D.setPaint(s_paint);
+		graphics2D.setRenderingHints(s_renderingHints);
+		graphics2D.setStroke(s_stroke);
+		graphics2D.setTransform(s_transform);
+	}
+}
