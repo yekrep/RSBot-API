978d73dc2819125d840428b6e10a9cc3c05a0fe5
Joe Haddad <timer@powerbot.org>
2012-03-08 19:10:11 -0500

    Basic Skills class, levels and experience.

---
+++ src/org/powerbot/game/api/methods/Skills.java
@@ -0,0 +1,26 @@
+package org.powerbot.game.api.methods;
+
+import org.powerbot.game.bot.Bot;
+import org.powerbot.game.client.Client;
+
+public class Skills {
+	public static int[] getLevels() {
+		final Client client = Bot.resolve().client;
+		return client.getSkillLevels();
+	}
+
+	public static int[] getMaxLevels() {
+		final Client client = Bot.resolve().client;
+		return client.getSkillLevelMaxes();
+	}
+
+	public static int[] getExperiences() {
+		final Client client = Bot.resolve().client;
+		return client.getSkillExperiences();
+	}
+
+	public static int[] getMaxExperiences() {
+		final Client client = Bot.resolve().client;
+		return client.getSkillExperienceMaxes();
+	}
+}
