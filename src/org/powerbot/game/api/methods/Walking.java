1a6529fe9f85756b89d95da0e5d7350a1fcb2f61
Joe Haddad <timer@powerbot.org>
2012-03-11 04:03:05 -0400

    Collision data (offest and flags).

--- src/org/powerbot/game/api/methods/Walking.java
+++ src/org/powerbot/game/api/methods/Walking.java
@@ -1,6 +1,12 @@
 package org.powerbot.game.api.methods;
 
+import org.powerbot.game.api.wrappers.Tile;
 import org.powerbot.game.bot.Bot;
+import org.powerbot.game.client.RSGroundDataBlocks;
+import org.powerbot.game.client.RSGroundDataInts;
+import org.powerbot.game.client.RSGroundDataX;
+import org.powerbot.game.client.RSGroundDataY;
+import org.powerbot.game.client.RSGroundInfoRSGroundArray;
 
 /**
  * @author Timer
@@ -14,5 +20,15 @@ public class Walking {
 	public static int getDestinationY() {
 		final Bot bot = Bot.resolve();
 		return bot.client.getDestY() * bot.multipliers.GLOBAL_DESTY;
+	}
+
+	public static Tile getCollisionOffset(final int plane) {
+		final Bot bot = Bot.resolve();
+		final Object groundDataInts = ((RSGroundDataInts) ((Object[]) ((RSGroundInfoRSGroundArray) bot.client.getRSGroundInfo()).getRSGroundInfoRSGroundArray())[plane]).getRSGroundDataInts();
+		return new Tile(((RSGroundDataX) groundDataInts).getRSGroundDataX() * bot.multipliers.GROUNDDATA_X, ((RSGroundDataY) groundDataInts).getRSGroundDataY() * bot.multipliers.GROUNDDATA_Y, plane);
+	}
+
+	public static int[][] getCollisionFlags(final int plane) {
+		return (int[][]) ((RSGroundDataBlocks) ((Object[]) ((RSGroundInfoRSGroundArray) Bot.resolve().client.getRSGroundInfo()).getRSGroundInfoRSGroundArray())[plane]).getRSGroundDataBlocks();
 	}
 }
