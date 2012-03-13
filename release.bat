f3e8716505b4c802905778da2a94640004f4f718
A. Ahmed <paris@powerbot.org>
2012-03-13 22:21:15 +0000

    ZKM obfuscation.

--- release.bat
+++ release.bat
@@ -2,35 +2,16 @@
 
 SETLOCAL
 
-SET Sz=7z
-SET advz=advzip
-
-CALL "%Sz%" > NUL 2>NUL
-IF NOT "%ERRORLEVEL%"=="0" GOTO :error
-CALL "%advz%" > NUL 2>NUL
-IF NOT "%ERRORLEVEL%"=="0" GOTO :error
-
 CALL make.bat
-ECHO Compressing release
+CALL make.bat :setvars
 
-SET name=RSBot
-SET dist=%name%.jar
-FOR /F %%G IN (resources\version.txt) DO SET version=%%G
-SET tmp=temp
-SET Path=%Path%;%ProgramFiles%\7-Zip
+CD "%lib%"
 
-RMDIR /S /Q "%tmp%" 2>NUL
-MKDIR "%tmp%"
-CD "%tmp%"
-CALL "%Sz%" x "..\%dist%" > NUL
-DEL /F /Q "..\%dist%"
-CALL "%Sz%" a -tzip "%dist%" . -mx=9 -mtc=off > NUL
-CALL "%advz%" -z -4 "%dist%" > NUL
+ECHO Obfuscating
+CALL java -D"ZKM_OPEN=%name%.jar" -D"ZKM_SAVE=.\\" -jar ZKM.jar script.txt
+DEL /F "%name%.jar.BACKUP"
+MOVE /Y "%name%.jar" "%name%-%version%.jar"
+
 CD ..
-MOVE /Y "%tmp%\%dist%" "%name%-%version%.jar" > NUL
-RMDIR /S /Q "%tmp%" 2>NUL
-GOTO :eof
 
-:error
-ECHO You do not have 7-zip installed and/or advzip available.
 GOTO :eof
