061ed5a30d2272de98c27b988de8a1ea68c8b593
A. Ahmed <paris@powerbot.org>
2012-03-15 23:55:35 +0000

    Improved view menu code.

--- src/org/powerbot/gui/component/BotMenuView.java
+++ src/org/powerbot/gui/component/BotMenuView.java
@@ -3,12 +3,12 @@ package org.powerbot.gui.component;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.lang.reflect.Constructor;
+import java.util.ArrayList;
 import java.util.EventListener;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
-import java.util.TreeMap;
-
 import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JMenu;
 
@@ -21,8 +21,6 @@ import org.powerbot.game.bot.event.impl.TClientState;
 import org.powerbot.game.bot.event.impl.TFloor;
 import org.powerbot.game.bot.event.impl.TMapBase;
 import org.powerbot.game.bot.event.impl.TPosition;
-import org.powerbot.game.bot.event.listener.PaintListener;
-import org.powerbot.game.bot.event.listener.internal.TextPaintListener;
 
 /**
  * @author Paris
@@ -31,33 +29,49 @@ public final class BotMenuView extends JMenu implements ActionListener {
 	private static final long serialVersionUID = 1L;
 	private final BotMenu parent;
 	private final Map<String, Class<? extends EventListener>> map;
+	private final List<String> items;
 	private static Map<Bot, Map<String, EventListener>> listeners;
 
 	private static final String ALL = "All";
+	private static final String MOUSE = "Mouse";
+	private static final String PLAYERS = "Players";
+	private static final String NPCS = "NPCs";
+	private static final String CLIENTSTATE = "Client State";
+	private static final String FLOOR = "Floor";
+	private static final String MAPBASE = "Map Base";
+	private static final String POSITION = "Position";
+	private static final String MESSAGES = "Messages";
+	private static final String SEPERATOR = "-";
 
 	public BotMenuView(final BotMenu parent) {
 		super(BotLocale.VIEW);
 		this.parent = parent;
 
-		final JCheckBoxMenuItem all = new JCheckBoxMenuItem(ALL);
-		all.addActionListener(this);
-		add(all);
-		addSeparator();
-
 		if (listeners == null) {
 			listeners = new HashMap<Bot, Map<String, EventListener>>();
 		}
-		map = new TreeMap<String, Class<? extends EventListener>>();
-		map.put("Mouse", DrawMouse.class);
-		map.put("Players", DrawPlayers.class);
-		map.put("Npcs", DrawNpcs.class);
 
-		map.put("Client State", TClientState.class);
-		map.put("Floor", TFloor.class);
-		map.put("Map Base", TMapBase.class);
-		map.put("Position", TPosition.class);
+		map = new HashMap<String, Class<? extends EventListener>>();
+		map.put(MOUSE, DrawMouse.class);
+		map.put(PLAYERS, DrawPlayers.class);
+		map.put(NPCS, DrawNpcs.class);
+		map.put(CLIENTSTATE, TClientState.class);
+		map.put(FLOOR, TFloor.class);
+		map.put(MAPBASE, TMapBase.class);
+		map.put(POSITION, TPosition.class);
+		map.put(MESSAGES, MessageLogger.class);
 
-		map.put("Messages", MessageLogger.class);
+		items = new ArrayList<String>(map.size());
+		items.add(MOUSE);
+		items.add(PLAYERS);
+		items.add(NPCS);
+		items.add(SEPERATOR);
+		items.add(CLIENTSTATE);
+		items.add(FLOOR);
+		items.add(MAPBASE);
+		items.add(POSITION);
+		items.add(SEPERATOR);
+		items.add(MESSAGES);
 
 		final Bot bot = Bot.bots.get(parent.parent.getOpenedTab());
 		Map<String, EventListener> listeners = BotMenuView.listeners.get(bot);
@@ -66,33 +80,29 @@ public final class BotMenuView extends JMenu implements ActionListener {
 			BotMenuView.listeners.put(bot, listeners);
 		}
 
-		for (final String key : map.keySet()) {
-			final Class<? extends EventListener> eventListener = map.get(key);
-			if (!PaintListener.class.isAssignableFrom(eventListener)) {
+		boolean selectedAll = true;
+
+		for (final String key : items) {
+			if (key.equals(SEPERATOR)) {
 				continue;
 			}
-			final boolean selected = listeners.containsKey(eventListener.getName());
-			final JCheckBoxMenuItem item = new JCheckBoxMenuItem(key, selected);
-			item.addActionListener(this);
-			add(item);
+			if (!listeners.containsKey(map.get(key).getName())) {
+				selectedAll = false;
+				break;
+			}
 		}
+
+		final JCheckBoxMenuItem all = new JCheckBoxMenuItem(ALL, selectedAll);
+		all.addActionListener(this);
+		add(all);
 		addSeparator();
-		for (final String key : map.keySet()) {
-			final Class<? extends EventListener> eventListener = map.get(key);
-			if (!TextPaintListener.class.isAssignableFrom(eventListener)) {
+
+		for (final String key : items) {
+			if (key.equals(SEPERATOR)) {
+				addSeparator();
 				continue;
 			}
-			final boolean selected = listeners.containsKey(eventListener.getName());
-			final JCheckBoxMenuItem item = new JCheckBoxMenuItem(key, selected);
-			item.addActionListener(this);
-			add(item);
-		}
-		addSeparator();
-		for (final String key : map.keySet()) {
 			final Class<? extends EventListener> eventListener = map.get(key);
-			if (PaintListener.class.isAssignableFrom(eventListener) || TextPaintListener.class.isAssignableFrom(eventListener)) {
-				continue;
-			}
 			final boolean selected = listeners.containsKey(eventListener.getName());
 			final JCheckBoxMenuItem item = new JCheckBoxMenuItem(key, selected);
 			item.addActionListener(this);
