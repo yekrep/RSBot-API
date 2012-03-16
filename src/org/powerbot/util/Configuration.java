e7a05cd8de507dd5b1404e251b185a8d236101b8
A. Ahmed <paris@powerbot.org>
2012-03-16 15:36:08 +0000

    Add spaces.

--- src/org/powerbot/util/Configuration.java
+++ src/org/powerbot/util/Configuration.java
@@ -29,15 +29,18 @@ public class Configuration {
 
 	static {
 		FROMJAR = Configuration.class.getClassLoader().getResource(Resources.Paths.VERSION) != null;
+
 		int v = 0;
 		try {
 			v = Integer.parseInt(IOHelper.readString(Resources.getResourceURL(Resources.Paths.VERSION)).trim());
 		} catch (final MalformedURLException ignored) {
 		}
 		VERSION = v;
+
 		final String appdata = System.getenv("APPDATA"), home = System.getProperty("user.home");
 		final String root = appdata != null && new File(appdata).isDirectory() ? appdata : home == null ? "~" : home;
 		STORE = root + File.separator + NAME + ".db";
+
 		final String os = System.getProperty("os.name");
 		if (os.contains("Mac")) {
 			OS = OperatingSystem.MAC;
