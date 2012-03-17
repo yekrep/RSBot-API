7387ee933dc6bfaa46db73a1dadb9dfa2cc1fbdd
Joe Haddad <timer@powerbot.org>
2012-03-16 22:58:29 -0400

    Wrote Area wrapper of a Polygon.

---
+++ src/org/powerbot/game/api/wrappers/Area.java
@@ -0,0 +1,25 @@
+package org.powerbot.game.api.wrappers;
+
+import java.awt.Polygon;
+
+public class Area extends Polygon {
+	public Area(final Tile t1, final Tile t2) {
+		this(new Tile[]{
+				new Tile(Math.min(t1.x, t2.x), Math.min(t1.y, t2.y), t1.plane),
+				new Tile(Math.max(t1.x, t2.x), Math.min(t1.y, t2.y), t1.plane),
+				new Tile(Math.max(t1.x, t2.x), Math.max(t1.y, t2.y), t2.plane),
+				new Tile(Math.min(t1.x, t2.x), Math.max(t1.y, t2.y), t2.plane)
+		});
+	}
+
+	public Area(final Tile[] bounds) {
+		int plane = -1;
+		for (final Tile tile : bounds) {
+			if (plane != -1 && tile.plane != plane) {
+				throw new RuntimeException("area does not support 3d");
+			}
+			plane = tile.plane;
+			addPoint(tile.x, tile.y);
+		}
+	}
+}
