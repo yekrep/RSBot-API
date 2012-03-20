96936dca2cddf17b4b5eb7cb055324608b3e4976
Joe Haddad <timer@powerbot.org>
2012-03-20 17:22:09 -0400

    Documented Mouse.

--- src/org/powerbot/game/api/methods/input/Mouse.java
+++ src/org/powerbot/game/api/methods/input/Mouse.java
@@ -16,50 +16,85 @@ import org.powerbot.game.api.util.Time;
 import org.powerbot.game.api.wrappers.Locatable;
 import org.powerbot.game.bot.Bot;
 
+/**
+ * A utility for the manipulation of the game's mouse.
+ *
+ * @author Timer
+ */
 public class Mouse {
 	private static final Map<ThreadGroup, Integer> dragLengths = new HashMap<ThreadGroup, Integer>();
 	private static final Map<ThreadGroup, Integer> sides = new HashMap<ThreadGroup, Integer>();
 
+	/**
+	 * @return The local x position of the mouse.
+	 */
 	public static int getX() {
 		final org.powerbot.game.client.input.Mouse mouse = getMouse();
 		return mouse == null ? -1 : mouse.getX();
 	}
 
+	/**
+	 * @return The local y position of the mouse.
+	 */
 	public static int getY() {
 		final org.powerbot.game.client.input.Mouse mouse = getMouse();
 		return mouse == null ? -1 : mouse.getY();
 	}
 
+	/**
+	 * @return The local (<code>Point</code>) position of the mouse.
+	 */
 	public static Point getLocation() {
 		final org.powerbot.game.client.input.Mouse mouse = getMouse();
 		return mouse == null ? new Point(-1, -1) : new Point(mouse.getX(), mouse.getY());
 	}
 
+	/**
+	 * @return The local x location at which the bot's mouse was last clicked.
+	 */
 	public static int getPressX() {
 		final org.powerbot.game.client.input.Mouse mouse = getMouse();
 		return mouse == null ? -1 : mouse.getPressX();
 	}
 
+	/**
+	 * @return The local y location at which the bot's mouse was last clicked.
+	 */
 	public static int getPressY() {
 		final org.powerbot.game.client.input.Mouse mouse = getMouse();
 		return mouse == null ? -1 : mouse.getPressY();
 	}
 
+	/**
+	 * @return The System time of the last recorded mouse click.
+	 */
 	public static long getPressTime() {
 		final org.powerbot.game.client.input.Mouse mouse = getMouse();
 		return mouse == null ? -1L : mouse.getPressTime();
 	}
 
+
+	/**
+	 * @return <tt>true</tt> if the mouse is present; otherwise <tt>false</tt>.
+	 */
 	public static boolean isPresent() {
 		final org.powerbot.game.client.input.Mouse mouse = getMouse();
 		return mouse != null && mouse.isPresent();
 	}
 
+	/**
+	 * @return <tt>true</tt> if the mouse is pressed; otherwise <tt>false</tt>.
+	 */
 	public static boolean isPressed() {
 		final org.powerbot.game.client.input.Mouse mouse = getMouse();
 		return mouse != null && mouse.isPressed();
 	}
 
+	/**
+	 * Clicks the mouse at it's current position.
+	 *
+	 * @param left <tt>true</tt> to click left; otherwise <tt>false</tt> to click right.
+	 */
 	public static void click(final boolean left) {
 		if (!isPresent()) {
 			return;
@@ -71,6 +106,12 @@ public class Mouse {
 		Time.sleep(Random.nextInt(50, 80));
 	}
 
+	/**
+	 * Holds the mouse at it's current position.
+	 *
+	 * @param time The amount of milliseconds to hold the mouse down for.
+	 * @param left <tt>true</tt> to click left; otherwise <tt>false</tt> to click right.
+	 */
 	public static void hold(final int time, final boolean left) {
 		if (!isPresent()) {
 			return;
@@ -82,16 +123,37 @@ public class Mouse {
 		Time.sleep(Random.nextInt(50, 80));
 	}
 
+	/**
+	 * Hops the mouse to the desired destination.
+	 *
+	 * @param x The x location to hop to.
+	 * @param y The y location to hop to.
+	 */
 	public static void hop(final int x, final int y) {
 		hop(x, y, 0, 0);
 	}
 
+	/**
+	 * Hops the mouse to the desired destination.
+	 *
+	 * @param x       The x location to hop to.
+	 * @param y       The y location to hop to.
+	 * @param randomX The random x gaussian distribution.
+	 * @param randomY The random y gaussian distribution.
+	 */
 	public static void hop(int x, int y, final int randomX, final int randomY) {
 		if (isOnCanvas(x, y)) {
 			moveMouse(x + Random.nextGaussian(-randomX, randomX, randomX), y + Random.nextGaussian(-randomY, randomY, randomY));
 		}
 	}
 
+	/**
+	 * Moves the mouse to the desired destination.
+	 *
+	 * @param x THe desired x location.
+	 * @param y The desired y location.
+	 * @return <tt>true</tt> if we reached this position; otherwise <tt>false</tt>.
+	 */
 	public static boolean move(final int x, final int y) {
 		final TaskContainer container = Bot.resolve().processor;
 		final MouseManipulator task = create(x, y, 0, 0, false, false);
@@ -104,6 +166,15 @@ public class Mouse {
 		return task.isAccepted();
 	}
 
+	/**
+	 * Moves the mouse to the desired destination.
+	 *
+	 * @param x       THe desired x location.
+	 * @param y       The desired y location.
+	 * @param randomX The random x gaussian distribution.
+	 * @param randomY The random y gaussian distribution.
+	 * @return <tt>true</tt> if we reached this position; otherwise <tt>false</tt>.
+	 */
 	public static boolean move(int x, int y, final int randomX, final int randomY) {
 		final TaskContainer container = Bot.resolve().processor;
 		final MouseManipulator task = create(x, y, randomX, randomY, false, false);
@@ -248,6 +319,11 @@ public class Mouse {
 		sides.put(Thread.currentThread().getThreadGroup(), length);
 	}
 
+	/**
+	 * @param x The x location to check.
+	 * @param y The y location to check.
+	 * @return <tt>true</tt> if the given point is on the canvas; otherwise <tt>false</tt>.
+	 */
 	public static boolean isOnCanvas(final int x, final int y) {
 		final Bot bot = Bot.resolve();
 		final Canvas canvas;
