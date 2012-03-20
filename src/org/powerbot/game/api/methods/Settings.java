f50c09b32d258109f56683444a104e0a6db4a36e
Joe Haddad <timer@powerbot.org>
2012-03-20 17:21:55 -0400

    Documented Settings.

--- src/org/powerbot/game/api/methods/Settings.java
+++ src/org/powerbot/game/api/methods/Settings.java
@@ -4,13 +4,22 @@ import org.powerbot.game.bot.Bot;
 import org.powerbot.game.client.SettingsData;
 
 /**
+ * A utility for the manipulation of game settings.
+ *
  * @author Timer
  */
 public class Settings {
+	/**
+	 * @return The <code>int[]</code> of all the game's settings in their respective positioning.
+	 */
 	public static int[] get() {
 		return ((int[]) ((SettingsData) Bot.resolve().client.getSettingArray()).getSettingsData()).clone();
 	}
 
+	/**
+	 * @param index The position of this setting in the game's database.
+	 * @return The setting value of the desired index.
+	 */
 	public static int get(final int index) {
 		final int[] settings = get();
 		if (index < settings.length) {
