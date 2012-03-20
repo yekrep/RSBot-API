39835983d17f29ea9cad3aba8f9d30ed7f4a3728
Joe Haddad <timer@powerbot.org>
2012-03-19 22:47:23 -0400

    Implemented model rendering.

--- src/org/powerbot/game/bot/event/impl/DrawModels.java
+++ src/org/powerbot/game/bot/event/impl/DrawModels.java
@@ -3,24 +3,59 @@ package org.powerbot.game.bot.event.impl;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.Point;
 import java.awt.RenderingHints;
+import java.util.HashMap;
 
+import org.powerbot.game.api.methods.interactive.Npcs;
 import org.powerbot.game.api.methods.interactive.Players;
+import org.powerbot.game.api.methods.location.Locations;
 import org.powerbot.game.api.wrappers.graphics.CapturedModel;
-import org.powerbot.game.api.wrappers.interactive.Player;
+import org.powerbot.game.api.wrappers.location.Location;
 import org.powerbot.game.bot.event.listener.PaintListener;
 
 public class DrawModels implements PaintListener {
+	private static final HashMap<Location.Type, Color> color_map = new HashMap<Location.Type, Color>();
+
+	static {
+		color_map.put(Location.Type.BOUNDARY, Color.BLACK);
+		color_map.put(Location.Type.FLOOR_DECORATION, Color.YELLOW);
+		color_map.put(Location.Type.INTERACTIVE, Color.WHITE);
+		color_map.put(Location.Type.WALL_DECORATION, Color.GRAY);
+	}
+
 	public void onRepaint(final Graphics graphics) {
-		Graphics2D render = (Graphics2D) graphics;
+		final Graphics2D render = (Graphics2D) graphics;
 		render.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
 				RenderingHints.VALUE_ANTIALIAS_ON);
-		render.setColor(Color.green);
-		final Player[] players = Players.getLoaded();
-		for (final Player player : players) {
-			final CapturedModel model = player.getModel();
-			if (model != null) {
+		for (final Location e : Locations.getLoaded()) {
+			final CapturedModel model = e.getModel();
+			if (model != null && e.isOnScreen()) {
+				render.setColor(color_map.get(e.getType()));
 				model.draw(render);
+				render.setColor(Color.green);
+				final Point p = model.getCentralPoint();
+				render.fillOval(p.x - 2, p.y - 2, 4, 4);
+			}
+		}
+		for (final org.powerbot.game.api.wrappers.interactive.Character e : Players.getLoaded()) {
+			final CapturedModel model = e.getModel();
+			if (model != null && e.isOnScreen()) {
+				render.setColor(Color.red);
+				model.draw(render);
+				render.setColor(Color.green);
+				final Point p = model.getCentralPoint();
+				render.fillOval(p.x - 2, p.y - 2, 4, 4);
+			}
+		}
+		for (final org.powerbot.game.api.wrappers.interactive.Character e : Npcs.getLoaded()) {
+			final CapturedModel model = e.getModel();
+			if (model != null && e.isOnScreen()) {
+				render.setColor(Color.magenta);
+				model.draw(render);
+				render.setColor(Color.green);
+				final Point p = model.getCentralPoint();
+				render.fillOval(p.x - 2, p.y - 2, 4, 4);
 			}
 		}
 	}
