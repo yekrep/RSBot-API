16cad135fa5f378b2dbab922b024e22552a53830
Joe Haddad <timer@powerbot.org>
2012-03-22 18:35:42 -0400

    Created AntiRandom abstract class for implementation.

---
+++ src/org/powerbot/game/bot/random/AntiRandom.java
@@ -0,0 +1,9 @@
+package org.powerbot.game.bot.random;
+
+import java.util.EventListener;
+
+import org.powerbot.concurrent.Task;
+import org.powerbot.lang.Activator;
+
+public abstract class AntiRandom extends Task implements Activator, EventListener {
+}
