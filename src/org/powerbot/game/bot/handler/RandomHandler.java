bf37702aef1fab8479e0f1c6865ffa4058506234
Joe Haddad <timer@powerbot.org>
2012-03-22 18:51:23 -0400

    Wrote RandomHandler body. Pause script, wait for execution pause, finish random, and resume script.

--- src/org/powerbot/game/bot/handler/RandomHandler.java
+++ src/org/powerbot/game/bot/handler/RandomHandler.java
@@ -1,10 +1,19 @@
 package org.powerbot.game.bot.handler;
 
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+
 import org.powerbot.concurrent.Task;
+import org.powerbot.game.api.ActiveScript;
+import org.powerbot.game.api.util.Random;
+import org.powerbot.game.api.util.Time;
 import org.powerbot.game.bot.Bot;
 import org.powerbot.game.bot.random.AntiRandom;
 import org.powerbot.game.bot.random.Login;
 
+/**
+ * @author Timer
+ */
 public class RandomHandler extends Task {
 	private final Bot bot;
 	private final AntiRandom[] antiRandoms;
@@ -17,7 +26,33 @@ public class RandomHandler extends Task {
 	}
 
 	public void run() {
-		while (bot.getActiveScript() != null) {
+		ActiveScript activeScript;
+		while ((activeScript = bot.getActiveScript()) != null) {
+			Future<?> submittedRandom = null;
+			for (final AntiRandom antiRandom : antiRandoms) {
+				if (antiRandom.applicable()) {
+					activeScript.pause(true);
+					while (activeScript.getContainer().isActive()) {
+						Time.sleep(Random.nextInt(500, 1200));
+					}
+
+					bot.processor.submit(antiRandom);
+					if (antiRandom.future != null) {
+						submittedRandom = antiRandom.future;
+						break;
+					}
+				}
+			}
+			if (submittedRandom != null) {
+				try {
+					submittedRandom.get();
+				} catch (final InterruptedException ignored) {
+				} catch (final ExecutionException ignored) {
+				}
+				activeScript.resume();
+			} else {
+				Time.sleep(Random.nextInt(1000, 5000));
+			}
 		}
 	}
 }
