c2175070b380f705bea92978f20c65b80d74654e
Joe Haddad <timer@powerbot.org>
2012-03-22 21:38:56 -0400

    Removed extra constructor. Require user to set manually.

--- src/org/powerbot/concurrent/action/Action.java
+++ src/org/powerbot/concurrent/action/Action.java
@@ -21,20 +21,9 @@ public class Action {
 	 * @param tasks     The tasks associated with this <code>Action</code>.
 	 */
 	public Action(final Activatable activator, final Task... tasks) {
-		this(true, activator, tasks);
-	}
-
-	/**
-	 * Initializes this <code>Action</code> with appropriate information required for processing.
-	 *
-	 * @param activator   The <code>Activator</code> associated with this <code>Action</code>.
-	 * @param tasks       The tasks associated with this <code>Action</code>.
-	 * @param requireLock <tt>true</tt> to require the ActionManager to lock while processing this action; otherwise <tt>false</tt>.
-	 */
-	public Action(final boolean requireLock, final Activatable activator, final Task... tasks) {
 		this.activator = activator;
 		this.tasks = tasks;
-		this.requireLock = requireLock;
-		this.resetExecutionQueue = false;
+		requireLock = true;
+		resetExecutionQueue = false;
 	}
 }
