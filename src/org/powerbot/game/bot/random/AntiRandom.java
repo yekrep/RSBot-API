d068ad2c3eb4de328192b17446f6eedec848694c
Joe Haddad <timer@powerbot.org>
2012-03-23 00:40:44 -0400

    Removed EventListener implementation.

--- src/org/powerbot/game/bot/random/AntiRandom.java
+++ src/org/powerbot/game/bot/random/AntiRandom.java
@@ -1,9 +1,7 @@
 package org.powerbot.game.bot.random;
 
-import java.util.EventListener;
-
 import org.powerbot.concurrent.Task;
 import org.powerbot.lang.Activatable;
 
-public abstract class AntiRandom extends Task implements Activatable, EventListener {
+public abstract class AntiRandom extends Task implements Activatable {
 }
