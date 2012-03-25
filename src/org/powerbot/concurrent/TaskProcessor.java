c8e60edacc45de13e0745d1a88eee3dc9b0c33b9
Joe Haddad <timer@powerbot.org>
2012-03-25 19:09:43 -0400

    Increase processor utilization.

--- src/org/powerbot/concurrent/TaskProcessor.java
+++ src/org/powerbot/concurrent/TaskProcessor.java
@@ -14,7 +14,7 @@ public class TaskProcessor implements TaskContainer {
 	private final ThreadPoolExecutor executor;
 
 	public TaskProcessor(final ThreadGroup threadGroup) {
-		final int nThreads = Runtime.getRuntime().availableProcessors() * 2;
+		final int nThreads = Runtime.getRuntime().availableProcessors() * 3;
 		executor = new ThreadPoolExecutor(nThreads, nThreads,
 				0L, TimeUnit.MILLISECONDS,
 				new LinkedBlockingQueue<Runnable>(), new ThreadPool(threadGroup));
