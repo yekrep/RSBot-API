5fd69e0dad77c14dfc4049f1e423b3760e03848e
Joe Haddad <timer@powerbot.org>
2012-03-26 17:29:43 -0400

    Complete base login random event.

--- src/org/powerbot/game/api/randoms/Login.java
+++ src/org/powerbot/game/api/randoms/Login.java
@@ -1,19 +1,203 @@
 package org.powerbot.game.api.randoms;
 
+import java.awt.Rectangle;
+
+import org.powerbot.concurrent.Task;
 import org.powerbot.game.api.AntiRandom;
 import org.powerbot.game.api.Manifest;
 import org.powerbot.game.api.methods.Game;
+import org.powerbot.game.api.methods.Widgets;
+import org.powerbot.game.api.methods.input.Keyboard;
+import org.powerbot.game.api.methods.input.Mouse;
+import org.powerbot.game.api.util.Random;
+import org.powerbot.game.api.util.Time;
+import org.powerbot.game.api.wrappers.widget.WidgetChild;
 
 /**
  * @author Timer
  */
 @Manifest(name = "Login", description = "Logs into the game and handles errors.", version = 0.1, authors = {"Timer"})
 public class Login extends AntiRandom {
+	private static final int WIDGET = 596;
+	private static final int WIDGET_LOGIN_TOKEN = 13;
+	private static final int WIDGET_LOGIN_USERNAME_TEXT = 70;
+	private static final int WIDGET_LOGIN_PASSWORD_TEXT = 76;
+	private static final int WIDGET_LOGIN_ENTER_GAME = 44;
+
+	private static final int WIDGET_LOBBY = 906;
+	private static final int WIDGET_LOBBY_PLAY = 184;
+
 	public boolean applicable() {
 		final int state = Game.getClientState();
-		return state == Game.INDEX_LOGIN_SCREEN || state == Game.INDEX_LOBBY_SCREEN;
+		return state == Game.INDEX_LOGIN_SCREEN || state == Game.INDEX_LOBBY_SCREEN || state == Game.INDEX_LOGGING_IN;
+	}
+
+	private enum LoginEvent {
+		TOKEN_FAILURE(WIDGET_LOGIN_TOKEN, "token failure", 0, new Task() {
+			public void run() {
+			}
+		});
+
+		private final String message;
+		private final int child, wait;
+		private final Task task;
+
+		LoginEvent(final int child, final String message, final int wait, final Task task) {
+			this.child = child;
+			this.message = message;
+			this.wait = wait;
+			this.task = task;
+		}
+
+		LoginEvent(final int child, final String message, final int wait) {
+			this(child, message, wait, null);
+		}
+	}
+
+	private enum LobbyEvent {
+		;
+
+		private final String message;
+		private final int child, wait;
+		private final Task task;
+
+		LobbyEvent(final int child, final String message, final int wait, final Task task) {
+			this.child = child;
+			this.message = message;
+			this.wait = wait;
+			this.task = task;
+		}
+
+		LobbyEvent(final int child, final String message, final int wait) {
+			this(child, message, wait, null);
+		}
 	}
 
 	public void run() {
+		if (Game.getClientState() == Game.INDEX_LOBBY_SCREEN) {
+			Widgets.get(WIDGET_LOBBY, WIDGET_LOBBY_PLAY).click(true);
+			Time.sleep(Random.nextInt(200, 500));
+			return;
+		}
+
+		if (Game.getClientState() == Game.INDEX_LOGIN_SCREEN) {
+			for (final LoginEvent loginEvent : LoginEvent.values()) {
+				final WidgetChild widgetChild = Widgets.get(WIDGET, loginEvent.child);
+				if (widgetChild != null && widgetChild.verify()) {
+					final String text = widgetChild.getText().toLowerCase().trim();
+
+					if (text.contains(loginEvent.message)) {
+						log.info("Handling login event: " + loginEvent.name());
+
+						if (loginEvent.wait > 0) {
+							Time.sleep(loginEvent.wait);
+						} else if (loginEvent.wait == -1) {
+							//TODO stop script
+						}
+
+						if (loginEvent.task != null) {
+							bot.getContainer().submit(loginEvent.task);
+						}
+						return;
+					}
+				}
+			}
+
+			if (isUsernameCorrect() && isPasswordValid()) {
+				attemptLogin();
+				Time.sleep(Random.nextInt(1200, 2000));
+			} else if (!isUsernameCorrect()) {
+				final String username = "";//TODO
+				final WidgetChild usernameTextBox = Widgets.get(WIDGET, WIDGET_LOGIN_USERNAME_TEXT);
+				if (!clickLoginInterface(usernameTextBox)) {
+					return;
+				}
+				Time.sleep(Random.nextInt(500, 700));
+				final int textLength = usernameTextBox.getText().length();
+				if (textLength > 0) {
+					erase(textLength);
+					return;
+				}
+				Keyboard.sendText(username, false);
+				Time.sleep(Random.nextInt(500, 700));
+			} else if (!isPasswordValid()) {
+				final String password = "";//TODO
+				final WidgetChild passwordTextBox = Widgets.get(WIDGET, WIDGET_LOGIN_PASSWORD_TEXT);
+				if (!clickLoginInterface(passwordTextBox)) {
+					return;
+				}
+				Time.sleep(Random.nextInt(500, 700));
+				final int textLength = passwordTextBox.getText().length();
+				if (textLength > 0) {
+					erase(textLength);
+					return;
+				}
+				Keyboard.sendText(password, false);
+				Time.sleep(Random.nextInt(500, 700));
+			}
+		}
+	}
+
+	private boolean clickLoginInterface(final WidgetChild i) {
+		if (!i.verify()) {
+			return false;
+		}
+		final Rectangle pos = i.getBoundingRectangle();
+		if (pos.x == -1 || pos.y == -1 || pos.width == -1 || pos.height == -1) {
+			return false;
+		}
+		final int dy = (int) (pos.getHeight() - 4) / 2;
+		final int maxRandomX = (int) (pos.getMaxX() - pos.getCenterX());
+		final int midx = (int) pos.getCenterX();
+		final int midy = (int) (pos.getMinY() + pos.getHeight() / 2);
+		if (i.getIndex() == WIDGET_LOGIN_PASSWORD_TEXT) {
+			return Mouse.click(getPasswordX(i), midy + Random.nextInt(-dy, dy), true);
+		}
+		return Mouse.click(midx + Random.nextInt(1, maxRandomX), midy + Random.nextInt(-dy, dy), true);
+	}
+
+	private int getPasswordX(final WidgetChild a) {
+		int x = 0;
+		final Rectangle pos = a.getBoundingRectangle();
+		final int dx = (int) (pos.getWidth() - 4) / 2;
+		final int midx = (int) (pos.getMinX() + pos.getWidth() / 2);
+		if (pos.x == -1 || pos.y == -1 || pos.width == -1 || pos.height == -1) {
+			return 0;
+		}
+		for (int i = 0; i < Widgets.get(WIDGET, WIDGET_LOGIN_PASSWORD_TEXT).getText().length(); i++) {
+			x += 11;
+		}
+		if (x > 44) {
+			return (int) (pos.getMinX() + x + 15);
+		} else {
+			return midx + Random.nextInt(-dx, dx);
+		}
+	}
+
+	private boolean isUsernameCorrect() {
+		final String userName = "";//TODO
+		return Widgets.get(WIDGET, WIDGET_LOGIN_USERNAME_TEXT).getText().toLowerCase().equalsIgnoreCase(userName);
+	}
+
+	private boolean isPasswordValid() {
+		String passWord = "";//TODO
+		return Widgets.get(WIDGET, WIDGET_LOGIN_PASSWORD_TEXT).getText().length() == (passWord == null ? 0 : passWord.length());
+	}
+
+	private void attemptLogin() {
+		if (Random.nextInt(0, 6) == 0) {
+			Widgets.get(WIDGET, WIDGET_LOGIN_ENTER_GAME).click(true);
+		} else {
+			Keyboard.sendKey('\n', Random.nextInt(100, 200));
+		}
+	}
+
+	private void erase(final int count) {
+		for (int i = 0; i <= count + Random.nextInt(1, 5); i++) {
+			Keyboard.sendKey('\b', Random.nextInt(50, 150));
+			if (Random.nextInt(0, 2) == 1) {
+				Time.sleep(Random.nextInt(25, 100));
+			}
+		}
 	}
 }
