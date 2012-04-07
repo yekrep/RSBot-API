d4ac6d48537518f6dbc673004b165a79ed918e04
Joe Haddad <timer@powerbot.org>
2012-04-06 22:39:02 -0400

    Updated Certer random event.

--- src/org/powerbot/game/bot/api/randoms/Certer.java
+++ src/org/powerbot/game/bot/api/randoms/Certer.java
@@ -25,10 +25,13 @@ public class Certer extends AntiRandom {
 	private static final int WIDGET_IDENTIFY = 184;
 	private static final int WIDGET_IDENTIFY_ITEM = 8;
 	private static final int WIDGET_IDENTIFY_ITEM_MODEL = 3;
+	private static final int WIDGET_CHAT = 1184;
+	private static final int WIDGET_CHAT_TEXT = 13;
 	private static final int[] WIDGET_MODEL_IDS = {2807, 8828, 8829, 8832, 8833, 8834, 8835, 8836, 8837};
 	private static final int[] LOCATION_ID_BOOKS = {42352, 42354};
 	private static final String[] ITEM_NAMES = {"bowl", "battleaxe", "fish",
 			"shield", "helmet", "ring", "shears", "sword", "spade"};
+	private int settingValue = 0;
 
 	@Override
 	public boolean validate() {
@@ -38,10 +41,16 @@ public class Certer extends AntiRandom {
 	@Override
 	public void run() {
 		if (Players.getLocal().getAnimation() != -1 || Players.getLocal().isMoving()) {
+			verbose("SLEEP: Walking or animating ...");
 			Time.sleep(Random.nextInt(500, 1000));
 			return;
 		}
-		if (Settings.get(SETTING_SOLVED) == 0x1982899a) {
+		verbose("SETTING_SOLVED: " + Settings.get(SETTING_SOLVED));
+		if (settingValue == 0) {
+			settingValue = Settings.get(SETTING_SOLVED);
+		}
+		if (Settings.get(SETTING_SOLVED) > settingValue) {
+			verbose("SOLVED: Attempt exit");
 			final Location portal = Locations.getNearest(LOCATION_ID_PORTAL);
 			if (portal != null) {
 				if (!portal.isOnScreen()) {
@@ -54,31 +63,45 @@ public class Certer extends AntiRandom {
 			}
 			return;
 		}
+		if (Widgets.get(WIDGET_CHAT, WIDGET_CHAT_TEXT).validate() &&
+				Widgets.get(WIDGET_CHAT, WIDGET_CHAT_TEXT).getText().contains("portal")) {
+			settingValue--;
+		}
 		if (Widgets.clickContinue()) {
+			verbose("Conversation ...");
 			Time.sleep(Random.nextInt(500, 1000));
 			return;
 		}
 		if (Widgets.get(WIDGET_IDENTIFY, WIDGET_IDENTIFY_ITEM).validate()) {
+			verbose("WIDGET VALIDATED: Quiz window");
 			final int model = Widgets.get(WIDGET_IDENTIFY, WIDGET_IDENTIFY_ITEM).getChild(WIDGET_IDENTIFY_ITEM_MODEL).getModelId();
 			String itemName = null;
+			verbose("Model id: " + model);
 			for (int i = 0; i < WIDGET_MODEL_IDS.length; i++) {
 				if (WIDGET_MODEL_IDS[i] == model) {
 					itemName = ITEM_NAMES[i];
+					verbose("Identified: " + itemName);
+					break;
 				}
 			}
 			if (itemName != null) {
+				verbose("Crawling widgets");
 				for (int j = 0; j < 3; j++) {
 					final WidgetChild child = Widgets.get(WIDGET_IDENTIFY, WIDGET_IDENTIFY_ITEM).getChild(j);
+					verbose("TEXT: " + child.getText().toLowerCase());
 					if (child.getText().toLowerCase().contains(itemName.toLowerCase())) {
+						verbose("FOUND!  Attempt click...");
 						if (child.click(true)) {
 							Time.sleep(Random.nextInt(1000, 1200));
 						}
+						break;
 					}
 				}
 			}
 			return;
 		}
 
+		verbose("We need to speak with ?iles!");
 		final Npc man = Npcs.getNearest(new Filter<Npc>() {
 			@Override
 			public boolean accept(final Npc npc) {
