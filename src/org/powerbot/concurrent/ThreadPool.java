bbb1ae90782e6f7618a724f8978d87250cd6d9bd
Joe Haddad <timer@powerbot.org>
2012-04-14 23:47:44 -0400

    Name threads.

--- src/org/powerbot/concurrent/ThreadPool.java
+++ src/org/powerbot/concurrent/ThreadPool.java
@@ -1,5 +1,7 @@
 package org.powerbot.concurrent;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.atomic.AtomicInteger;
 
@@ -10,6 +12,7 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 public class ThreadPool implements ThreadFactory {
 	public static final String THREADGROUPNAMEPREFIX = "ThreadPool@";
+	public static final Map<Runnable, String> suffix = new HashMap<Runnable, String>();
 
 	private final AtomicInteger threadNumber;
 	private final ThreadGroup threadGroup;
@@ -23,10 +26,24 @@ public class ThreadPool implements ThreadFactory {
 	 * {@inheritDoc}
 	 */
 	public Thread newThread(final Runnable r) {
-		return new Thread(
-				threadGroup, r,
-				new StringBuilder(THREADGROUPNAMEPREFIX).append(hashCode()).append("-").append(threadNumber.getAndIncrement()).append('/').
-						append(Thread.currentThread().getName()).append("@").append(Thread.currentThread().getThreadGroup()).toString()
-		);
+		String addition = suffix.get(r);
+		if (addition == null) {
+			addition = "";
+
+			final String threadName = Thread.currentThread().getName();
+			if (threadName.contains(" /NAME/ ")) {
+				final String[] threadData = threadName.split(" /NAME/ ");
+				if (threadData.length == 2) {
+					addition = threadData[1];
+				}
+			}
+		}
+		final StringBuilder builder = new StringBuilder(THREADGROUPNAMEPREFIX).append(hashCode()).append("-").append(threadNumber.getAndIncrement()).append('/').
+				append(Thread.currentThread().getName()).append("@").append(Thread.currentThread().getThreadGroup());
+		if (!addition.isEmpty()) {
+			builder.append(" /NAME/ ");
+			builder.append(addition);
+		}
+		return new Thread(threadGroup, r, builder.toString());
 	}
 }
