c181e3dd5027024c2218fcfc8a05abb11cc8d392
A. Ahmed <paris@powerbot.org>
2012-04-25 22:49:17 +0100

    Hard code version number.

--- src/org/powerbot/util/Configuration.java
+++ src/org/powerbot/util/Configuration.java
@@ -1,9 +1,7 @@
 package org.powerbot.util;
 
 import java.io.File;
-import java.net.MalformedURLException;
 
-import org.powerbot.util.io.IOHelper;
 import org.powerbot.util.io.Resources;
 
 /**
@@ -13,7 +11,7 @@ public class Configuration {
 	public static final String NAME = "RSBot";
 	public static final boolean FROMJAR;
 	public static boolean DEVMODE = false;
-	public static final int VERSION;
+	public static final int VERSION = 4006;
 	public static final String STORE, BOOTSETTINGS;
 	public static final OperatingSystem OS;
 
@@ -30,13 +28,6 @@ public class Configuration {
 
 	static {
 		FROMJAR = Configuration.class.getClassLoader().getResource(Resources.Paths.VERSION) != null;
-
-		int v = 0;
-		try {
-			v = Integer.parseInt(IOHelper.readString(Resources.getResourceURL(Resources.Paths.VERSION)).trim());
-		} catch (final MalformedURLException ignored) {
-		}
-		VERSION = v;
 
 		final String appdata = System.getenv("APPDATA"), home = System.getProperty("user.home");
 		final String root = appdata != null && new File(appdata).isDirectory() ? appdata : home == null ? "~" : home;
