4b4c02ae4217f46b0bbb3d867a31887cb1d18d36
A. Ahmed <paris@powerbot.org>
2012-04-28 15:19:01 +0100

    Split underlying XOR encryption code into reusable class.

--- src/org/powerbot/util/io/XORInputStream.java
+++ src/org/powerbot/util/io/XORInputStream.java
@@ -4,42 +4,21 @@ import java.io.FilterInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-import javax.crypto.Cipher;
-
 /**
  * @author Paris
  */
 public final class XORInputStream extends FilterInputStream {
-	private final byte[] key;
-	private final int opmode, l;
-	private int n, d;
-	public static final int DELTA = 0x9e3779b9;
+	private final XOR xor;
 
 	public XORInputStream(final InputStream in, final byte[] key, final int opmode) {
 		super(in);
-		this.key = key;
-		if (!(opmode == Cipher.DECRYPT_MODE || opmode == Cipher.ENCRYPT_MODE)) {
-			throw new IllegalArgumentException();
-		}
-		this.opmode = opmode;
-		l = this.key.length;
-		n = 0;
-		d = DELTA;
-	}
-
-	private void rotate(final byte[] b, final int off, final int len) {
-		for (int i = off; i < off + len; i++) {
-			final int z = n++ % l, x = key[z];
-			this.d += key[l - z - 1];
-			final int d = this.d & 0x7f;
-			b[i] = (byte) (opmode == Cipher.DECRYPT_MODE ? (b[i] + d) % 0xff ^ x : (b[i] ^ x) - d % 0xff);
-		}
+		xor = new XOR(key, opmode);
 	}
 
 	@Override
 	public int read(final byte[] b, final int off, final int len) throws IOException {
 		final int result = super.read(b, off, len);
-		rotate(b, off, len);
+		xor.rotate(b, off, len);
 		return result;
 	}
 }
--- src/org/powerbot/util/io/XOROutputStream.java
+++ src/org/powerbot/util/io/XOROutputStream.java
@@ -4,40 +4,20 @@ import java.io.FilterOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 
-import javax.crypto.Cipher;
-
 /**
  * @author Paris
  */
 public final class XOROutputStream extends FilterOutputStream {
-	private final byte[] key;
-	private final int opmode, l;
-	private int n, d;
+	private final XOR xor;
 
 	public XOROutputStream(final OutputStream out, final byte[] key, final int opmode) {
 		super(out);
-		this.key = key;
-		if (!(opmode == Cipher.DECRYPT_MODE || opmode == Cipher.ENCRYPT_MODE)) {
-			throw new IllegalArgumentException();
-		}
-		this.opmode = opmode;
-		l = this.key.length;
-		n = 0;
-		d = XORInputStream.DELTA;
-	}
-
-	private void rotate(final byte[] b, final int off, final int len) {
-		for (int i = off; i < off + len; i++) {
-			final int z = n++ % l, x = key[z];
-			this.d += key[l - z - 1];
-			final int d = this.d & 0x7f;
-			b[i] = (byte) (opmode == Cipher.DECRYPT_MODE ? (b[i] + d) % 0xff ^ x : (b[i] ^ x) - d % 0xff);
-		}
+		xor = new XOR(key, opmode);
 	}
 
 	@Override
 	public void write(final byte[] b, final int off, final int len) throws IOException {
-		rotate(b, off, len);
+		xor.rotate(b, off, len);
 		super.out.write(b, off, len);
 	}
 }
