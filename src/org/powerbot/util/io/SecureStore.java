902465e8ebaacb69318b7ec2e1e778a108dfcfa5
A. Ahmed <paris@powerbot.org>
2012-04-29 19:39:04 +0100

    Revert to RC4.

--- src/org/powerbot/util/io/SecureStore.java
+++ src/org/powerbot/util/io/SecureStore.java
@@ -29,8 +29,8 @@ import org.powerbot.util.StringUtil;
 public final class SecureStore {
 	private final static Logger log = Logger.getLogger(SecureStore.class.getName());
 	private final static SecureStore instance = new SecureStore();
-	private final static int MAGIC = 0x00525354, VERSION = 1006, BLOCKSIZE = 512, MAXBLOCKS = 2048;
-	private final static String CIPHER_ALGORITHM = "XOR", KEY_ALGORITHM = "";
+	private final static int MAGIC = 0x00525354, VERSION = 1007, BLOCKSIZE = 512, MAXBLOCKS = 2048;
+	private final static String CIPHER_ALGORITHM = "RC4", KEY_ALGORITHM = "RC4";
 	private final File store;
 	private final Map<String, TarEntry> entries;
 	private byte[] key;
