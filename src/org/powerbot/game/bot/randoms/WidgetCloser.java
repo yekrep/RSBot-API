968f2f26e0e5e144f1ba4c160c98384a6e3cecfb
Joe Haddad <timer@powerbot.org>
2012-04-29 21:21:18 -0400

    Close skill guide.

--- src/org/powerbot/game/bot/randoms/WidgetCloser.java
+++ src/org/powerbot/game/bot/randoms/WidgetCloser.java
@@ -24,6 +24,7 @@ public class WidgetCloser extends AntiRandom {
 		children.put(906, 354);//membership offers
 		children.put(1252, 6);//Squeal of Fortune notification
 		children.put(1253, 76);//Squeal of Fortune window
+		children.put(1218, 77);//Advanced skill guide
 	}
 
 	public boolean validate() {
