412df4c8ea7bb29a33eeee2a722efcfb132e12a2
Joe Haddad <timer@powerbot.org>
2012-04-30 21:41:24 -0400

    Fixed small bug with getNearest.

--- wrappers/Area.java
+++ wrappers/Area.java
@@ -9,23 +9,24 @@ import org.powerbot.game.api.methods.interactive.Players;
 
 /**
  * A polygonal area of tiles.
+ *
  * @author Timer and Odell
  */
 public class Area {
 	protected final Polygon polygon;
 	protected int plane = -1;
-	private Tile[] taCache = null;
+	private Tile[] tileArrayCache = null;
 
 	/**
 	 * Constructs a rectangular area.
 	 */
 	public Area(final Tile t1, final Tile t2) {
-		this(new Tile[]{
+		this(
 				new Tile(Math.min(t1.getX(), t2.getX()), Math.min(t1.getY(), t2.getY()), t1.getPlane()),
 				new Tile(Math.max(t1.getX(), t2.getX()), Math.min(t1.getY(), t2.getY()), t1.getPlane()),
 				new Tile(Math.max(t1.getX(), t2.getX()), Math.max(t1.getY(), t2.getY()), t2.getPlane()),
 				new Tile(Math.min(t1.getX(), t2.getX()), Math.max(t1.getY(), t2.getY()), t2.getPlane())
-		});
+		);
 	}
 
 	/**
@@ -44,7 +45,7 @@ public class Area {
 
 	public void translate(final int x, final int y) {
 		polygon.translate(x, y);
-		taCache = null;
+		tileArrayCache = null;
 	}
 
 	/**
@@ -60,27 +61,30 @@ public class Area {
 	public int getPlane() {
 		return plane;
 	}
-	
+
 	/**
 	 * Adds a Tile to this Area.
+	 *
 	 * @param t The Tile to add.
 	 */
 	public void addTile(final Tile t) {
 		addTile(t.getX(), t.getY());
 	}
-	
+
 	/**
 	 * Adds a Tile to this Area.
+	 *
 	 * @param x The x coordinate.
 	 * @param y The y coordinate.
 	 */
 	public void addTile(final int x, final int y) {
 		polygon.addPoint(x, y);
-		taCache = null;
+		tileArrayCache = null;
 	}
-	
+
 	/**
 	 * Determines whether the given x,y pair is contained in the area.
+	 *
 	 * @param x The x coordinate.
 	 * @param y The y coordinate.
 	 * @return whether the area contains this pair.
@@ -88,9 +92,10 @@ public class Area {
 	public boolean contains(final int x, final int y) {
 		return polygon.contains(x, y);
 	}
-	
+
 	/**
 	 * Determines whether at least one of the given tiles is contained in this area.
+	 *
 	 * @param tiles The tiles to verify.
 	 * @return <tt>true</tt> if at least one of the tiles is contained, otherwise <tt>false</tt>.
 	 */
@@ -102,9 +107,10 @@ public class Area {
 		}
 		return false;
 	}
-	
+
 	/**
 	 * Determines whether all the given tiles are contained in this area.
+	 *
 	 * @param tiles The tiles to verify.
 	 * @return <tt>true</tt> if all tiles are contained, otherwise <tt>false</tt>.
 	 */
@@ -116,33 +122,35 @@ public class Area {
 		}
 		return true;
 	}
-	
+
 	/**
 	 * @return the averaged center tile of this area
 	 */
 	public Tile getCentralTile() {
 		return polygon.npoints > 0 ? new Tile((int) Math.round(avg(polygon.xpoints)), (int) Math.round(avg(polygon.ypoints)), plane) : null;
 	}
-	
+
 	/**
 	 * Finds the nearest tile in this area to the local player.
+	 *
 	 * @return the nearest tile contained in this area closest to the local player.
 	 */
 	public Tile getNearest() {
 		return getNearest(Players.getLocal().getLocation());
 	}
-	
+
 	/**
 	 * Finds the nearest tile in this area to the base tile.
+	 *
 	 * @param base The base tile to measure the closest tile to.
 	 * @return the nearest tile contained in this area closest to the base tile.
 	 */
-	public Tile getNearest(Tile base) {
-		Tile[] tiles = getTileArray();
+	public Tile getNearest(final Tile base) {
+		final Tile[] tiles = getTileArray();
 		Tile tile = null;
 		long dist = Long.MAX_VALUE, temp;
-		for (Tile t : tiles) {
-			temp = (long) Calculations.distanceTo(t);
+		for (final Tile t : tiles) {
+			temp = (long) Calculations.distanceTo(base);
 			if (t == null || temp < dist) {
 				dist = temp;
 				tile = t;
@@ -150,41 +158,44 @@ public class Area {
 		}
 		return tile;
 	}
-	
+
 	/**
 	 * @return the tiles backing this Area.
 	 */
 	public Tile[] getBoundingTiles() {
-		Tile[] bounding = new Tile[polygon.npoints];
+		final Tile[] bounding = new Tile[polygon.npoints];
 		for (int i = 0; i < polygon.npoints; i++) {
 			bounding[i] = new Tile(polygon.xpoints[i], polygon.ypoints[i], plane);
 		}
 		return bounding;
 	}
-	
+
 	/**
 	 * @return an array of all the contained tiles in this area.
 	 */
 	public Tile[] getTileArray() {
-		if (taCache == null) {
-			Rectangle bounds = getBounds();
-			ArrayList<Tile> tiles = new ArrayList<Tile>(bounds.width * bounds.height);
-			int xMax = bounds.x + bounds.width, yMax = bounds.y + bounds.height;
+		if (tileArrayCache == null) {
+			final Rectangle bounds = getBounds();
+			final ArrayList<Tile> tiles = new ArrayList<Tile>(bounds.width * bounds.height);
+			final int xMax = bounds.x + bounds.width, yMax = bounds.y + bounds.height;
 			for (int x = bounds.x; x < xMax; x++) {
 				for (int y = bounds.y; y < yMax; y++) {
-					if (contains(x, y))
+					if (contains(x, y)) {
 						tiles.add(new Tile(x, y, plane));
+					}
 				}
 			}
-			taCache = tiles.toArray(new Tile[tiles.size()]);
+			tileArrayCache = tiles.toArray(new Tile[tiles.size()]);
 		}
-		return taCache;
+		return tileArrayCache;
 	}
-	
+
 	private double avg(final int... nums) {
 		long total = 0;
-		for (int i : nums) total += (long) i;
+		for (int i : nums) {
+			total += (long) i;
+		}
 		return (double) total / (double) nums.length;
 	}
-	
+
 }
