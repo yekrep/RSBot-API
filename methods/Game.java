4d4d55bfabf5c6bb40f5e424a50f70a272396cd7
Joe Haddad <timer@powerbot.org>
2012-04-30 21:50:46 -0400

    Patched logout code.

--- methods/Game.java
+++ methods/Game.java
@@ -3,9 +3,11 @@ package org.powerbot.game.api.methods;
 import java.awt.Canvas;
 import java.awt.Dimension;
 
+import org.powerbot.game.api.util.Time;
 import org.powerbot.game.api.util.internal.Constants;
 import org.powerbot.game.api.util.internal.Multipliers;
 import org.powerbot.game.api.wrappers.Tile;
+import org.powerbot.game.api.wrappers.widget.WidgetChild;
 import org.powerbot.game.bot.Context;
 import org.powerbot.game.client.BaseInfoInts;
 import org.powerbot.game.client.BaseInfoX;
@@ -118,20 +120,19 @@ public class Game {
 	 * @param lobby <tt>true</tt> if the method should log out to the lobby, <tt>false</tt> if the method should fully log out.
 	 * @return <tt>true</tt> if and only if the client's state equals the state you want it to be in.
 	 */
-	public static boolean logout(boolean lobby) {
+	public static boolean logout(final boolean lobby) {
 		if ((Game.getClientState() == Game.INDEX_LOBBY_SCREEN && lobby) || (Game.getClientState() == Game.INDEX_LOGIN_SCREEN && !lobby)) {
 			return true;
-		} else if (Tabs.getCurrent().equals(Tabs.LOGOUT)) {
+		}
+		if (Tabs.LOGOUT.open()) {
 			final WidgetChild w = Widgets.get(182, lobby ? 6 : 13);
-			if (w != null && !w.validate() && w.interact("Exit to " + (lobby ? "Lobby" : "Login"))) {
+			if (w != null && w.validate() && w.interact("Exit to " + (lobby ? "Lobby" : "Login"))) {
 				for (int i = 0; i < 10; i++, Time.sleep(100, 200)) {
 					if ((Game.getClientState() == Game.INDEX_LOBBY_SCREEN && lobby) || (Game.getClientState() == Game.INDEX_LOGIN_SCREEN && !lobby)) {
 						return true;
 					}
 				}
 			}
-		} else {
-			Tabs.LOGOUT.open(true);
 		}
 		return false;
 	}
