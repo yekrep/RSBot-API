4513d8679e5647549b5d17950fb3b024fc6b1eb4
A. Ahmed <paris@powerbot.org>
2012-05-12 14:34:23 +0100

    Update secure store version.

--- src/org/powerbot/util/io/SecureStore.java
+++ src/org/powerbot/util/io/SecureStore.java
@@ -28,7 +28,7 @@ import org.powerbot.util.StringUtil;
 public final class SecureStore {
 	private final static Logger log = Logger.getLogger(SecureStore.class.getName());
 	private static SecureStore instance = null;
-	private final static int MAGIC = 0x00525354, VERSION = 1007, BLOCKSIZE = 512, MAXBLOCKS = 2048;
+	private final static int MAGIC = 0x00525354, VERSION = 1008, BLOCKSIZE = 512, MAXBLOCKS = 2048;
 	private final static String CIPHER_ALGORITHM = "RC4", KEY_ALGORITHM = "RC4";
 	private final File store;
 	private final Map<String, TarEntry> entries;
