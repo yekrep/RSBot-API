dbedad908838c206222875ac569b399ad6e60fc1
Joe Haddad <timer@powerbot.org>
2012-05-20 12:02:46 -0400

    Enable scene object definitions.

--- wrappers/node/SceneObject.java
+++ wrappers/node/SceneObject.java
@@ -15,10 +15,13 @@ import org.powerbot.game.api.wrappers.RegionOffset;
 import org.powerbot.game.api.wrappers.Tile;
 import org.powerbot.game.api.wrappers.graphics.CapturedModel;
 import org.powerbot.game.api.wrappers.graphics.model.SceneObjectModel;
+import org.powerbot.game.bot.Context;
 import org.powerbot.game.client.HardReference;
+import org.powerbot.game.client.HashTable;
 import org.powerbot.game.client.Model;
 import org.powerbot.game.client.Node;
 import org.powerbot.game.client.RSAnimable;
+import org.powerbot.game.client.RSInfo;
 import org.powerbot.game.client.RSInteractable;
 import org.powerbot.game.client.RSInteractableData;
 import org.powerbot.game.client.RSObject;
@@ -85,7 +88,8 @@ public class SceneObject implements Entity, Locatable, Identifiable {
 	}
 
 	public SceneObjectDefinition getDefinition() {
-		final Node ref = Nodes.lookup(null, getId());//TODO?
+		final HashTable rsObjectDefLoaders = (HashTable) ((RSInfo) Context.client().getRSGroundInfo()).getRSObjectDefLoaders();
+		final Node ref = Nodes.lookup(rsObjectDefLoaders, getId());
 		if (ref != null) {
 			if (ref instanceof HardReference) {
 				return new SceneObjectDefinition((RSObjectDef) ((HardReference) ref).get());
