9f1771b03882e3acddb3da8031d9aba57f4e0b48
A. Ahmed <paris@powerbot.org>
2012-05-21 01:05:52 +0100

    Version 4011.

--- src/org/powerbot/util/Configuration.java
+++ src/org/powerbot/util/Configuration.java
@@ -16,7 +16,7 @@ public class Configuration {
 	public static final boolean FROMJAR;
 	public static boolean DEVMODE = false;
 	public static final boolean SUPERDEV;
-	public static final int VERSION = 4010;
+	public static final int VERSION = 4011;
 	public static final String STORE;
 	public static final File LOCK;
 	public static final OperatingSystem OS;
