429f70f01258b46d64850e815660cf9403dc2b37
A. Ahmed <paris@powerbot.org>
2012-06-04 21:55:06 +0100

    Use temp files for UIDManager.

--- src/org/powerbot/game/bot/handler/UIDManager.java
+++ src/org/powerbot/game/bot/handler/UIDManager.java
@@ -1,13 +1,20 @@
 package org.powerbot.game.bot.handler;
 
-import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.security.GeneralSecurityException;
+import java.io.OutputStream;
 import java.util.HashMap;
+import java.util.zip.Adler32;
 
+import javax.crypto.Cipher;
+
+import org.powerbot.util.Configuration;
+import org.powerbot.util.StringUtil;
+import org.powerbot.util.io.CipherStreams;
 import org.powerbot.util.io.IOHelper;
-import org.powerbot.util.io.SecureStore;
 
 /**
  * @author Timer
@@ -16,15 +23,31 @@ public class UIDManager {
 	private static final String FILE_NAME = "gameuids.txt";
 	private static String newline = System.getProperty("line.separator");
 	private static String separator = "#";
+	private final String name;
+	private final File store;
+	private final byte[] key;
+	private final static String CIPHER_ALGORITHM = "RC4", KEY_ALGORITHM = "RC4";
 
 	private HashMap<String, byte[]> uids = new HashMap<String, byte[]>();
 	private String lastUsed = "";
 
 	public UIDManager() {
-		try {
-			final InputStream inputStream = SecureStore.getInstance().read(FILE_NAME);
-			if (inputStream != null) {
-				for (final String line : IOHelper.readString(inputStream).split("\n")) {
+		final Adler32 c = new Adler32();
+		c.update(new byte[] {0x1b, 0x22, 0xe, 0x19, 0x49});
+		final long uid = Configuration.getUID();
+		c.update((int) (uid & 0xffffffff));
+		c.update((int) (uid >> 32));
+		final long l = c.getValue();
+		name = String.format("%s.tmp", Long.toHexString(l));
+		store = new File(System.getProperty("java.io.tmpdir"), name);
+		key = CipherStreams.getSharedKey(StringUtil.getBytesUtf8(name));
+
+		InputStream fis = null;
+		if (store.isFile()) {
+			try {
+				fis = new FileInputStream(store);
+				final InputStream is = CipherStreams.getCipherInputStream(fis, Cipher.ENCRYPT_MODE, key, CIPHER_ALGORITHM, KEY_ALGORITHM);
+				for (final String line : IOHelper.readString(is).split("\n")) {
 					if (!line.isEmpty()) {
 						final String[] data = line.split(separator, 2);
 						if (data.length == 2) {
@@ -32,8 +55,15 @@ public class UIDManager {
 						}
 					}
 				}
+			} catch (final Exception ignored) {
+			} finally {
+				if (fis != null) {
+					try {
+						fis.close();
+					} catch (final IOException ignored2) {
+					}
+				}
 			}
-		} catch (final Throwable ignored) {
 		}
 	}
 
@@ -63,17 +93,24 @@ public class UIDManager {
 	}
 
 	public void save() throws IOException {
-		final ByteArrayOutputStream out = new ByteArrayOutputStream();
-		for (final String key : uids.keySet()) {
-			out.write(key.getBytes());
-			out.write(separator.getBytes());
-			out.write(uids.get(key));
-			out.write(newline.getBytes());
-		}
+		OutputStream fos = null;
 		try {
-			SecureStore.getInstance().write(FILE_NAME, out.toByteArray());
-		} catch (GeneralSecurityException ignored) {
-			throw new IOException("failed to write data");
+			fos = new FileOutputStream(store);
+			final OutputStream os = CipherStreams.getCipherOutputStream(fos, Cipher.DECRYPT_MODE, key, CIPHER_ALGORITHM, KEY_ALGORITHM);
+			for (final String key : uids.keySet()) {
+				os.write(key.getBytes());
+				os.write(separator.getBytes());
+				os.write(uids.get(key));
+				os.write(newline.getBytes());
+			}
+		} catch (final Exception ignored) {
+		} finally {
+			if (fos != null) {
+				try {
+					fos.close();
+				} catch (final IOException ignored2) {
+				}
+			}
 		}
 	}
 }
