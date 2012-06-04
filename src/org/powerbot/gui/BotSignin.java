9c6ae08bce3949773c65bd0a9762cda32f89d19e
A. Ahmed <paris@powerbot.org>
2012-06-04 22:31:28 +0100

    We're not on the cloud just yet.

--- src/org/powerbot/gui/BotSignin.java
+++ src/org/powerbot/gui/BotSignin.java
@@ -94,7 +94,7 @@ public final class BotSignin extends JDialog implements ActionListener {
 
 		info.setHorizontalAlignment(SwingConstants.CENTER);
 		info.setText("<html><center>Sign in to your " + BotLocale.WEBSITE +
-				" account to access your script collection and synchronise your accounts securely on the " + BotLocale.THECLOUD + "</center></html>");
+				" account to access your script collection and (soon) synchronise your accounts securely on the " + BotLocale.THECLOUD + "</center></html>");
 		info.setHorizontalAlignment(SwingConstants.CENTER);
 		info.setOpaque(true);
 		info.setBackground(null);
