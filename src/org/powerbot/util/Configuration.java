845fc5b5ac674f6ca50a9172299ace56bae94229
A. Ahmed <paris@powerbot.org>
2012-06-05 15:16:05 +0100

    Prematurely bump version number so server responds with correct information for script collections.

--- src/org/powerbot/util/Configuration.java
+++ src/org/powerbot/util/Configuration.java
@@ -16,7 +16,7 @@ public class Configuration {
 	public static final boolean FROMJAR;
 	public static boolean DEVMODE = false;
 	public static final boolean SUPERDEV;
-	public static final int VERSION = 4013;
+	public static final int VERSION = 4014;
 	public static final File LOCK;
 	public static final OperatingSystem OS;
 
