b39ef2b4c3c7f322eaeba72b3c6116ead487e715
A. Ahmed <paris@powerbot.org>
2012-07-02 22:20:59 +0100

    Version 4020.

--- src/org/powerbot/util/Configuration.java
+++ src/org/powerbot/util/Configuration.java
@@ -16,7 +16,7 @@ public class Configuration {
 	public static final boolean FROMJAR;
 	public static boolean DEVMODE = false;
 	public static final boolean SUPERDEV;
-	public static final int VERSION = 4019;
+	public static final int VERSION = 4020;
 	public static final File LOCK;
 	public static final OperatingSystem OS;
 
