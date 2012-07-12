2b0cd0b048c92ef08fe12d7a823e5ef355f778b3
A. Ahmed <paris@powerbot.org>
2012-07-12 20:32:38 +0100

    Version 4023.

--- src/org/powerbot/util/Configuration.java
+++ src/org/powerbot/util/Configuration.java
@@ -16,7 +16,7 @@ public class Configuration {
 	public static final boolean FROMJAR;
 	public static boolean DEVMODE = false;
 	public static final boolean SUPERDEV;
-	public static final int VERSION = 4022;
+	public static final int VERSION = 4023;
 	public static final File LOCK;
 	public static final OperatingSystem OS;
 
