e2011b7abf162ce11575a78347f258ca47aeb8d7
Joe Haddad <timer@powerbot.org>
2012-07-31 15:37:29 -0400

    Adjust line y position (+50).

--- src/org/powerbot/util/StringUtil.java
+++ src/org/powerbot/util/StringUtil.java
@@ -19,7 +19,7 @@ public class StringUtil {
 	 */
 	public static void drawLine(final Graphics render, final int row, final String text) {
 		final int height = render.getFontMetrics().getHeight() + 4;
-		final int x = 7, y = row * height + height + 19;
+		final int x = 7, y = row * height + height + 19 + 50;
 		render.setColor(Color.GREEN);
 		render.drawString(text, x, y);
 	}
