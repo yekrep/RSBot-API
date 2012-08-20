74dfb6c86ae82f8f8c9ebe113d6e8563c87bb598
Joe Haddad <timer@powerbot.org>
2012-08-20 00:12:42 -0400

    Document a node.

--- src/org/powerbot/core/script/job/state/Node.java
+++ src/org/powerbot/core/script/job/state/Node.java
@@ -3,8 +3,18 @@ package org.powerbot.core.script.job.state;
 import org.powerbot.core.concurrent.Task;
 
 /**
+ * A {@link Node} is an {@link Task} in which has an activation method ({@link org.powerbot.core.script.job.state.Node#activate()}).
+ * A {@link Node} is generally deployed within a state environment (see {@link Tree}).
+ * <p/>
+ * The {@link Node} is not checked for activation when submitted.
+ *
  * @author Timer
  */
 public abstract class Node extends Task {
+	/**
+	 * Determines whether or not to execute this {@link Node}.
+	 *
+	 * @return <tt>true</tt> to activate the {@link Node}; otherwise, <tt>false</tt>.
+	 */
 	public abstract boolean activate();
 }
