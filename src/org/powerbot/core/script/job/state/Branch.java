6bd0c605fcaefd9ed46ca8946723b3aeb4fa9e19
Joe Haddad <timer@powerbot.org>
2012-08-20 00:12:51 -0400

    Document a branch.

--- src/org/powerbot/core/script/job/state/Branch.java
+++ src/org/powerbot/core/script/job/state/Branch.java
@@ -6,6 +6,9 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 import java.util.concurrent.atomic.AtomicReference;
 
 /**
+ * A {@link Branch} is a {@link Node} which activates multiple children node when submitted, typically after activation ({@link org.powerbot.core.script.job.state.Branch#branch()}).
+ * Activation of this {@link Branch} requires one {@link Node} to be considered active ({@link org.powerbot.core.script.job.state.Node#activate()}) in addition to {@link org.powerbot.core.script.job.state.Branch#branch()}.
+ *
  * @author Timer
  */
 public abstract class Branch extends Node {
@@ -13,13 +16,26 @@ public abstract class Branch extends Node {
 
 	private final AtomicReference<Node> current_node = new AtomicReference<>();
 
+	/**
+	 * Constructs a {@link Branch}, which is a {@link Node} which activates a series of {@link Node}s when activated via {@link org.powerbot.core.script.job.state.Branch#branch()}.
+	 *
+	 * @param nodes The children {@link Node}s to activate when this {@link Branch} activates ({@link org.powerbot.core.script.job.state.Branch#branch()}).
+	 */
 	public Branch(final Node[] nodes) {
 		this.nodes = new ConcurrentLinkedQueue<>();
 		this.nodes.addAll(Arrays.asList(nodes));
 	}
 
+	/**
+	 * Determines whether or not this branch should activate.
+	 *
+	 * @return <tt>true</tt> to activate the {@link Branch}; otherwise, <tt>false</tt>.
+	 */
 	public abstract boolean branch();
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public final boolean activate() {
 		if (branch()) {
@@ -33,6 +49,10 @@ public abstract class Branch extends Node {
 		return false;
 	}
 
+	/**
+	 * Executes the children {@link Node}s.
+	 * This method will return if there is already a {@link Node} being processed under this {@link Branch}.
+	 */
 	@Override
 	public final void execute() {
 		final Node running_node = current_node.get();
