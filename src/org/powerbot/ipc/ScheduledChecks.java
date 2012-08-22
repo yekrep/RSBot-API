9ff6684f2d3883b8880e0cfd71d424191a4ef6a0
A. Ahmed <paris@powerbot.org>
2012-08-23 00:09:04 +0100

    Timeout local scripts for non-developers.

--- src/org/powerbot/ipc/ScheduledChecks.java
+++ src/org/powerbot/ipc/ScheduledChecks.java
@@ -2,14 +2,19 @@ package org.powerbot.ipc;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.logging.Logger;
 
+import org.powerbot.game.api.ActiveScript;
+import org.powerbot.game.bot.Bot;
 import org.powerbot.service.NetworkAccount;
 
 /**
  * @author Paris
  */
 public final class ScheduledChecks implements ActionListener {
+	private final static Logger log = Logger.getLogger(ScheduledChecks.class.getName());
 	public static volatile long SESSION_TIME = 0;
+	public static final int LOCALSCRIPT_TIMEOUT = 60;
 
 	@Override
 	public void actionPerformed(final ActionEvent e) {
@@ -22,6 +27,14 @@ public final class ScheduledChecks implements ActionListener {
 			if (a < System.currentTimeMillis() - 1000 * 60 * 10) {
 				SESSION_TIME = System.currentTimeMillis();
 				NetworkAccount.getInstance().session(0);
+			}
+		}
+
+		if (Bot.isInstantiated() && Bot.getInstance().getActiveScript() != null) {
+			final ActiveScript script = Bot.getInstance().getActiveScript();
+			if (script.getDefinition().local && !NetworkAccount.getInstance().isDeveloper() && script.started < System.currentTimeMillis() - 1000 * LOCALSCRIPT_TIMEOUT && !script.getContainer().isShutdown()) {
+				log.info("Local script stopped after timeout - not authorised developer");
+				script.kill();
 			}
 		}
 	}
