45cb55ab10b4c37da733dfa4702290a941507608
Joe Haddad <timer@powerbot.org>
2012-09-03 14:29:28 -0400

    confirm destroy of ticket

--- src/org/powerbot/game/bot/randoms/SpinTickets.java
+++ src/org/powerbot/game/bot/randoms/SpinTickets.java
@@ -4,11 +4,13 @@ import org.powerbot.game.api.AntiRandom;
 import org.powerbot.game.api.Manifest;
 import org.powerbot.game.api.methods.Game;
 import org.powerbot.game.api.methods.Tabs;
+import org.powerbot.game.api.methods.Widgets;
 import org.powerbot.game.api.methods.tab.Inventory;
 import org.powerbot.game.api.util.Random;
 import org.powerbot.game.api.util.Time;
 import org.powerbot.game.api.util.Timer;
 import org.powerbot.game.api.wrappers.node.Item;
+import org.powerbot.game.api.wrappers.widget.Widget;
 import org.powerbot.game.api.wrappers.widget.WidgetChild;
 
 @Manifest(name = "Squeal of Fortune Ticket Claim", authors = {"Timer"})
@@ -33,8 +35,28 @@ public class SpinTickets extends AntiRandom {
 			final WidgetChild item = ticket.getWidgetChild();
 			if (item.interact("Destroy")) {
 				final Timer timer = new Timer(Random.nextInt(2000, 3500));
-				while (timer.isRunning() && item.validate()) {
-					Time.sleep(Random.nextInt(100, 300));
+				while (timer.isRunning() && !Widgets.get(1183).validate()) {
+					Time.sleep(150);
+				}
+				final Widget chat = Widgets.get(1183);
+				if (chat.validate()) {
+					WidgetChild destroy = null;
+					for (final WidgetChild child : chat.getChildren()) {
+						final String toolTip;
+						if ((toolTip = child.getTooltip()) != null && toolTip.trim().equalsIgnoreCase("Destroy") && child.visible()) {
+							destroy = child;
+							break;
+						}
+					}
+					if (destroy == null) {
+						return;
+					}
+					if (destroy.interact("Destroy")) {
+						timer.reset();
+						while (timer.isRunning() && item.validate()) {
+							Time.sleep(Random.nextInt(100, 300));
+						}
+					}
 				}
 			}
 		}
