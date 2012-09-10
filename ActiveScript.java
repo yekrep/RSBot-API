9380f701ba105ea598a844c0d2ae2a2a30601135
Joe Haddad <timer@powerbot.org>
2012-09-10 19:02:38 -0400

    Remove strategy daemon and utilize 4.1 loop.

--- ActiveScript.java
+++ ActiveScript.java
@@ -2,74 +2,43 @@ package org.powerbot.game.api;
 
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.EventListener;
+import java.util.LinkedList;
 import java.util.List;
-import java.util.concurrent.Future;
-import java.util.concurrent.SynchronousQueue;
-import java.util.concurrent.ThreadPoolExecutor;
-import java.util.concurrent.TimeUnit;
 
 import org.powerbot.concurrent.LoopTask;
-import org.powerbot.concurrent.Processor;
-import org.powerbot.concurrent.ThreadPool;
-import org.powerbot.concurrent.strategy.DaemonState;
 import org.powerbot.concurrent.strategy.Strategy;
-import org.powerbot.concurrent.strategy.StrategyDaemon;
 import org.powerbot.concurrent.strategy.StrategyGroup;
+import org.powerbot.core.script.job.Job;
 import org.powerbot.core.script.job.Task;
-import org.powerbot.event.EventManager;
-import org.powerbot.game.bot.Context;
+import org.powerbot.game.api.util.Time;
+import org.powerbot.util.Configuration;
 
 /**
  * @author Timer
  */
 @Deprecated
-public abstract class ActiveScript extends org.powerbot.core.script.ActiveScript implements Processor {
+public abstract class ActiveScript extends org.powerbot.core.script.ActiveScript {
 	public final long started;
-
-	private EventManager eventManager;
-	private ThreadPoolExecutor executor;
-	private StrategyDaemon strategyDaemon;
 	private final List<LoopTask> loopTasks;
-	private final List<EventListener> listeners;
-
-	private Context context;
+	private final List<Strategy> strategies;
+	private int iterationSleep = 200;
 
 	public ActiveScript() {
 		started = System.currentTimeMillis();
-		eventManager = null;
-		executor = null;
-		strategyDaemon = null;
 		loopTasks = Collections.synchronizedList(new ArrayList<LoopTask>());
-		listeners = Collections.synchronizedList(new ArrayList<EventListener>());
+		strategies = Collections.synchronizedList(new ArrayList<Strategy>());
 
 		getStartupJobs().add(new Task() {
 			@Override
 			public void execute() {
 				setup();
-				resume();
-				if (context != null) {
-					context.ensureAntiRandoms();
-				}
 			}
 		});
 	}
 
-	public final void init(final Context context) {
-		this.context = context;
-		eventManager = context.getEventManager();
-		executor = new ThreadPoolExecutor(1, Integer.MAX_VALUE, 60, TimeUnit.HOURS, new SynchronousQueue<Runnable>(), new ThreadPool(context.getThreadGroup()), new ThreadPoolExecutor.CallerRunsPolicy());
-		strategyDaemon = new StrategyDaemon(executor, context.getContainer());
-	}
-
 	public final void provide(final Strategy strategy) {
-		strategyDaemon.append(strategy);
-
-		if (!listeners.contains(strategy)) {
-			listeners.add(strategy);
-			if (!isLocked()) {
-				eventManager.accept(strategy);
-			}
+		if (!strategies.contains(strategy)) {
+			strategies.add(strategy);
 		}
 	}
 
@@ -80,10 +49,7 @@ public abstract class ActiveScript extends org.powerbot.core.script.ActiveScript
 	}
 
 	public final void revoke(final Strategy strategy) {
-		strategyDaemon.omit(strategy);
-
-		listeners.remove(strategy);
-		eventManager.remove(strategy);
+		strategies.remove(strategy);
 	}
 
 	public final void revoke(final StrategyGroup group) {
@@ -92,129 +58,109 @@ public abstract class ActiveScript extends org.powerbot.core.script.ActiveScript
 		}
 	}
 
-	public final Future<?> submit(final Runnable task) {
-		return executor.submit(task);
+	public final Job submit(final Runnable task) {
+		final Task job = new Task() {
+			@Override
+			public void execute() {
+				task.run();
+			}
+		};
+		getContainer().submit(job);
+		return job;
 	}
 
-	public final boolean submit(final LoopTask loopTask) {
+	public final org.powerbot.core.script.job.LoopTask submit(final LoopTask loopTask) {
 		if (loopTasks.contains(loopTask)) {
-			return false;
+			return null;
 		}
 
-		loopTask.init(this);
-		loopTask.start();
-		loopTasks.add(loopTask);
-		listeners.add(loopTask);
-		eventManager.accept(loopTask);
-		executor.submit(loopTask);
-		return true;
-	}
-
-	public final void terminated(final Runnable task) {
-		if (task instanceof LoopTask) {
-			final LoopTask loopTask = (LoopTask) task;
-			listeners.remove(loopTask);
-			eventManager.remove(loopTask);
-		}
+		final org.powerbot.core.script.job.LoopTask task = new org.powerbot.core.script.job.LoopTask() {
+			@Override
+			public int loop() {
+				return loopTask.loop();//TODO remove from loopTasks when completed
+			}
+		};
+		getContainer().submit(task);
+		return task;
 	}
 
 	public final void setIterationDelay(final int milliseconds) {
-		strategyDaemon.setIterationSleep(milliseconds);
+		iterationSleep = milliseconds;
 	}
 
 	protected abstract void setup();
 
 	@Override
 	public int loop() {
-		return 2000;
-	}
+		try {
+			final List<Job> jobs = new LinkedList<>();
 
-	public final void resume() {
-		eventManager.accept(ActiveScript.this);
-		final List<LoopTask> cache_list = new ArrayList<>();
-		cache_list.addAll(loopTasks);
-		for (final LoopTask task : cache_list) {
-			if (task.isKilled()) {
-				loopTasks.remove(task);
-				continue;
-			}
-
-			if (!task.isRunning()) {
-				task.start();
-				executor.submit(task);
-			}
-		}
-		for (final EventListener eventListener : listeners) {
-			eventManager.accept(eventListener);
-		}
-		strategyDaemon.listen();
-	}
-
-	public final void pause() {
-		pause(false);
-	}
-
-	public final void pause(final boolean removeListener) {
-		strategyDaemon.lock();
-		for (final LoopTask task : loopTasks) {
-			task.stop();
-		}
-		if (removeListener) {
-			eventManager.remove(ActiveScript.this);
-			for (final EventListener eventListener : listeners) {
-				eventManager.remove(eventListener);
-			}
-		}
-	}
-
-	public final void askStop() {
-		if (!executor.isShutdown()) {
-			executor.submit(new Runnable() {
-				public void run() {
-					onStop();
+			final List<Strategy> strategies_clone = new ArrayList<Strategy>();
+			strategies_clone.addAll(strategies);
+			for (final Strategy strategy : strategies_clone) {
+				if (getContainer().isPaused()) {
+					break;
 				}
-			});
+				if (strategy.tasks == null || !strategy.validate() ||
+						(strategy.sync && !strategy.isIdle())) {
+					continue;
+				}
+				for (final Runnable task : strategy.tasks) {
+					final Job job = new Task() {
+						@Override
+						public void execute() {
+							task.run();
+						}
+					};
+					getContainer().submit(job);
+					jobs.add(job);
+				}
+				strategy.executingJobs = jobs.toArray(new Job[jobs.size()]);
+				if (strategy.lock) {
+					final Object lock = new Object();
+					getContainer().submit(createFutureDisposer(jobs, lock));
+					awaitNotify(jobs, lock);
+				}
+				if (strategy.reset) {
+					break;
+				}
+			}
+			Task.sleep(iterationSleep);
+		} catch (final Throwable t) {
+			if (Configuration.DEVMODE) {
+				t.printStackTrace();
+			}
 		}
-		eventManager.remove(ActiveScript.this);
-		for (final LoopTask task : loopTasks) {
-			task.stop();
-			task.kill();
+		return 0;
+	}
+
+	private Job createFutureDisposer(final List<Job> jobs, final Object threadObject) {
+		return new Task() {
+			public void execute() {
+				while (jobs.size() > 0) {
+					final Job job = jobs.get(0);
+					job.join();
+
+					if (!job.isAlive()) {
+						jobs.remove(0);
+					}
+					Task.sleep(0);
+				}
+				synchronized (threadObject) {
+					threadObject.notify();
+				}
+			}
+		};
+	}
+
+	private void awaitNotify(final List<Job> jobs, final Object threadObject) {
+		synchronized (threadObject) {
+			if (jobs.size() > 0) {
+				try {
+					wait();
+				} catch (final InterruptedException ignored) {
+				}
+			}
 		}
-		loopTasks.clear();
-		for (final EventListener eventListener : listeners) {
-			eventManager.remove(eventListener);
-		}
-		listeners.clear();
-		strategyDaemon.destroy();
-		executor.shutdown();
-
-		final String name = Thread.currentThread().getThreadGroup().getName();
-		if (name.startsWith("GameDefinition-") ||
-				name.startsWith("ThreadPool-")) {
-			context.updateControls();
-		}
-	}
-
-	public void onStop() {
-	}
-
-	public final void kill() {
-		executor.shutdownNow();
-	}
-
-	public final DaemonState getState() {
-		return strategyDaemon.state;
-	}
-
-	public final boolean isRunning() {
-		return getState() != DaemonState.DESTROYED;
-	}
-
-	public final boolean isLocked() {
-		return getState() == DaemonState.LOCKED;
-	}
-
-	public final ThreadPoolExecutor getStrategyDaemon() {
-		return executor;
 	}
 }
