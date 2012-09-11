9d97c1b33c6f52c1e6425612df3554fcf6604202
Joe Haddad <timer@powerbot.org>
2012-09-10 22:39:01 -0400

    Remove debugging.

--- src/org/powerbot/gui/component/BotToolBar.java
+++ src/org/powerbot/gui/component/BotToolBar.java
@@ -262,7 +262,6 @@ public final class BotToolBar extends JToolBar implements ActionListener {
 			final ScriptHandler script = bot.getScriptHandler();
 			final boolean active = script != null && script.isActive();
 			final boolean running = active && !script.isPaused();
-			System.out.println(active + " " + running);
 			scriptPlay.setIcon(new ImageIcon(Resources.getImage(running ? Resources.Paths.PAUSE : Resources.Paths.PLAY)));
 			scriptPlay.setToolTipText(running ? BotLocale.PAUSESCRIPT : active ? BotLocale.RESUMESCRIPT : BotLocale.PLAYSCRIPT);
 			scriptStop.setEnabled(active);
