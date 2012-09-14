b4603e1cc43919a07954851aa72c53d41578f42b
Joe Haddad <timer@powerbot.org>
2012-09-13 21:06:11 -0400

    Ensure random events time out after 4-5 minutes.

--- src/org/powerbot/core/bot/handler/RandomHandler.java
+++ src/org/powerbot/core/bot/handler/RandomHandler.java
@@ -27,6 +27,9 @@ import org.powerbot.core.script.random.SandwichLady;
 import org.powerbot.core.script.random.ScapeRune;
 import org.powerbot.core.script.random.SpinTickets;
 import org.powerbot.core.script.random.WidgetCloser;
+import org.powerbot.game.api.Manifest;
+import org.powerbot.game.api.util.Random;
+import org.powerbot.game.api.util.Timer;
 import org.powerbot.game.bot.Bot;
 
 /**
@@ -37,7 +40,10 @@ public class RandomHandler extends LoopTask {
 
 	private final AntiRandom[] antiRandoms;
 	private final ScriptHandler handler;
+
 	private AntiRandom running;
+	private Manifest manifest;
+	private final Timer timeout;
 
 	public RandomHandler(final Bot bot, final ScriptHandler scriptHandler) {
 		antiRandoms = new AntiRandom[]{
@@ -70,18 +76,31 @@ public class RandomHandler extends LoopTask {
 		}
 		handler = scriptHandler;
 		running = null;
+		timeout = new Timer(0);
 	}
 
 	@Override
 	public int loop() {
 		if (running != null && !running.activate()) {
 			running = null;
+			if (manifest != null) {
+				log.info("Random solver completed: " + manifest.name());
+			}
+
 			handler.resume();
 		}
 		if (running == null) {
 			for (final AntiRandom random : antiRandoms) {
 				if (random.activate()) {
 					running = random;
+					timeout.setEndIn(Random.nextInt(240, 300) * 1000);
+
+					final Class<?> clazz = running.getClass();
+					manifest = clazz.isAnnotationPresent(Manifest.class) ? clazz.getAnnotation(Manifest.class) : null;
+					if (manifest != null) {
+						log.info("Random solver started: " + manifest.name());
+					}
+
 					handler.pause();
 					break;
 				}
@@ -89,6 +108,15 @@ public class RandomHandler extends LoopTask {
 		}
 
 		if (running != null) {
+			if (!timeout.isRunning()) {
+				handler.stop();
+
+				if (manifest != null) {
+					log.info("Random solver failed: " + manifest.name());
+				}
+				return -1;
+			}
+
 			running.execute();
 			return 0;
 		}
