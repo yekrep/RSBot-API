abcd657c85f7f41a1b453d2cfa583d24694d647c
A. Ahmed <paris@powerbot.org>
2013-01-01 21:55:41 +0000

    New client pack upload system.

--- src/org/powerbot/game/loader/ClientLoader.java
+++ src/org/powerbot/game/loader/ClientLoader.java
@@ -1,7 +1,8 @@
 package org.powerbot.game.loader;
 
 import java.io.IOException;
-import java.net.SocketTimeoutException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.HashMap;
@@ -9,6 +10,7 @@ import java.util.Map;
 import java.util.concurrent.Callable;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.zip.GZIPOutputStream;
 
 import org.powerbot.asm.NodeManipulator;
 import org.powerbot.core.bot.Bot;
@@ -81,9 +83,18 @@ public class ClientLoader implements Callable<Boolean> {
 				return false;
 			}
 			if (this.classes.size() > 0) {
-				NodeManipulator nodeManipulator;
+				NodeManipulator nodeManipulator = null;
 				try {
-					nodeManipulator = getNodeManipulator();
+					while (true) {
+						try {
+							nodeManipulator = getNodeManipulator(this.classes);
+							break;
+						} catch (final PendingException p) {
+							int d = p.getDelay() / 1000;
+							log.warning("Request pending, trying again in " + (d < 60 ? d + " seconds" : (int) Math.ceil(d / 60) + " minutes"));
+							Thread.sleep(p.getDelay());
+						}
+					}
 				} catch (final Throwable e) {
 					log.log(Level.FINE, "Failed to load manipulator: ", e);
 					return false;
@@ -129,20 +140,86 @@ public class ClientLoader implements Callable<Boolean> {
 		return null;
 	}
 
-	public NodeManipulator getNodeManipulator() throws AdaptException {
+	public NodeManipulator getNodeManipulator(final Map<String, byte[]> classes) throws AdaptException, IOException, PendingException {
 		final String packHash = getHash();
+		final int delay = 1000 * 60 * 3 + 30;
 
-		final String id = "(" + packHash.substring(0, 6) + ")";
-		log.info("Loading client patch " + id);
-		try {
-			return Bot.getInstance().modScript = new ModScript(IOHelper.read(HttpClient.openStream(new URL(String.format(Configuration.URLs.CLIENTPATCH, packHash)))));
-		} catch (final SocketTimeoutException ignored) {
-			log.severe("Cannot connect to update server " + id);
-		} catch (final NullPointerException ignored) {
-			log.severe("Error parsing client patch " + id);
-		} catch (final IOException e) {
-			log.log(Level.SEVERE, "Client patch " + id + " unavailable", "Outdated");
+		final HttpURLConnection con = HttpClient.getHttpConnection(new URL(String.format(Configuration.URLs.CLIENTPATCH, packHash)));
+		con.setInstanceFollowRedirects(false);
+		con.connect();
+		if (con.getResponseCode() == HttpURLConnection.HTTP_OK) {
+			return Bot.getInstance().modScript = new ModScript(IOHelper.read(HttpClient.getInputStream(con)));
+		} else {
+			final HttpURLConnection bucket = HttpClient.getHttpConnection(new URL(String.format(Configuration.URLs.CLIENTBUCKET, packHash)));
+			bucket.setInstanceFollowRedirects(false);
+			bucket.connect();
+			switch (bucket.getResponseCode()) {
+			case HttpURLConnection.HTTP_SEE_OTHER:
+				final String dest = bucket.getHeaderField("Location");
+				log.info("Updating client pack, please do not disconnect or close");
+				final HttpURLConnection put = HttpClient.getHttpConnection(new URL(dest));
+				put.setRequestMethod("PUT");
+				put.setDoOutput(true);
+				final OutputStream out = new GZIPOutputStream(put.getOutputStream());
+				writePack(classes, out);
+				out.flush();
+				out.close();
+				final int r = put.getResponseCode();
+				put.disconnect();
+				if (r == HttpURLConnection.HTTP_OK) {
+					final HttpURLConnection bucket_notify = HttpClient.getHttpConnection(new URL(String.format(Configuration.URLs.CLIENTBUCKET, packHash)));
+					bucket_notify.setRequestMethod("PUT");
+					bucket_notify.connect();
+					final int r_notify = bucket_notify.getResponseCode();
+					bucket_notify.disconnect();
+					if (r_notify == HttpURLConnection.HTTP_OK || r_notify == HttpURLConnection.HTTP_ACCEPTED) {
+						throw new PendingException(delay * 2);
+					} else {
+						throw new AdaptException("Failed to load node manipulator; after reupload");
+					}
+				} else {
+					throw new AdaptException("Failed to load node manipulator");
+				}
+			case HttpURLConnection.HTTP_ACCEPTED:
+				throw new PendingException(delay);
+			}
 		}
-		throw new AdaptException("Failed to load node manipulator; unable to reach server or client unsupported");
+
+		return null;
+	}
+
+	private void writePack(final Map<String, byte[]> classes, final OutputStream out) throws IOException {
+		final int MAGIC = 0xC1A5700F, END_OF_FILE = 0x1;
+		writeInt(MAGIC, out);
+
+		synchronized (classes) {
+			for (final byte[] data : classes.values()) {
+				writeInt(data.length, out);
+				out.write(data);
+			}
+		}
+
+		writeInt(END_OF_FILE, out);
+	}
+
+	private void writeInt(final int v, final OutputStream o) throws IOException {
+		final int m = 0xff;
+		o.write(v >>> 24);
+		o.write(v >>> 16 & m);
+		o.write(v >>> 8 & m);
+		o.write(v & m);
+	}
+
+	private final class PendingException extends Exception {
+		private static final long serialVersionUID = -6937383190630297216L;
+		private final int delay;
+
+		public PendingException(final int delay) {
+			this.delay = delay;
+		}
+
+		public int getDelay() {
+			return delay;
+		}
 	}
 }
