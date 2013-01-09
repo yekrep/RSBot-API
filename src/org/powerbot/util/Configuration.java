7fc7947dde34f1dc3e0250b3279378c3c82d9d38
A. Ahmed <paris@powerbot.org>
2013-01-09 21:54:27 +0000

    Version 4047.

--- src/org/powerbot/util/Configuration.java
+++ src/org/powerbot/util/Configuration.java
@@ -19,7 +19,7 @@ public class Configuration {
 	public static final boolean FROMJAR;
 	public static boolean DEVMODE = false;
 	public static final boolean SUPERDEV;
-	public static final int VERSION = 4046;
+	public static final int VERSION = 4047;
 	public static volatile int VERSION_LATEST = -1;
 	public static final OperatingSystem OS;
 
