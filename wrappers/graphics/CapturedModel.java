80d2cc16064444f0dee2b49d8a370dee4dd72fbe
Joe Haddad <timer@powerbot.org>
2013-01-28 01:43:23 -0500

    optimize model code

--- wrappers/graphics/CapturedModel.java
+++ wrappers/graphics/CapturedModel.java
@@ -10,14 +10,13 @@ import org.powerbot.game.api.methods.Calculations;
 import org.powerbot.game.api.util.Filter;
 import org.powerbot.game.api.util.Random;
 import org.powerbot.game.bot.Context;
-import org.powerbot.game.client.Model;
 import org.powerbot.game.client.ModelCapture;
 
 public abstract class CapturedModel {
-	public static Filter<CapturedModel> newVertexFilter(final short[] vertex_a) {
+	public static Filter<CapturedModel> newVertexFilter(final short[] faceA) {
 		return new Filter<CapturedModel>() {
 			public boolean accept(final CapturedModel m) {
-				return Arrays.equals(m.indices1, vertex_a);
+				return Arrays.equals(m.faceA, faceA);
 			}
 		};
 	}
@@ -26,26 +25,27 @@ public abstract class CapturedModel {
 	protected int[] yPoints;
 	protected int[] zPoints;
 
-	protected short[] indices1;
-	protected short[] indices2;
-	protected short[] indices3;
+	protected short[] faceA;
+	protected short[] faceB;
+	protected short[] faceC;
 
 	protected int numVertices;
 	protected int numFaces;
 
-	public CapturedModel(final Model model) {
+	public CapturedModel(final org.powerbot.game.client.Model model) {
 		xPoints = model.getXPoints();
 		yPoints = model.getYPoints();
 		zPoints = model.getZPoints();
-		indices1 = model.getIndices1();
-		indices2 = model.getIndices2();
-		indices3 = model.getIndices3();
+		faceA = model.getIndices1();
+		faceB = model.getIndices2();
+		faceC = model.getIndices3();
+
 		if (model instanceof ModelCapture) {
 			numVertices = ((ModelCapture) model).getNumVertices();
 			numFaces = ((ModelCapture) model).getNumFaces();
 		} else {
 			numVertices = Math.min(xPoints.length, Math.min(yPoints.length, zPoints.length));
-			numFaces = Math.min(indices1.length, Math.min(indices2.length, indices3.length));
+			numFaces = Math.min(faceA.length, Math.min(faceB.length, faceC.length));
 		}
 	}
 
@@ -55,10 +55,55 @@ public abstract class CapturedModel {
 
 	protected abstract void update();
 
+	@Deprecated
+	public boolean isOnScreen() {
+		return nextTriangle() != -1;
+	}
+
+	public int nextTriangle() {
+		update();
+		final int mark = Random.nextInt(0, numFaces);
+		int index = firstOnScreenIndex(mark, numFaces);
+		return index != -1 ? index : firstOnScreenIndex(0, mark);
+	}
+
+	public Point getCentroid(final int index) {
+		if (index < 0 || index >= numFaces) return null;
+		update();
+		final int x = getLocalX();
+		final int y = getLocalY();
+		final int height = Calculations.calculateTileHeight(x, y);
+		final Point localPoint = Calculations.worldToScreen(
+				x + (this.xPoints[this.faceA[index]] + this.xPoints[this.faceB[index]] + this.xPoints[this.faceC[index]]) / 3,
+				height + (this.yPoints[this.faceA[index]] + this.yPoints[this.faceB[index]] + this.yPoints[this.faceC[index]]) / 3,
+				y + (this.zPoints[this.faceA[index]] + this.zPoints[this.faceB[index]] + this.zPoints[this.faceC[index]]) / 3
+		);
+		return Calculations.isOnScreen(localPoint) ? localPoint : null;
+	}
+
+	@Deprecated
+	public Point getNextViewportPoint() {
+		return getNextPoint();
+	}
+
+	public Point getNextPoint() {
+		update();
+		final int mark = Random.nextInt(0, numFaces);
+		Point point = firstOnScreenCentroid(mark, numFaces);
+		return point != null ? point : (point = firstOnScreenCentroid(0, mark)) != null ? point : new Point(-1, -1);
+	}
+
+	@Deprecated
 	public Point getCentralPoint() {
+		return getCenterPoint();
+	}
+
+	public Point getCenterPoint() {
 		if (numFaces < 1) {
 			return new Point(-1, -1);
 		}
+		update();
+
 		int totalXAverage = 0;
 		int totalYAverage = 0;
 		int totalHeightAverage = 0;
@@ -68,17 +113,10 @@ public abstract class CapturedModel {
 		final int y = getLocalY();
 		final int height = Calculations.calculateTileHeight(x, y);
 
-		final int[][] points = projectVertices();
-
 		while (index < numFaces) {
-			final int index1 = indices1[index];
-			final int index2 = indices2[index];
-			final int index3 = indices3[index];
-			if (points[index1][2] + points[index2][2] + points[index3][2] == 3) {
-				totalXAverage += (xPoints[index1] + xPoints[index2] + xPoints[index3]) / 3;
-				totalYAverage += (zPoints[index1] + zPoints[index2] + zPoints[index3]) / 3;
-				totalHeightAverage += (yPoints[index1] + yPoints[index2] + yPoints[index3]) / 3;
-			}
+			totalXAverage += (xPoints[faceA[index]] + xPoints[faceB[index]] + xPoints[faceC[index]]) / 3;
+			totalYAverage += (zPoints[faceA[index]] + zPoints[faceB[index]] + zPoints[faceC[index]]) / 3;
+			totalHeightAverage += (yPoints[faceA[index]] + yPoints[faceB[index]] + yPoints[faceC[index]]) / 3;
 			index++;
 		}
 
@@ -94,50 +132,18 @@ public abstract class CapturedModel {
 		return new Point(-1, -1);
 	}
 
-	public Point getNextViewportPoint() {
-		final java.util.List<Point> foundPoints = new ArrayList<Point>();
-		final int[][] points = projectVertices();
-		for (int index = 0; index < numVertices; index++) {
-			if (points[index][2] == 1) {
-				foundPoints.add(new Point(points[index][0], points[index][1]));
-			}
-		}
-		if (foundPoints.size() > 0 && isOnScreen()) {
-			return foundPoints.get(Random.nextInt(0, foundPoints.size()));
-		}
-		return new Point(-1, -1);
-	}
-
-	public boolean contains(final Point point) {
-		final Polygon[] polygons = getBounds();
-		for (final Polygon triangle : polygons) {
-			if (triangle.contains(point.x, point.y)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public boolean isOnScreen() {
-		final int[][] points = projectVertices();
-		for (int index = 0; index < numFaces; index++) {
-			final int index1 = indices1[index];
-			final int index2 = indices2[index];
-			final int index3 = indices3[index];
-			if (points[index1][2] + points[index2][2] + points[index3][2] == 3) {
-				return true;
-			}
-		}
-		return false;
-	}
-
+	@Deprecated
 	public Polygon[] getBounds() {
+		return getTriangles();
+	}
+
+	public Polygon[] getTriangles() {
 		final int[][] points = projectVertices();
-		ArrayList<Polygon> polys = new ArrayList<Polygon>(numFaces);
+		final ArrayList<Polygon> polygons = new ArrayList<>(numFaces);
 		for (int index = 0; index < numFaces; index++) {
-			final int index1 = indices1[index];
-			final int index2 = indices2[index];
-			final int index3 = indices3[index];
+			final int index1 = faceA[index];
+			final int index2 = faceB[index];
+			final int index3 = faceC[index];
 
 			final int xPoints[] = new int[3];
 			final int yPoints[] = new int[3];
@@ -150,19 +156,40 @@ public abstract class CapturedModel {
 			yPoints[2] = points[index3][1];
 
 			if (points[index1][2] + points[index2][2] + points[index3][2] == 3) {
-				polys.add(new Polygon(xPoints, yPoints, 3));
+				polygons.add(new Polygon(xPoints, yPoints, 3));
 			}
 		}
-		return polys.toArray(new Polygon[polys.size()]);
+		return polygons.toArray(new Polygon[polygons.size()]);
 	}
 
+	public boolean contains(final Point point) {
+		final int x = point.x, y = point.y;
+		final int[][] points = projectVertices();
+		int index = 0;
+		while (index < numFaces) {
+			final int index1 = faceA[index];
+			final int index2 = faceB[index];
+			final int index3 = faceC[index];
+			if (points[index1][2] + points[index2][2] + points[index3][2] == 3 &&
+					isPointInTriangle(x, y, points[index1][0], points[index1][1], points[index2][0], points[index2][1], points[index3][0], points[index3][1])) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	@Deprecated
 	public void draw(final Graphics render) {
+		drawWireFrame(render);
+	}
+
+	public void drawWireFrame(final Graphics render) {
 		final int[][] screen = projectVertices();
 
 		for (int index = 0; index < numFaces; index++) {
-			int index1 = indices1[index];
-			int index2 = indices2[index];
-			int index3 = indices3[index];
+			int index1 = faceA[index];
+			int index2 = faceB[index];
+			int index3 = faceC[index];
 
 			int point1X = screen[index1][0];
 			int point1Y = screen[index1][1];
@@ -179,7 +206,41 @@ public abstract class CapturedModel {
 		}
 	}
 
-	public int[][] projectVertices() {
+	private int firstOnScreenIndex(final int pos, final int length) {
+		final Context context = Context.get();
+		final Calculations.Toolkit toolkit = context.getToolkit();
+		final Calculations.Viewport viewport = context.getViewport();
+
+		final int x = getLocalX();
+		final int y = getLocalY();
+		final int h = Calculations.calculateTileHeight(x, y);
+		int index = pos;
+		while (index < length) {
+			final Point point = Calculations.worldToScreen(toolkit, viewport,
+					x + (this.xPoints[this.faceA[index]] + this.xPoints[this.faceB[index]] + this.xPoints[this.faceC[index]]) / 3,
+					h + (this.yPoints[this.faceA[index]] + this.yPoints[this.faceB[index]] + this.yPoints[this.faceC[index]]) / 3,
+					y + (this.zPoints[this.faceA[index]] + this.zPoints[this.faceB[index]] + this.zPoints[this.faceC[index]]) / 3
+			);
+			if (Calculations.isOnScreen(point)) {
+				return index;
+			}
+			++index;
+		}
+		return -1;
+	}
+
+	private Point firstOnScreenCentroid(final int pos, final int length) {
+		final int index = firstOnScreenIndex(pos, length);
+		return index != -1 ? getCentroid(index) : null;
+	}
+
+	private boolean isPointInTriangle(final int x, final int y, int x1, int y1, int x2, int y2, int x3, int y3) {
+		return (x - x2) * (y1 - y2) - (x1 - x2) * (y - y2) < 0 &&
+				(x - x3) * (y2 - y3) - (x2 - x3) * (y - y3) < 0 &&
+				(x - x1) * (y3 - y1) - (x3 - x1) * (y - y1) < 0;
+	}
+
+	private int[][] projectVertices() {
 		final Context context = Context.get();
 		final Calculations.Toolkit toolkit = context.getToolkit();
 		final Calculations.Viewport viewport = context.getViewport();
@@ -212,29 +273,18 @@ public abstract class CapturedModel {
 		return screen;
 	}
 
-	public int getNumFaces() {
-		return numFaces;
-	}
-
-	public short[] getIndices1() {
-		return indices1;
-	}
-
-	public short[] getIndices2() {
-		return indices2;
-	}
-
-	public short[] getIndices3() {
-		return indices3;
-	}
-
 	@Override
 	public boolean equals(final Object o) {
-		return o != null && o instanceof CapturedModel && Arrays.equals(((CapturedModel) o).indices1, indices1);
+		if (o != null && o instanceof CapturedModel) {
+			final CapturedModel model = (CapturedModel) o;
+			return Arrays.equals(faceA, model.faceA) &&
+					Arrays.equals(xPoints, model.xPoints) && Arrays.equals(yPoints, model.yPoints) && Arrays.equals(zPoints, model.zPoints);
+		}
+		return false;
 	}
 
 	@Override
 	public String toString() {
-		return "[faces=" + numFaces + "vertices=" + numVertices + "] " + Arrays.toString(indices1);
+		return "[faces=" + numFaces + "vertices=" + numVertices + "] " + Arrays.toString(faceA);
 	}
 }
