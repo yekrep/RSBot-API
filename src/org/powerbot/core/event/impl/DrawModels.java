770106e0d6d95f8f2686c289b728b01ce3aaeeab
Joe Haddad <timer@powerbot.org>
2013-01-28 01:46:31 -0500

    New model debugging (more efficient).

--- src/org/powerbot/core/event/impl/DrawModels.java
+++ src/org/powerbot/core/event/impl/DrawModels.java
@@ -2,10 +2,9 @@ package org.powerbot.core.event.impl;
 
 import java.awt.Color;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
 import java.awt.Point;
-import java.awt.RenderingHints;
 import java.util.HashMap;
+import java.util.Map;
 
 import org.powerbot.core.event.listeners.PaintListener;
 import org.powerbot.game.api.methods.interactive.NPCs;
@@ -13,61 +12,60 @@ import org.powerbot.game.api.methods.interactive.Players;
 import org.powerbot.game.api.methods.node.GroundItems;
 import org.powerbot.game.api.methods.node.SceneEntities;
 import org.powerbot.game.api.wrappers.graphics.CapturedModel;
+import org.powerbot.game.api.wrappers.interactive.NPC;
+import org.powerbot.game.api.wrappers.interactive.Player;
+import org.powerbot.game.api.wrappers.node.GroundItem;
 import org.powerbot.game.api.wrappers.node.SceneObject;
 
 public class DrawModels implements PaintListener {
-	private static final HashMap<Integer, Color> color_map = new HashMap<Integer, Color>();
+	private static final Map<Integer, Color> interactivePalette = new HashMap<>();
 
 	static {
-		color_map.put(SceneEntities.TYPE_BOUNDARY, Color.BLACK);
-		color_map.put(SceneEntities.TYPE_FLOOR_DECORATION, Color.YELLOW);
-		color_map.put(SceneEntities.TYPE_INTERACTIVE, Color.WHITE);
-		color_map.put(SceneEntities.TYPE_WALL_DECORATION, Color.GRAY);
+		interactivePalette.put(SceneEntities.TYPE_BOUNDARY, Color.black);
+		interactivePalette.put(SceneEntities.TYPE_FLOOR_DECORATION, Color.yellow);
+		interactivePalette.put(SceneEntities.TYPE_INTERACTIVE, Color.white);
+		interactivePalette.put(SceneEntities.TYPE_WALL_DECORATION, Color.gray);
 	}
 
-	public void onRepaint(final Graphics graphics) {
-		final Graphics2D render = (Graphics2D) graphics;
-		render.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-				RenderingHints.VALUE_ANTIALIAS_ON);
-		for (final SceneObject e : SceneEntities.getLoaded()) {
-			final CapturedModel model = e.getModel();
-			if (model != null && e.getLocation().isOnScreen()) {
-				render.setColor(color_map.get(e.getType()));
-				model.draw(render);
-				render.setColor(Color.green);
-				final Point p = model.getCentralPoint();
-				render.fillOval(p.x - 2, p.y - 2, 4, 4);
-			}
+	@Override
+	public void onRepaint(final Graphics render) {
+		/* Interactive entities */
+		final SceneObject[] objects = SceneEntities.getLoaded();
+		for (final SceneObject object : objects) {
+			final CapturedModel model = object.getModel();
+			if (model == null || model.nextTriangle() == -1) continue;
+			render.setColor(interactivePalette.get(object.getType()));
+			model.drawWireFrame(render);
+
+			final Point p = model.getCenterPoint();
+			render.setColor(Color.green);
+			render.fillOval(p.x - 2, p.y - 2, 5, 5);
 		}
-		for (final org.powerbot.game.api.wrappers.interactive.Character e : Players.getLoaded()) {
-			final CapturedModel model = e.getModel();
-			if (model != null && e.getLocation().isOnScreen()) {
-				render.setColor(Color.red);
-				model.draw(render);
-				render.setColor(Color.green);
-				final Point p = model.getCentralPoint();
-				render.fillOval(p.x - 2, p.y - 2, 4, 4);
-			}
+
+		/* Renderable entities */
+		render.setColor(Color.blue);
+		final GroundItem[] groundItems = GroundItems.getLoaded();
+		for (final GroundItem groundItem : groundItems) {
+			final CapturedModel model = groundItem.getModel();
+			if (model == null || model.nextTriangle() == -1) continue;
+			model.drawWireFrame(render);
 		}
-		for (final org.powerbot.game.api.wrappers.interactive.Character e : NPCs.getLoaded()) {
-			final CapturedModel model = e.getModel();
-			if (model != null && e.getLocation().isOnScreen()) {
-				render.setColor(Color.magenta);
-				model.draw(render);
-				render.setColor(Color.green);
-				final Point p = model.getCentralPoint();
-				render.fillOval(p.x - 2, p.y - 2, 4, 4);
-			}
+
+		/* Mobile entities */
+		render.setColor(Color.red);
+		final Player[] players = Players.getLoaded();
+		for (final Player player : players) {
+			final CapturedModel model = player.getModel();
+			if (model == null || model.nextTriangle() == -1) continue;
+			model.drawWireFrame(render);
 		}
-		for (final org.powerbot.game.api.wrappers.node.GroundItem e : GroundItems.getLoaded()) {
-			final CapturedModel model = e.getModel();
-			if (model != null && e.getLocation().isOnScreen()) {
-				render.setColor(Color.orange);
-				model.draw(render);
-				render.setColor(Color.green);
-				final Point p = model.getCentralPoint();
-				render.fillOval(p.x - 2, p.y - 2, 4, 4);
-			}
+
+		render.setColor(Color.magenta);
+		final NPC[] npcs = NPCs.getLoaded();
+		for (final NPC npc : npcs) {
+			final CapturedModel model = npc.getModel();
+			if (model == null || model.nextTriangle() == -1) continue;
+			model.drawWireFrame(render);
 		}
 	}
 }
