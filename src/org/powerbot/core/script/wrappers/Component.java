7f0dcfbabd87a94f90025cdfc8cc6be3b95c40b9
Joe Haddad <timer@powerbot.org>
2013-02-12 17:48:06 -0500

    Component updates

--- src/org/powerbot/core/script/wrappers/Component.java
+++ src/org/powerbot/core/script/wrappers/Component.java
@@ -1,20 +1,280 @@
 package org.powerbot.core.script.wrappers;
 
-import org.powerbot.game.client.RSInterface;
+import java.awt.Point;
+import java.awt.Rectangle;
 
-public class Component {
-	private final Widget container;
+import org.powerbot.core.Bot;
+import org.powerbot.core.script.internal.HashTable;
+import org.powerbot.core.script.methods.Widgets;
+import org.powerbot.game.client.Client;
+import org.powerbot.game.client.RSInterface;
+import org.powerbot.game.client.RSInterfaceNode;
+
+public class Component {//TODO isValid, getChildren, isVisible
+	private final Widget widget;
 	private final Component parent;
 	private final int index;
 
-	public Component(final Widget container, final int index) {
-		this(container, null, index);
+	public Component(final Widget widget, final int index) {
+		this(widget, null, index);
 	}
 
-	public Component(final Widget container, final Component parent, final int index) {
-		this.container = container;
+	public Component(final Widget widget, final Component parent, final int index) {
+		this.widget = widget;
 		this.parent = parent;
 		this.index = index;
+	}
+
+	public Widget getWidget() {
+		return widget;
+	}
+
+	public Component getParent() {
+		return parent;
+
+	}
+
+	public int getIndex() {
+		return this.index;
+	}
+
+	public String[] getActions() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getActions() : null;
+	}
+
+	public int getTextureId() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getTextureID() : -1;
+	}
+
+	public int getBorderThickness() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getBorderThinkness() : -1;
+	}
+
+	public int getId() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getID() : -1;
+	}
+
+	public int getChildIndex() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getComponentIndex() : -1;
+	}
+
+	public String getItemName() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getComponentName() : null;
+	}
+
+	public int getItemId() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getComponentID() : -1;
+	}
+
+	public int getItemStackSize() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getComponentStackSize() : -1;
+	}
+
+	public int getModelId() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getModelID() : -1;
+	}
+
+	public int getModelType() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getModelType() : -1;
+	}
+
+	public int getModelZoom() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getModelZoom() : -1;
+	}
+
+	public int getParentId() {
+		final Client client = Bot.client();
+		final RSInterface component = getInternalComponent();
+		if (client == null || component == null) return -1;
+
+		final int pId = component.getParentID();
+		if (pId != -1) return pId;
+
+		final int containerId = getId() >>> 16;
+		final HashTable ncI = new HashTable(client.getRSInterfaceNC());
+		for (RSInterfaceNode node = (RSInterfaceNode) ncI.getFirst(); node != null; node = (RSInterfaceNode) ncI.getNext()) {
+			if (containerId == node.getMainID()) {
+				return (int) node.getId();
+			}
+		}
+
+		return -1;
+	}
+
+	public Point getAbsoluteLocation() {
+		final Client client = Bot.client();
+		final RSInterface component = getInternalComponent();
+		if (client == null || component == null) return new Point(-1, -1);
+		final int pId = getParentId();
+		int x = 0, y = 0;
+		if (pId != -1) {
+			final Point point = org.powerbot.core.script.methods.Widgets.get(pId >> 16, pId & 0xffff).getAbsoluteLocation();
+			x = point.x;
+			y = point.y;
+		} else {
+			final Rectangle[] bounds = client.getRSInterfaceBoundsArray();
+			final int index = component.getBoundsArrayIndex();
+			if (bounds != null && index > 0 && index < bounds.length && bounds[index] != null) {
+				return new Point(bounds[index].x, bounds[index].y);
+			}
+			//x = getMasterX();
+			//y = getMasterY();
+		}
+		if (pId != -1) {
+			final Component child = org.powerbot.core.script.methods.Widgets.get(pId >> 16, pId & 0xffff);
+			final int horizontalScrollSize = child.getMaxHorizontalScroll(), verticalScrollSize = child.getMaxVerticalScroll();
+			if (horizontalScrollSize > 0 || verticalScrollSize > 0) {
+				x -= child.getScrollX();
+				y -= child.getScrollY();
+			}
+		}
+		x += component.getX();
+		y += component.getY();
+		return new Point(x, y);
+	}
+
+	public Point getRelativeLocation() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? new Point(component.getX(), component.getY()) : new Point(-1, -1);
+	}
+
+	public String getSelectedAction() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getSelectedActionName() : null;
+	}
+
+	public int getShadowColor() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getShadowColor() : -1;
+	}
+
+	public int getContentType() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getSpecialType() : -1;
+	}
+
+	public String getText() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getText() : null;
+	}
+
+	public int getTextColor() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getTextColor() : -1;
+	}
+
+	public String getTooltip() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getTooltip() : null;
+	}
+
+	public int getType() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getType() : -1;
+	}
+
+	public int getWidth() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getWidth() : -1;
+	}
+
+	public int getHeight() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getHeight() : -1;
+	}
+
+	public int getXRotation() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getXRotation() : -1;
+	}
+
+	public int getYRotation() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getYRotation() : -1;
+	}
+
+	public int getZRotation() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getZRotation() : -1;
+	}
+
+	public boolean isVerticallyFlipped() {
+		final RSInterface component = getInternalComponent();
+		return component != null && component.isVerticallyFlipped();
+	}
+
+	public boolean isHorizontallyFlipped() {
+		final RSInterface component = getInternalComponent();
+		return component != null && component.isHorizontallyFlipped();
+
+	}
+
+	public int getScrollX() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getHorizontalScrollbarPosition() : -1;
+	}
+
+	public int getMaxHorizontalScroll() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getHorizontalScrollbarSize() : -1;
+	}
+
+	public int getScrollWidth() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getHorizontalScrollbarThumbSize() : -1;
+	}
+
+	public int getScrollY() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getVerticalScrollbarPosition() : -1;
+	}
+
+	public int getMaxVerticalScroll() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getVerticalScrollbarSize() : -1;
+	}
+
+	public int getScrollHeight() {
+		final RSInterface component = getInternalComponent();
+		return component != null ? component.getVerticalScrollbarThumbSize() : -1;
+	}
+
+	public boolean isInventory() {
+		final RSInterface component = getInternalComponent();
+		return component != null && component.isInventoryInterface();
+	}
+
+	private Rectangle getInteractRectangle() {
+		final Point absLocation = getAbsoluteLocation();
+		if (absLocation.x == -1 && absLocation.y == -1) return new Rectangle(-1, -1, 0, 0);
+		final boolean canScroll = isInScrollableArea();
+		return new Rectangle(absLocation.x, absLocation.y,
+				canScroll ? getWidth() : getScrollWidth(),
+				canScroll ? getHeight() : getScrollHeight()
+		);
+	}
+
+	private boolean isInScrollableArea() {
+		int pId = getParentId();
+		if (pId == -1) return false;
+
+		Component scrollableArea = Widgets.get(pId >> 16, pId & 0xffff);
+		while (scrollableArea.getMaxVerticalScroll() == 0 && (pId = scrollableArea.getParentId()) != -1) {
+			scrollableArea = Widgets.get(pId >> 16, pId & 0xffff);
+		}
+
+		return scrollableArea.getMaxVerticalScroll() != 0;
 	}
 
 	private RSInterface getInternalComponent() {
@@ -23,9 +283,8 @@ public class Component {
 			final RSInterface parentComponent = parent.getInternalComponent();
 			components = parentComponent != null ? parentComponent.getComponents() : null;
 		} else {
-			components = container.getInternalComponents();
+			components = widget.getInternalComponents();
 		}
 		return components != null && index < components.length ? components[index] : null;
 	}
-
 }
