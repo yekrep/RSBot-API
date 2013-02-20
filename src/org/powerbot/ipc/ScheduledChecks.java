9617b1c3d5e7eb34b83a3accb369281780a3ccbb
A. Ahmed <paris@powerbot.org>
2013-02-20 20:21:20 +0000

    Track uptime events.

--- src/org/powerbot/ipc/ScheduledChecks.java
+++ src/org/powerbot/ipc/ScheduledChecks.java
@@ -2,6 +2,7 @@ package org.powerbot.ipc;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
@@ -19,12 +20,18 @@ public final class ScheduledChecks implements ActionListener {
 	public static volatile long SESSION_TIME = 0;
 	public static final long LOCALSCRIPT_TIMEOUT = 15 * 60000000000L;
 	public static final AtomicLong timeout = new AtomicLong(0);
+	private static final long started = System.nanoTime();
 
 	@Override
 	public void actionPerformed(final ActionEvent e) {
 		if (!Controller.getInstance().isBound() || Controller.getInstance().getRunningInstances() < 1) {
 			Tracker.getInstance().trackEvent("exit", "instance-overload");
 			System.exit(1);
+		}
+
+		final long uptime = TimeUnit.NANOSECONDS.toMinutes(System.nanoTime() - started);
+		if (uptime % 10 == 0) {
+			Tracker.getInstance().trackEvent("uptime", Long.toString(uptime));
 		}
 
 		if (Bot.instantiated() && Bot.instance().getScriptHandler() != null) {
