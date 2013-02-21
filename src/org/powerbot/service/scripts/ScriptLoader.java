a30a272ee8e5ea34d1dbd750720f1c8819794cdc
A. Ahmed <paris@powerbot.org>
2013-02-21 20:57:09 +0000

    Revert "Revert "Pre-obfuscate script class loader.""
    This reverts commit 689a6ed027d63e3f7513409978f198f1b47a94b2.

---
+++ src/org/powerbot/service/scripts/ScriptLoader.java
@@ -0,0 +1,38 @@
+package org.powerbot.service.scripts;
+
+import java.io.ByteArrayOutputStream;
+import java.lang.reflect.Method;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
+import org.powerbot.util.io.IOHelper;
+
+/**
+ * @author Paris
+ */
+public final class ScriptLoader {
+
+	public static Object getInstance(final ZipInputStream in) throws Exception {
+		final Class<?> scl = ScriptClassLoader.class;
+		final Object cl = scl.newInstance();
+		final Method m = ClassLoader.class.getDeclaredMethod("defineClass", String.class, byte[].class, int.class, int.class);
+		m.setAccessible(true);
+
+		ZipEntry entry;
+		while ((entry = in.getNextEntry()) != null) {
+			final ByteArrayOutputStream out = new ByteArrayOutputStream();
+			final byte[] data = new byte[IOHelper.BUFFER_SIZE];
+			int len;
+			while ((len = in.read(data)) != -1) {
+				out.write(data, 0, len);
+			}
+			final byte[] buf = out.toByteArray();
+			final String name = entry.getName();
+			m.invoke(cl, name.substring(0, name.indexOf('.')).replace('/', '.'), buf, 0, buf.length);
+			in.closeEntry();
+		}
+		in.close();
+
+		return cl;
+	}
+}
