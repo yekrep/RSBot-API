9474fe4929233332e3f046ed6e7a7127ef87ac62
Joe Haddad <timer@powerbot.org>
2013-03-03 18:25:39 -0500

    beta character interaction

--- src/org/powerbot/core/script/wrappers/Interactive.java
+++ src/org/powerbot/core/script/wrappers/Interactive.java
@@ -1,6 +1,8 @@
 package org.powerbot.core.script.wrappers;
 
 public interface Interactive extends Targetable {
+	public boolean isOnScreen();
+
 	public boolean hover();
 
 	public boolean click(boolean left);
