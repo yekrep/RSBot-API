3c8c7ac4f6bac33d016e52df1f28fefa9a38c6c9
Joe Haddad <timer@powerbot.org>
2013-03-11 11:49:14 -0400

    renderable model type for new api

---
+++ src/org/powerbot/script/xenon/wrappers/RenderableModel.java
@@ -0,0 +1,31 @@
+package org.powerbot.script.xenon.wrappers;
+
+import org.powerbot.game.client.RSInteractable;
+
+public class RenderableModel extends Model {
+	private RSInteractable interactable;
+
+	public RenderableModel(final org.powerbot.game.client.Model model, final RSInteractable interactable) {
+		super(model);
+		this.interactable = interactable;
+	}
+
+	@Override
+	public int getX() {
+		return (int) interactable.getData().getLocation().getX();
+	}
+
+	@Override
+	public int getY() {
+		return (int) interactable.getData().getLocation().getY();
+	}
+
+	@Override
+	public byte getPlane() {
+		return interactable.getPlane();
+	}
+
+	@Override
+	public void update() {
+	}
+}
