0afe599f1d950498dd42e067ac2887f409a2364c
Joe Haddad <timer@powerbot.org>
2013-03-11 21:18:03 -0400

    complete item definition

--- src/org/powerbot/script/xenon/wrappers/ItemDefinition.java
+++ src/org/powerbot/script/xenon/wrappers/ItemDefinition.java
@@ -4,10 +4,40 @@ import java.lang.ref.WeakReference;
 
 import org.powerbot.game.client.RSItemDef;
 
-public class ItemDefinition {//TODO complete
+public class ItemDefinition implements Validatable {
 	private final WeakReference<RSItemDef> def;
 
 	ItemDefinition(final RSItemDef def) {
 		this.def = new WeakReference<>(def);
+	}
+
+	public int getId() {
+		final RSItemDef def = this.def.get();
+		return def != null ? def.getID() : -1;
+	}
+
+	public String getName() {
+		final RSItemDef def = this.def.get();
+		return def != null ? def.getName() : null;
+	}
+
+	public boolean isMembers() {
+		final RSItemDef def = this.def.get();
+		return def != null && def.isMembersObject();
+	}
+
+	public String[] getActions() {
+		final RSItemDef def = this.def.get();
+		return def != null ? def.getActions() : null;
+	}
+
+	public String[] getGroundActions() {
+		final RSItemDef def = this.def.get();
+		return def != null ? def.getGroundActions() : null;
+	}
+
+	@Override
+	public boolean isValid() {
+		return def.get() != null;
 	}
 }
