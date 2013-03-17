05c2886e01406ea208fad3843a5bf02778c6adfa
A. Ahmed <paris@powerbot.org>
2013-03-17 19:09:58 +0000

    Runtime counters

--- src/org/powerbot/script/AbstractScript.java
+++ src/org/powerbot/script/AbstractScript.java
@@ -3,8 +3,12 @@ package org.powerbot.script;
 import java.util.ArrayDeque;
 import java.util.Collection;
 import java.util.Map;
+import java.util.Queue;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.FutureTask;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 import org.powerbot.script.util.Prioritizable;
@@ -20,6 +24,8 @@ public abstract class AbstractScript implements Script, Prioritizable {
 	protected final Logger log = Logger.getLogger(getClass().getName());
 	private final Map<State, Collection<FutureTask<Boolean>>> tasks;
 	private ScriptController controller;
+	private final AtomicLong started, suspended;
+	private final Queue<Long> suspensions;
 
 	public AbstractScript() {
 		tasks = new ConcurrentHashMap<State, Collection<FutureTask<Boolean>>>(State.values().length);
@@ -29,6 +35,31 @@ public abstract class AbstractScript implements Script, Prioritizable {
 		}
 
 		tasks.get(State.START).add(new FutureTask<Boolean>(this, true));
+
+		started = new AtomicLong(System.nanoTime());
+		suspended = new AtomicLong(0);
+		suspensions = new SynchronousQueue<Long>();
+
+		tasks.get(State.START).add(new FutureTask<Boolean>(new Runnable() {
+			@Override
+			public void run() {
+				started.set(System.nanoTime());
+			}
+		}, true));
+
+		tasks.get(State.SUSPEND).add(new FutureTask<Boolean>(new Runnable() {
+			@Override
+			public void run() {
+				suspensions.offer(System.nanoTime());
+			}
+		}, true));
+
+		tasks.get(State.RESUME).add(new FutureTask<Boolean>(new Runnable() {
+			@Override
+			public void run() {
+				suspended.addAndGet(System.nanoTime() - suspensions.poll());
+			}
+		}, true));
 	}
 
 	/**
@@ -90,5 +121,23 @@ public abstract class AbstractScript implements Script, Prioritizable {
 	 */
 	public void sleep(final int min, final int max) {
 		sleep(Random.nextInt(min, max));
+	}
+
+	/**
+	 * Returns the total running time.
+	 *
+	 * @return the total runtime so far in seconds (including pauses)
+	 */
+	public long getTotalRuntime() {
+		return TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - started.get());
+	}
+
+	/**
+	 * Returns the actual running time.
+	 *
+	 * @return the actual runtime so far in seconds
+	 */
+	public long getRuntime() {
+		return TimeUnit.NANOSECONDS.toSeconds(System.nanoTime() - started.get() - suspended.get());
 	}
 }
