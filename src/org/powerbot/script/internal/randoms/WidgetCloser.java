9b4da19b8c1bc0ee51a9461ad9d60633a53c66ac
Joe Haddad <timer@powerbot.org>
2013-03-19 23:41:44 -0400

    Update to Xenon API

--- src/org/powerbot/script/internal/randoms/WidgetCloser.java
+++ src/org/powerbot/script/internal/randoms/WidgetCloser.java
@@ -1,24 +1,23 @@
 package org.powerbot.script.internal.randoms;
 
-import org.powerbot.game.api.methods.Widgets;
-import org.powerbot.game.api.wrappers.widget.WidgetChild;
 import org.powerbot.script.Manifest;
 import org.powerbot.script.TaskScript;
 import org.powerbot.script.task.AsyncTask;
+import org.powerbot.script.xenon.Widgets;
+import org.powerbot.script.xenon.wrappers.Component;
 
-@Manifest(name = "Widget closer", authors = { "Timer" }, description = "Closes widgets")
+@Manifest(name = "Widget closer", authors = {"Timer"}, description = "Closes widgets")
 public class WidgetCloser extends TaskScript {
-
 	private static final int[] COMPONENTS = {
-		21 << 16 | 43, // beholding a player's statuette (duellist's cap)
-		1234 << 16 | 15, // membership offers
-		906 << 16 | 354, // membership offers
-		906 << 16 | 493, // email register
-		1252 << 16 | 8, // Squeal of Fortune notification
-		1253 << 16 | 16, // Squeal of Fortune window
-		1218 << 16 | 77, // advanced skill guide
-		1107 << 16 | 157, // clan popups
-		755 << 16 | 44, // world map
+			21 << 16 | 43, // beholding a player's statuette (duellist's cap)
+			1234 << 16 | 15, // membership offers
+			906 << 16 | 354, // membership offers
+			906 << 16 | 493, // email register
+			1252 << 16 | 8, // Squeal of Fortune notification
+			1253 << 16 | 16, // Squeal of Fortune window
+			1218 << 16 | 77, // advanced skill guide
+			1107 << 16 | 157, // clan popups
+			755 << 16 | 44, // world map
 	};
 
 	public WidgetCloser() {
@@ -26,12 +25,11 @@ public class WidgetCloser extends TaskScript {
 	}
 
 	private final class Task extends AsyncTask {
-
 		@Override
 		public boolean isValid() {
 			for (final int p : COMPONENTS) {
-				final WidgetChild child = Widgets.get(p >> 16, p & 0xffff);
-				if (child != null && child.validate()) {
+				final Component child = Widgets.get(p >> 16, p & 0xffff);
+				if (child != null && child.isValid()) {
 					return true;
 				}
 			}
@@ -41,8 +39,8 @@ public class WidgetCloser extends TaskScript {
 		@Override
 		public void run() {
 			for (final int p : COMPONENTS) {
-				final WidgetChild child = Widgets.get(p >> 16, p & 0xffff);
-				if (child != null && child.validate()) {
+				final Component child = Widgets.get(p >> 16, p & 0xffff);
+				if (child != null && child.isValid()) {
 					child.click(true);
 				}
 			}
