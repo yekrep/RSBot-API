9bfa9d4ee65efae00cd28bd026d6e3e848c8ee7c
A. Ahmed <paris@powerbot.org>
2013-03-23 16:20:11 -0400

    Bump up version due to hotfix

--- src/org/powerbot/util/Configuration.java
+++ src/org/powerbot/util/Configuration.java
@@ -18,7 +18,7 @@ public class Configuration {
 	public static final String NAME = "RSBot", TITLE = NAME + " 4S";
 	public static final boolean FROMJAR;
 	public static final boolean SUPERDEV;
-	public static final int VERSION = 4051;
+	public static final int VERSION = 4052;
 	public static final boolean BETA = true;
 	public static volatile int VERSION_LATEST = -1;
 	public static final OperatingSystem OS;
