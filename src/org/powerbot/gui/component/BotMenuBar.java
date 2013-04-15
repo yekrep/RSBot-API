9a96aebe2445dbbccdadb139cfe4c00548cbb24a
A. Ahmed <paris@powerbot.org>
2013-04-15 03:15:43 +0100

    Add script play/stop menu options

--- src/org/powerbot/gui/component/BotMenuBar.java
+++ src/org/powerbot/gui/component/BotMenuBar.java
@@ -3,8 +3,10 @@ package org.powerbot.gui.component;
 import org.powerbot.bot.Bot;
 import org.powerbot.gui.BotChrome;
 import org.powerbot.gui.controller.BotInteract;
+import org.powerbot.script.internal.ScriptManager;
 import org.powerbot.service.NetworkAccount;
 import org.powerbot.util.Configuration;
+import org.powerbot.util.io.Resources;
 
 import javax.swing.*;
 import javax.swing.event.MenuEvent;
@@ -16,12 +18,12 @@ import java.awt.event.ActionListener;
  * @author Paris
  */
 public class BotMenuBar extends JMenuBar implements ActionListener {
-	private final JMenuItem signin;
+	private final JMenuItem signin, play, stop;
 	private final JCheckBoxMenuItem logpane;
 
 	public BotMenuBar() {
-		final JMenu file = new JMenu(BotLocale.FILE), edit = new JMenu(BotLocale.EDIT),
-				view = new JMenu(BotLocale.VIEW), input = new JMenu(BotLocale.INPUT), help = new JMenu(BotLocale.HELP);
+		final JMenu file = new JMenu(BotLocale.FILE), edit = new JMenu(BotLocale.EDIT), view = new JMenu(BotLocale.VIEW),
+				script = new JMenu(BotLocale.SCRIPTS), input = new JMenu(BotLocale.INPUT), help = new JMenu(BotLocale.HELP);
 
 		file.add(item(BotLocale.NEWTAB));
 		if (Configuration.OS != Configuration.OperatingSystem.MAC) {
@@ -72,6 +74,34 @@ public class BotMenuBar extends JMenuBar implements ActionListener {
 			}
 		});
 
+		final ImageIcon[] playIcons = new ImageIcon[] { new ImageIcon(Resources.getImage(Resources.Paths.PLAY)), new ImageIcon(Resources.getImage(Resources.Paths.PAUSE)) };
+		play = item(BotLocale.PLAYSCRIPT);
+		play.setIcon(playIcons[0]);
+		script.add(play);
+		stop = item(BotLocale.STOPSCRIPT);
+		stop.setIcon(new ImageIcon(Resources.getImage(Resources.Paths.STOP)));
+		script.add(stop);
+
+		script.addMenuListener(new MenuListener() {
+			@Override
+			public void menuSelected(final MenuEvent e) {
+				final boolean b = Bot.instantiated();
+				final ScriptManager container = b ? Bot.getInstance().getScriptController() : null;
+				final boolean active = container != null && !container.getScripts().isEmpty() && !container.isStopping(), running = active && !container.isSuspended();
+				play.setText(running ? BotLocale.PAUSESCRIPT : active ? BotLocale.RESUMESCRIPT : BotLocale.PLAYSCRIPT);
+				play.setIcon(playIcons[running ? 1 : 0]);
+				stop.setEnabled(active);
+			}
+
+			@Override
+			public void menuDeselected(final MenuEvent e) {
+			}
+
+			@Override
+			public void menuCanceled(final MenuEvent e) {
+			}
+		});
+
 		input.addMenuListener(new MenuListener() {
 			@Override
 			public void menuSelected(final MenuEvent e) {
@@ -102,6 +132,7 @@ public class BotMenuBar extends JMenuBar implements ActionListener {
 		add(file);
 		add(edit);
 		add(view);
+		add(script);
 		add(input);
 		add(help);
 	}
@@ -124,6 +155,8 @@ public class BotMenuBar extends JMenuBar implements ActionListener {
 			case BotLocale.SIGNIN: BotInteract.showDialog(BotInteract.Action.SIGNIN); break;
 			case BotLocale.LOGPANE: logpane.setState(BotInteract.toggleLogPane()); break;
 			case BotLocale.ACCOUNTS: BotInteract.showDialog(BotInteract.Action.ACCOUNTS); break;
+			case BotLocale.PLAYSCRIPT: case BotLocale.PAUSESCRIPT: case BotLocale.RESUMESCRIPT: BotInteract.scriptPlayPause(); break;
+			case BotLocale.STOPSCRIPT: BotInteract.scriptStop(); break;
 			case BotLocale.ABOUT: BotInteract.showDialog(BotInteract.Action.ABOUT); break;
 			case BotLocale.LICENSE: BotInteract.showDialog(BotInteract.Action.LICENSE); break;
 			case BotLocale.WEBSITE: BotChrome.openURL(Configuration.URLs.SITE); break;
