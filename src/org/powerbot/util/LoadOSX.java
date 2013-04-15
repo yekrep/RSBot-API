d96df424bb1e065f44050b7ffeb44952b3fc302f
A. Ahmed <paris@powerbot.org>
2013-04-15 18:19:20 +0100

    Use annotations so code doesn't break after obfuscation

--- src/org/powerbot/util/LoadOSX.java
+++ src/org/powerbot/util/LoadOSX.java
@@ -1,9 +1,13 @@
 package org.powerbot.util;
 
 import org.powerbot.OSXAdapter;
-import org.powerbot.gui.BotChrome;
 import org.powerbot.gui.controller.BotInteract;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import java.lang.reflect.Method;
 import java.util.concurrent.Callable;
 
 /**
@@ -14,17 +18,32 @@ public class LoadOSX implements Callable<Boolean> {
 	@Override
 	public Boolean call() throws Exception {
 		if (Configuration.OS == Configuration.OperatingSystem.MAC) {
-			OSXAdapter.setAboutHandler(this, getClass().getDeclaredMethod("about", null));
-			OSXAdapter.setQuitHandler(null, getClass().getDeclaredMethod("quit", null));
+			for (final Method m : getClass().getDeclaredMethods()) {
+				if (m.isAnnotationPresent(OSXAdapterInfo.class)) {
+					switch (m.getAnnotation(OSXAdapterInfo.class).mode())
+					{
+						case 1: OSXAdapter.setAboutHandler(this, m); break;
+						case 2: OSXAdapter.setQuitHandler(this, m); break;
+					}
+				}
+			}
 		}
 		return true;
 	}
 
+	@LoadOSX.OSXAdapterInfo(mode = 1)
 	public static void about() {
 		BotInteract.showDialog(BotInteract.Action.ABOUT);
 	}
 
+	@LoadOSX.OSXAdapterInfo(mode = 2)
 	public static void quit() {
 		BotInteract.tabClose(false);
+	}
+
+	@Retention(RetentionPolicy.RUNTIME)
+	@Target({ElementType.METHOD})
+	public @interface OSXAdapterInfo {
+		public int mode() default 0;
 	}
 }
