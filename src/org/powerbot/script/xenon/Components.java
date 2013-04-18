f0d5b96f71c0fe8ad6c9d147255cd86eb49f6f8b
Joe Haddad <timer@powerbot.org>
2013-04-18 11:26:08 -0400

    Game tab helpers (getCurrentTab and openTab).

--- src/org/powerbot/script/xenon/Components.java
+++ src/org/powerbot/script/xenon/Components.java
@@ -28,7 +28,6 @@ class Components {
 		}
 		if (container.compass != -1) return Widgets.get(container.index, container.compass);
 		return null;
-
 	}
 
 	static Component getMap() {
@@ -46,6 +45,25 @@ class Components {
 			}
 		}
 		if (container.map != -1) return Widgets.get(container.index, container.map);
+		return null;
+	}
+
+	static Component getTab(final int index) {
+		final Container container = get();
+		if (container == null || index < 0 || index >= container.tabs.length) return null;
+
+		if (container.tabs[index] == -1) {
+			final Widget game = Widgets.get(container.index);
+			final Component[] components = game != null ? game.getComponents() : null;
+			if (components != null) for (final Component c : components) {
+				final String[] actions = c.getActions();
+				if (actions != null && actions.length == 1 && actions[0].equalsIgnoreCase(Game.TAB_NAMES[index])) {
+					container.tabs[index] = c.getIndex();
+					break;
+				}
+			}
+		}
+		if (container.tabs[index] != -1) return Widgets.get(container.index, container.tabs[index]);
 		return null;
 	}
 
--- src/org/powerbot/script/xenon/Game.java
+++ src/org/powerbot/script/xenon/Game.java
@@ -8,15 +8,61 @@ import org.powerbot.game.client.BaseInfo;
 import org.powerbot.game.client.Client;
 import org.powerbot.game.client.RSInfo;
 import org.powerbot.script.internal.Constants;
+import org.powerbot.script.xenon.util.Delay;
+import org.powerbot.script.xenon.util.Timer;
+import org.powerbot.script.xenon.wrappers.Component;
 import org.powerbot.script.xenon.wrappers.Tile;
 
 public class Game {
+	public static final int TAB_NONE = -1;
+	public static final int TAB_COMBAT = 0;
+	public static final int TAB_NOTICEBOARD = 1;
+	public static final int TAB_STATS = 2;
+	public static final int TAB_ACADEMY = 3;
+	public static final int TAB_INVENTORY = 4;
+	public static final int TAB_EQUIPMENT = 5;
+	public static final int TAB_PRAYER = 6;
+	public static final int TAB_ABILITY_BOOK = 7;
+	public static final int TAB_EXTRAS = 8;
+	public static final int TAB_FRIENDS = 9;
+	public static final int TAB_FRIENDS_CHAT = 10;
+	public static final int TAB_CLAN_CHAT = 11;
+	public static final int TAB_OPTIONS = 12;
+	public static final int TAB_EMOTES = 13;
+	public static final int TAB_MUSIC = 14;
+	public static final int TAB_NOTES = 15;
+	public static final int TAB_LOGOUT = 16;
+	public static final String[] TAB_NAMES = {
+			"Combat", "Noticeboard", "Stats", "Combat Academy", "Inventory", "Worn Equipment", "Prayer List", "Ability Book",
+			"Extras", "Friends List", "Friends Chat", "Clan Chat", "Options", "Emotes", "Music Player", "Notes", "Exit"
+	};
 	public static final int INDEX_LOGIN_SCREEN = 3;
 	public static final int INDEX_LOBBY_SCREEN = 7;
 	public static final int INDEX_LOGGING_IN = 9;
 	public static final int INDEX_MAP_LOADED = 11;
 	public static final int INDEX_MAP_LOADING = 12;
 	public static final int[] INDEX_LOGGED_IN = {INDEX_MAP_LOADED, INDEX_MAP_LOADING};
+
+	public static int getCurrentTab() {
+		Component c;
+		for (int i = 0; i < TAB_NAMES.length - 1; i++) {
+			if ((c = Components.getTab(i)) != null) {
+				if (c.getTextureId() != -1) return i;
+			}
+		}
+		if ((c = Widgets.get(182, 1)) != null && c.isVisible()) return TAB_LOGOUT;
+		return TAB_NONE;
+	}
+
+	public static boolean openTab(final int index) {
+		if (index < 0 || index >= TAB_NAMES.length) return false;
+		final Component c = Components.getTab(index);
+		if (c != null && c.isValid() && c.click(true)) {
+			final Timer t = new Timer(800);
+			while (t.isRunning() && getCurrentTab() != index) Delay.sleep(15);
+		}
+		return getCurrentTab() == index;
+	}
 
 	public static int getClientState() {
 		final Client client = Bot.client();
