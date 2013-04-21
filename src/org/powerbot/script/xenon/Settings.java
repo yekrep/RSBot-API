1117ccba813eada878d95f102a665bbac6a678c2
Joe Haddad <timer@powerbot.org>
2013-04-21 15:50:00 -0400

    bank tab setting

--- src/org/powerbot/script/xenon/Settings.java
+++ src/org/powerbot/script/xenon/Settings.java
@@ -5,6 +5,8 @@ import org.powerbot.game.client.Client;
 import org.powerbot.game.client.PlayerMetaInfo;
 
 public class Settings {
+	public static final int BANK_TAB = 110;
+
 	public static int[] getArray() {
 		final Client client = Bot.client();
 		if (client == null) return new int[0];
