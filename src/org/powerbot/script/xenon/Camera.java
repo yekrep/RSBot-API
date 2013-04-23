ba71b667b6d5c3bf3f77f601afe9d4bc2d295544
Joe Haddad <timer@powerbot.org>
2013-04-22 22:32:26 -0400

    completed setPitch method

--- src/org/powerbot/script/xenon/Camera.java
+++ src/org/powerbot/script/xenon/Camera.java
@@ -1,7 +1,11 @@
 package org.powerbot.script.xenon;
 
+import java.awt.event.KeyEvent;
+
 import org.powerbot.bot.Bot;
 import org.powerbot.game.client.Client;
+import org.powerbot.script.xenon.util.Delay;
+import org.powerbot.script.xenon.util.Timer;
 
 public class Camera {
 	public static int getX() {
@@ -29,11 +33,25 @@ public class Camera {
 		return client != null ? (int) ((client.getCameraPitch() - 1024) / 20.48) : -1;
 	}
 
-	public int setPitch(final int pitch) {
+	public static int setPitch(final int pitch) {
 		int p = getPitch();
 		if (p == pitch) return 0;
 		final boolean up = pitch > p;
-		//TODO set pitch
+		Keyboard.pressKey(up ? KeyEvent.VK_UP : KeyEvent.VK_DOWN);
+		int curr;
+		final Timer timer = new Timer(100);
+		while (timer.isRunning()) {
+			curr = getPitch();
+			if (curr != p) {
+				p = curr;
+				timer.reset();
+			}
+			if (up && curr >= pitch) break;
+			else if (!up && curr <= pitch) break;
+
+			Delay.sleep(5, 10);
+		}
+		Keyboard.releaseKey(up ? KeyEvent.VK_UP : KeyEvent.VK_DOWN);
 		return p - pitch;
 	}
 }
