a6d33a86646de33cc42cca08cb3012a316211fcd
Joe Haddad <timer@powerbot.org>
2013-04-22 22:31:28 -0400

    key pressed and key released helpers in keyboard

--- src/org/powerbot/script/xenon/Keyboard.java
+++ src/org/powerbot/script/xenon/Keyboard.java
@@ -1,5 +1,7 @@
 package org.powerbot.script.xenon;
 
+import java.awt.event.KeyEvent;
+
 import org.powerbot.bot.Bot;
 import org.powerbot.script.internal.input.InputHandler;
 
@@ -13,5 +15,29 @@ public class Keyboard {
 		if (inputHandler == null) return;
 		if (pressEnter) str += '\n';
 		inputHandler.send(str);
+	}
+
+	public static void pressKey(final int vk) {
+		final InputHandler inputHandler = Bot.inputHandler();
+		if (inputHandler == null) return;
+		inputHandler.send(inputHandler.constructKeyEvent(KeyEvent.KEY_PRESSED, vk));
+	}
+
+	public static void pressKey(final int vk, final char c) {
+		final InputHandler inputHandler = Bot.inputHandler();
+		if (inputHandler == null) return;
+		inputHandler.send(inputHandler.constructKeyEvent(KeyEvent.KEY_PRESSED, vk, c));
+	}
+
+	public static void releaseKey(final int vk) {
+		final InputHandler inputHandler = Bot.inputHandler();
+		if (inputHandler == null) return;
+		inputHandler.send(inputHandler.constructKeyEvent(KeyEvent.KEY_RELEASED, vk));
+	}
+
+	public static void releaseKey(final int vk, final char c) {
+		final InputHandler inputHandler = Bot.inputHandler();
+		if (inputHandler == null) return;
+		inputHandler.send(inputHandler.constructKeyEvent(KeyEvent.KEY_RELEASED, vk, c));
 	}
 }
