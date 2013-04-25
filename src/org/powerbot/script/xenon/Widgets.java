cb0ffddc0bf27a5f5a78359864427907c41ce010
Joe Haddad <timer@powerbot.org>
2013-04-24 22:26:14 -0400

    Widgets#scroll for new api

--- src/org/powerbot/script/xenon/Widgets.java
+++ src/org/powerbot/script/xenon/Widgets.java
@@ -1,5 +1,6 @@
 package org.powerbot.script.xenon;
 
+import java.awt.Point;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
@@ -7,6 +8,8 @@ import java.util.Map;
 import org.powerbot.bot.Bot;
 import org.powerbot.game.client.Client;
 import org.powerbot.game.client.RSInterfaceBase;
+import org.powerbot.script.xenon.util.Delay;
+import org.powerbot.script.xenon.util.Random;
 import org.powerbot.script.xenon.wrappers.Component;
 import org.powerbot.script.xenon.wrappers.Widget;
 
@@ -36,6 +39,36 @@ public class Widgets {
 	}
 
 	public static boolean scroll(final Component component, final Component bar) {
-		return false;//TODO scroll
+		if (component == null || bar == null || !component.isValid() || bar.getChildrenCount() != 6) return false;
+		Component area = component;
+		int id;
+		while (area.getScrollHeight() == 0 && (id = area.getParentId()) != -1) area = get(id >> 16, id & 0xffff);
+		if (area.getScrollHeight() == 0) return false;
+
+		final Point abs = area.getAbsoluteLocation();
+		if (abs.x == -1 || abs.y == -1) return false;
+		final int height = area.getHeight();
+		final Point p = component.getAbsoluteLocation();
+		if (p.y >= abs.y && p.y <= abs.y + height - component.getHeight()) return true;
+
+		final Component _bar = bar.getChild(0);
+		final int size = area.getScrollHeight();
+		int pos = (int) ((float) _bar.getHeight() / size * (component.getRelativeLocation().y + Random.nextInt(-height / 2, height / 2 - component.getHeight())));
+		if (pos < 0) pos = 0;
+		else if (pos >= _bar.getHeight()) pos = _bar.getHeight() - 1;
+		final Point nav = _bar.getAbsoluteLocation();
+		nav.translate(Random.nextInt(0, _bar.getWidth()), pos);
+		if (!Mouse.click(nav, true)) return false;
+		Delay.sleep(200, 400);
+
+		boolean up;
+		Point a;
+		Component c;
+		while ((a = component.getAbsoluteLocation()).y < abs.y || a.y > abs.y + height - component.getHeight()) {
+			up = a.y < abs.y;
+			c = bar.getChild(up ? 4 : 5);
+			if (c.click()) Delay.sleep(100, 200);
+		}
+		return a.y >= abs.y && a.y <= height + abs.y + height - component.getHeight();
 	}
 }
