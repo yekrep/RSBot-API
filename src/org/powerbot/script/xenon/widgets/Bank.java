ae0a80c1af855d2f8d0ceb2f2aa8cc04f897dd25
Joe Haddad <timer@powerbot.org>
2013-04-25 22:40:38 -0400

    Bank deposit and withdraw method

--- src/org/powerbot/script/xenon/widgets/Bank.java
+++ src/org/powerbot/script/xenon/widgets/Bank.java
@@ -1,9 +1,13 @@
 package org.powerbot.script.xenon.widgets;
 
+import java.awt.Point;
+import java.awt.Rectangle;
 import java.util.Arrays;
 
+import org.powerbot.script.xenon.Keyboard;
 import org.powerbot.script.xenon.Settings;
 import org.powerbot.script.xenon.Widgets;
+import org.powerbot.script.xenon.tabs.Inventory;
 import org.powerbot.script.xenon.util.Delay;
 import org.powerbot.script.xenon.util.Filter;
 import org.powerbot.script.xenon.util.Random;
@@ -16,6 +20,7 @@ public class Bank {
 	public static final int WIDGET = 762;
 	public static final int COMPONENT_CLOSE = 45;
 	public static final int COMPONENT_ITEMS = 95;
+	public static final int COMPONENT_SCROLLBAR = 116;
 	public static final int SETTING_BANK_STATE = 1248;
 
 	public static boolean isOpen() {
@@ -102,12 +107,90 @@ public class Bank {
 
 	public static boolean setCurrentTab(final int index) {
 		final Component c = Widgets.get(WIDGET, 63 - (index * 2));
-		return c != null && c.isValid() && c.click(true);
+		if (c != null && c.isValid() && c.click(true)) {
+			final Timer timer = new Timer(800);
+			while (timer.isRunning() && getCurrentTab() != index) Delay.sleep(15);
+			return getCurrentTab() == index;
+		}
+		return false;
 	}
 
 	public static Item getTabItem(final int index) {
 		final Component c = Widgets.get(WIDGET, 63 - (index * 2));
 		if (c != null && c.isValid()) return new Item(c);
 		return null;
+	}
+
+	public static boolean withdraw(final int id, final int amount) {
+		final Item item = getItem(id);
+		if (item == null) return false;
+		final Component container = Widgets.get(WIDGET, COMPONENT_ITEMS);
+		if (container == null || !container.isValid()) return false;
+
+		final Component c = item.getComponent();
+		Point p = c.getRelativeLocation();
+		if (p.y == 0) for (int i = 0; i < 5 && getCurrentTab() != 0; i++) if (!setCurrentTab(0)) Delay.sleep(100, 200);
+		if ((p = c.getRelativeLocation()).y == 0) return false;
+		final Rectangle bounds = container.getViewportRect();
+		final Component scroll = Widgets.get(WIDGET, COMPONENT_SCROLLBAR);
+		if (scroll == null) return false;
+		if (!bounds.contains(c.getBoundingRect()) && !Widgets.scroll(c, scroll)) return false;
+		if (!bounds.contains(c.getBoundingRect())) return false;
+
+		String action = "Withdraw-" + amount;
+		if (amount == 0 ||
+				(item.getStackSize() <= amount && amount != 1 && amount != 5 && amount != 10)) action = "Withdraw-All";
+		else if (amount == -1 || amount == (item.getStackSize() - 1)) action = "Withdraw-All but one";
+
+		final int inv = Inventory.getCount(true);
+		if (containsAction(c, action)) {
+			if (!c.interact(action)) return false;
+		} else {
+			if (!c.interact("Withdraw-X")) return false;
+			for (int i = 0; i < 20 && !isInputWidgetOpen(); i++) Delay.sleep(100, 200);
+			if (!isInputWidgetOpen()) return false;
+			Delay.sleep(200, 800);
+			Keyboard.sendln(amount + "");
+		}
+		for (int i = 0; i < 25 && Inventory.getCount(true) == inv; i++) Delay.sleep(100, 200);
+		return Inventory.getCount(true) != inv || Inventory.isFull();
+	}
+
+	public static boolean deposit(final int id, final int amount) {
+		if (!isOpen() || amount < 0) return false;
+		final Item item = Inventory.getItem(id);
+		if (item == null) return false;
+		String action = "Deposit-" + amount;
+		final int c = Inventory.getCount(true, id);
+		if (c == 1) action = "Depoist";
+		else if (c <= amount || amount == 0) {
+			action = "Deposit-All";
+		}
+
+		final Component comp = item.getComponent();
+		final int inv = Inventory.getCount(true);
+		if (containsAction(comp, action)) {
+			if (!comp.interact(action)) return false;
+		} else {
+			if (!comp.interact("Withdraw-X")) return false;
+			for (int i = 0; i < 20 && !isInputWidgetOpen(); i++) Delay.sleep(100, 200);
+			if (!isInputWidgetOpen()) return false;
+			Delay.sleep(200, 800);
+			Keyboard.sendln(amount + "");
+		}
+		for (int i = 0; i < 25 && Inventory.getCount(true) == inv; i++) Delay.sleep(100, 200);
+		return Inventory.getCount(true) != inv;
+	}
+
+	private static boolean containsAction(final Component c, final String action) {
+		final String[] actions = c.getActions();
+		if (action == null) return false;
+		for (final String a : actions) if (a != null && a.matches("^" + action + "(<.*>)?$")) return true;
+		return false;
+	}
+
+	private static boolean isInputWidgetOpen() {
+		final Component child = Widgets.get(752, 3);
+		return child != null && child.isValid() && child.isOnScreen();
 	}
 }
