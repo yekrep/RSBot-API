909d0256b3fd27390f83854f9dd98658a0bc326e
Joe Haddad <timer@powerbot.org>
2013-05-13 00:19:44 -0400

    Documentation
    Documented the Npc utility

--- src/org/powerbot/script/xenon/Npcs.java
+++ src/org/powerbot/script/xenon/Npcs.java
@@ -13,7 +13,17 @@ import org.powerbot.script.xenon.wrappers.Npc;
 import org.powerbot.script.xenon.wrappers.Player;
 import org.powerbot.script.xenon.wrappers.Tile;
 
+/**
+ * A utility which provides access to the loaded non-player actors in the game.
+ *
+ * @author Timer
+ */
 public class Npcs {
+	/**
+	 * Returns all the {@link Npc}s in the region.
+	 *
+	 * @return an array of the loaded {@link Npc}s
+	 */
 	public static Npc[] getLoaded() {
 		final Client client = Bot.client();
 		if (client == null) return new Npc[0];
@@ -34,6 +44,12 @@ public class Npcs {
 		return Arrays.copyOf(npcs, d);
 	}
 
+	/**
+	 * Returns the {@link Npc}s in the region which are accepted by the provided filter.
+	 *
+	 * @param filter the {@link Filter} by which to accept {@link Npc}s
+	 * @return an array of the filtered {@link Npc}s
+	 */
 	public static Npc[] getLoaded(final Filter<Npc> filter) {
 		final Npc[] npcs = getLoaded();
 		final Npc[] set = new Npc[npcs.length];
@@ -42,6 +58,12 @@ public class Npcs {
 		return Arrays.copyOf(set, d);
 	}
 
+	/**
+	 * Returns the {@link Npc}s in the region with the provided id(s).
+	 *
+	 * @param ids a list of ids to accept
+	 * @return an array of the filtered {@link Npc}s
+	 */
 	public static Npc[] getLoaded(final int... ids) {
 		return getLoaded(new Filter<Npc>() {
 			@Override
@@ -53,6 +75,12 @@ public class Npcs {
 		});
 	}
 
+	/**
+	 * Returns the nearest {@link Npc} in the region accepted by the provided {@link Filter}.
+	 *
+	 * @param filter the {@link Filter} by which to accept an {@link Npc}
+	 * @return the {@link Npc} nearest to the local player accepted by the {@link Filter}
+	 */
 	public static Npc getNearest(final Filter<Npc> filter) {
 		Npc nearest = null;
 		double dist = 104d;
@@ -74,6 +102,12 @@ public class Npcs {
 		return nearest;
 	}
 
+	/**
+	 * Returns the nearest {@link Npc} in the region with the given id(s).
+	 *
+	 * @param ids a list of ids to accept
+	 * @return the {@link Npc} nearest to the local player with one of the given ids
+	 */
 	public static Npc getNearest(final int... ids) {
 		return getNearest(new Filter<Npc>() {
 			@Override
