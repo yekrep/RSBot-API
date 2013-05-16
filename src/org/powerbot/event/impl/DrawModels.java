1b693c9594948911a88613bfaa0c938fdb40c918
Joe Haddad <timer@powerbot.org>
2013-05-15 23:56:42 -0400

    Shuffle color scheme and update drawing methods for model debug

--- src/org/powerbot/event/impl/DrawModels.java
+++ src/org/powerbot/event/impl/DrawModels.java
@@ -1,11 +1,10 @@
 package org.powerbot.event.impl;
 
+import java.awt.AlphaComposite;
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
-import java.util.HashMap;
-import java.util.Map;
 
 import org.powerbot.event.PaintListener;
 import org.powerbot.script.xenon.GroundItems;
@@ -19,50 +18,35 @@ import org.powerbot.script.xenon.wrappers.Npc;
 import org.powerbot.script.xenon.wrappers.Player;
 
 public class DrawModels implements PaintListener {
-	private static final Map<GameObject.Type, Color> palette = new HashMap<>();
-
-	static {
-		palette.put(GameObject.Type.BOUNDARY, Color.black);
-		palette.put(GameObject.Type.FLOOR_DECORATION, Color.yellow);
-		palette.put(GameObject.Type.INTERACTIVE, Color.white);
-		palette.put(GameObject.Type.WALL_DECORATION, Color.gray);
-	}
+	private static final Color[] C = {Color.GREEN, Color.WHITE, Color.BLACK, Color.BLUE};
+	private static final float[] A = {0.1f, 0.15f, 1f, 0.2f};
 
 	@Override
 	public void onRepaint(final Graphics render) {
 		((Graphics2D) render).setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-		Model m;
 		final GameObject[] objects = Objects.getLoaded(20);
-		for (final GameObject o : objects) {
-			m = o.getModel();
+		for (final GameObject obj : objects) {
+			final Model m = obj.getModel();
 			if (m == null) continue;
-			final Color c = palette.get(o.getType());
-			render.setColor(new Color(c.getRed(), c.getGreen(), c.getBlue(), 20));
+			final int o = obj.getType().ordinal();
+			((Graphics2D) render).setComposite(AlphaComposite.getInstance(AlphaComposite.SRC_OVER, A[o]));
+			render.setColor(C[o]);
 			m.drawWireFrame(render);
 		}
 
-		render.setColor(new Color(255, 0, 0, 20));
 		final Player[] players = Players.getLoaded();
 		for (final Player actor : players) {
-			m = actor.getModel();
-			if (m == null) continue;
-			m.drawWireFrame(render);
+			actor.draw(render, 0.08f);
 		}
 
-		render.setColor(new Color(225, 0, 225, 20));
 		final Npc[] npcs = Npcs.getLoaded();
 		for (final Npc actor : npcs) {
-			m = actor.getModel();
-			if (m == null) continue;
-			m.drawWireFrame(render);
+			actor.draw(render, 0.08f);
 		}
 
-		render.setColor(new Color(225, 225, 0, 20));
 		final GroundItem[] groundItems = GroundItems.getLoaded(20);
 		for (final GroundItem item : groundItems) {
-			m = item.getModel();
-			if (m == null) continue;
-			m.drawWireFrame(render);
+			item.draw(render, 0.08f);
 		}
 	}
 }
