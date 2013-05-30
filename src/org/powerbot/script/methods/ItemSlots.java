73b5d63a77eebe3bc77826a8c14f22cc347f8fe4
Joe Haddad <timer@powerbot.org>
2013-05-30 02:54:41 -0400

    Items API

--- src/org/powerbot/script/methods/ItemSlots.java
+++
@@ -1,4 +0,0 @@
-package org.powerbot.script.methods;
-
-class ItemSlots {
-}
---
+++ src/org/powerbot/script/methods/Items.java
@@ -0,0 +1,27 @@
+package org.powerbot.script.methods;
+
+import org.powerbot.bot.Bot;
+import org.powerbot.client.Client;
+import org.powerbot.client.HashTable;
+import org.powerbot.client.ItemSlot;
+import org.powerbot.script.wrappers.Item;
+
+public class Items {//you might want to move/hide this class
+	public static final int INDEX_INVENTORY = 0;
+	public static final int INDEX_EQUIPMENT = 0;
+	public static final int INDEX_BANK = 0;
+
+	private static Item[] getItems(int index) {
+		final Client client = Bot.client();
+		final HashTable table;
+		if (client == null || (table = client.getItemSlots()) == null) return new Item[0];
+		Object n = Game.lookup(table, index);
+		if (n == null || !(n instanceof ItemSlot)) return new Item[0];
+		final ItemSlot slot = (ItemSlot) n;
+		int[] ids = slot.getIds(), stacks = slot.getStackSizes();
+		if (ids.length != stacks.length) return new Item[0];
+		Item[] items = new Item[ids.length];
+		for (int i = 0; i < ids.length; i++) items[i] = new Item(ids[i], stacks[i]);
+		return items;
+	}
+}
