0dec48dbee02e3679011648490514183ba24ed69
Joe Haddad <timer@powerbot.org>
2013-06-02 01:57:54 -0400

    Make Deque wrapper safe

--- src/org/powerbot/script/internal/wrappers/Deque.java
+++ src/org/powerbot/script/internal/wrappers/Deque.java
@@ -5,47 +5,49 @@ import org.powerbot.client.NodeDeque;
 
 public class Deque<N> {
 	private final NodeDeque nl;
-	private Node current;
+	private Node curr;
 
 	public Deque(final NodeDeque nl) {
+		if (nl == null) throw new IllegalArgumentException();
 		this.nl = nl;
 	}
 
 	public int size() {
-		int size = 0;
-		Node node = nl.getTail().getNext();
-
-		while (node != null && node != nl.getTail()) {
-			node = node.getNext();
-			size++;
+		int s = 0;
+		Node t = nl.getTail();
+		Node n;
+		if (t != null) n = t.getNext();
+		else n = null;
+		while (n != null && n != t) {
+			n = n.getNext();
+			++s;
 		}
-
-		return size;
+		return s;
 	}
 
 	@SuppressWarnings("unchecked")
 	public N getHead() {
-		Node node = nl.getTail().getNext();
-
-		if (node == null || node == nl.getTail()) {
-			current = null;
+		Node t = nl.getTail();
+		Node n;
+		if (t != null) n = t.getNext();
+		else n = null;
+		if (n == null || n == t) {
+			curr = null;
 			return null;
 		}
-		current = node.getNext();
-
-		return (N) node;
+		curr = n.getNext();
+		return (N) n;
 	}
 
 	@SuppressWarnings("unchecked")
 	public N getNext() {
-		Node node = current;
-
-		if (node == null || node == nl.getTail()) {
-			current = null;
+		Node t = nl.getTail();
+		Node n = curr;
+		if (t == null || n == null || n == t) {
+			curr = null;
 			return null;
 		}
-		current = node.getNext();
-
-		return (N) node;
+		curr = n.getNext();
+		return (N) n;
 	}
 }
