d07f69e1366bc477adadedabc9fd9f5074af9096
Joe Haddad <timer@powerbot.org>
2013-06-02 13:43:48 -0400

    fix

--- src/org/powerbot/script/PollingScript.java
+++ src/org/powerbot/script/PollingScript.java
@@ -13,102 +13,101 @@ import org.powerbot.script.framework.Suspendable;
  * @author Paris
  */
 public abstract class PollingScript extends AbstractScript implements Suspendable, Stoppable {
-    private final AtomicBoolean suspended, stopping;
+	private final AtomicBoolean suspended, stopping;
 
-    public PollingScript() {
-        suspended = new AtomicBoolean(false);
-        stopping = new AtomicBoolean(false);
+	public PollingScript() {
+		suspended = new AtomicBoolean(false);
+		stopping = new AtomicBoolean(false);
 
-        getTasks(State.SUSPEND).add(new FutureTask<>(new Runnable() {
-            @Override
-            public void run() {
-                suspended.set(true);
-                suspend();
-            }
-        }, true));
+		getTasks(State.SUSPEND).add(new FutureTask<>(new Runnable() {
+			@Override
+			public void run() {
+				suspended.set(true);
+				suspend();
+			}
+		}, true));
 
-        getTasks(State.RESUME).add(new FutureTask<>(new Runnable() {
-            @Override
-            public void run() {
-                suspended.set(false);
-                resume();
-            }
-        }, true));
+		getTasks(State.RESUME).add(new FutureTask<>(new Runnable() {
+			@Override
+			public void run() {
+				suspended.set(false);
+				resume();
+			}
+		}, true));
 
-        getTasks(State.STOP).add(new FutureTask<>(new Runnable() {
-            @Override
-            public void run() {
-                stopping.set(true);
-                stop();
-            }
-        }, true));
-    }
+		getTasks(State.STOP).add(new FutureTask<>(new Runnable() {
+			@Override
+			public void run() {
+				stopping.set(true);
+				stop();
+			}
+		}, true));
+	}
 
-    /**
-     * The body of the loop.
-     *
-     * @return the delay in milliseconds before the next call
-     */
-    public abstract int poll();
+	/**
+	 * The body of the loop.
+	 *
+	 * @return the delay in milliseconds before the next call
+	 */
+	public abstract int poll();
 
-    /**
-     * Initiates this {@code Script}.
-     */
-    @Override
-    public final void run() {
-        start();
-        final Script instance = this;
-        getScriptController().getExecutorService().submit(new Runnable() {
-            @Override
-            public void run() {
-                while (!stopping.get()) {
-                    final Queue<Script> queue = getScriptController().getLockQueue();
-                    sleep(Math.max(0, suspended.get() || (!queue.isEmpty() && !queue.contains(instance)) ? 600 : poll()));
-                }
-            }
-        });
-    }
+	/**
+	 * Initiates this {@code Script}.
+	 */
+	@Override
+	public final void run() {
+		start();
+		getScriptController().getExecutorService().submit(new Runnable() {
+			@Override
+			public void run() {
+				while (!stopping.get()) {
+					final Queue<Script> queue = getScriptController().getLockQueue();
+					sleep(Math.max(0, suspended.get() || (!queue.isEmpty() && !queue.contains(PollingScript.this)) ? 600 : poll()));
+				}
+			}
+		});
+	}
 
-    /**
-     * Called when the script is first started.
-     */
-    public void start() {
-    }
+	/**
+	 * Called when the script is first started.
+	 */
+	public void start() {
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public final boolean isSuspended() {
-        return suspended.get();
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public final boolean isSuspended() {
+		return suspended.get();
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public synchronized void suspend() {
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public synchronized void suspend() {
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public synchronized void resume() {
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public synchronized void resume() {
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public final boolean isStopping() {
-        return stopping.get();
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public final boolean isStopping() {
+		return stopping.get();
+	}
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public synchronized void stop() {
-    }
+	/**
+	 * {@inheritDoc}
+	 */
+	@Override
+	public synchronized void stop() {
+	}
 }
