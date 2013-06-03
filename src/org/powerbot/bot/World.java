d8654c7693ee0652ff7e2e8179c6145140e34300
Joe Haddad <timer@powerbot.org>
2013-06-03 03:55:53 -0400

    make client an atomic reference

--- src/org/powerbot/bot/World.java
+++ src/org/powerbot/bot/World.java
@@ -1,18 +1,20 @@
 package org.powerbot.bot;
 
+import java.util.concurrent.atomic.AtomicReference;
+
 import org.powerbot.client.Client;
 import org.powerbot.script.methods.Game;
 
 public class World {
 	private static final World EMPTY = new World();
-	private Client client;
-	private Game.Toolkit toolkit;
-	private Game.Viewport viewport;
+	private final Game.Toolkit toolkit;
+	private final Game.Viewport viewport;
+	private AtomicReference<Client> client;
 
 	public World() {
-		this.client = null;
 		this.toolkit = new Game.Toolkit();
 		this.viewport = new Game.Viewport();
+		this.client = new AtomicReference<>(null);
 	}
 
 	public static World getWorld() {
@@ -21,11 +23,11 @@ public class World {
 	}
 
 	public void setClient(Client client) {
-		this.client = client;
+		this.client.set(client);
 	}
 
 	public Client getClient() {
-		return this.client;
+		return this.client.get();
 	}
 
 	public Game.Toolkit getToolkit() {
