5e8538302c7af60f4ab7a30d503e5ea92d20a61e
A. Ahmed <paris@powerbot.org>
2013-06-13 01:49:25 +0100

    Add filters and sorters

--- src/org/powerbot/script/wrappers/Identifiable.java
+++ src/org/powerbot/script/wrappers/Identifiable.java
@@ -1,5 +1,33 @@
 package org.powerbot.script.wrappers;
 
+import org.powerbot.script.lang.AbstractQuery;
+
 public interface Identifiable {
 	public int getId();
+
+	public class Matcher implements AbstractQuery.Filter<Identifiable> {
+		private final int[] ids;
+
+		public Matcher(final int... ids) {
+			this.ids = ids;
+		}
+
+		public Matcher(final Identifiable... ids) {
+			this.ids = new int[ids.length];
+			for (int i = 0; i < ids.length; i++) {
+				this.ids[i] = ids[i].getId();
+			}
+		}
+
+		@Override
+		public boolean accept(final Identifiable i) {
+			final int x = i.getId();
+			for (int id : ids) {
+				if (id == x) {
+					return true;
+				}
+			}
+			return false;
+		}
+	}
 }
--- src/org/powerbot/script/wrappers/Locatable.java
+++ src/org/powerbot/script/wrappers/Locatable.java
@@ -1,5 +1,52 @@
 package org.powerbot.script.wrappers;
 
+import java.util.Comparator;
+
+import org.powerbot.script.lang.AbstractQuery;
+
 public interface Locatable {
 	public Tile getLocation();
+
+	public class Matcher implements AbstractQuery.Filter<Locatable> {
+		private final Locatable target;
+
+		public Matcher(final Locatable target) {
+			this.target = target;
+		}
+
+		@Override
+		public boolean accept(final Locatable l) {
+			return l.getLocation().equals(target);
+		}
+	}
+
+	public class WithinRange implements AbstractQuery.Filter<Locatable> {
+		private final Locatable target;
+		private final double distance;
+
+		public WithinRange(final Locatable target, final double distance) {
+			this.target = target;
+			this.distance = distance;
+		}
+
+		@Override
+		public boolean accept(final Locatable l) {
+			return l.getLocation().distanceTo(target.getLocation()) <= distance;
+		}
+	}
+
+	public class NearestTo implements Comparator<Locatable> {
+		private final Locatable target;
+
+		public NearestTo(final Locatable target) {
+			this.target = target;
+		}
+
+		@Override
+		public int compare(final Locatable o1, final Locatable o2) {
+			final Tile t = target.getLocation();
+			final double d1 = o1.getLocation().distanceTo(t), d2 = o2.getLocation().distanceTo(t);
+			return Double.compare(d1, d2);
+		}
+	}
 }
