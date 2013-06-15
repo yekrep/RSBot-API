4dc9e931e083b18cd031ec99d3ae4eca58c58a30
Joe Haddad <timer@powerbot.org>
2013-06-15 07:13:35 -0400

    polling passive

---
+++ src/org/powerbot/script/internal/randoms/PollingPassive.java
@@ -0,0 +1,24 @@
+package org.powerbot.script.internal.randoms;
+
+import org.powerbot.event.PaintListener;
+import org.powerbot.script.internal.ScriptContainer;
+import org.powerbot.script.lang.Validatable;
+import org.powerbot.script.methods.ClientFactory;
+import org.powerbot.script.task.PollingTask;
+
+import java.awt.Graphics;
+
+public abstract class PollingPassive extends PollingTask implements Validatable, PaintListener {
+	public PollingPassive(ClientFactory ctx, ScriptContainer container) {
+		super(ctx, container);
+	}
+
+	@Override
+	public final boolean isSuspended() {
+		return false;
+	}
+
+	@Override
+	public void onRepaint(Graphics render) {
+	}
+}
