84d654d5379ec3b3c223dcfae53d3ccbede652fc
Joe Haddad <timer@powerbot.org>
2013-06-15 07:12:48 -0400

    Sleep helper methods

--- src/org/powerbot/script/methods/ClientLink.java
+++ src/org/powerbot/script/methods/ClientLink.java
@@ -1,9 +1,33 @@
 package org.powerbot.script.methods;
 
+import org.powerbot.script.util.Random;
+
 public class ClientLink {
 	public ClientFactory ctx;
 
 	public ClientLink(ClientFactory factory) {
 		this.ctx = factory;
+	}
+
+	/**
+	 * Sleeps for the specified duration.
+	 *
+	 * @param millis the duration in milliseconds.
+	 */
+	public void sleep(final int millis) {
+		try {
+			Thread.sleep(millis);
+		} catch (final InterruptedException ignored) {
+		}
+	}
+
+	/**
+	 * Sleeps for a random duration between the specified intervals.
+	 *
+	 * @param min the minimum duration (inclusive)
+	 * @param max the maximum duration (exclusive)
+	 */
+	public void sleep(final int min, final int max) {
+		sleep(Random.nextInt(min, max));
 	}
 }
