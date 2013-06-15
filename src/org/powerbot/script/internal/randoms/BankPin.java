d7e5bbe829a8c5d01769ad2bceb9e8678a7a7040
Joe Haddad <timer@powerbot.org>
2013-06-15 07:13:21 -0400

    Convert Bank Pin

--- src/org/powerbot/script/internal/randoms/BankPin.java
+++ src/org/powerbot/script/internal/randoms/BankPin.java
@@ -1,32 +1,40 @@
 package org.powerbot.script.internal.randoms;
 
 import org.powerbot.script.Manifest;
-import org.powerbot.script.PollingScript;
-import org.powerbot.script.util.Delay;
+import org.powerbot.script.internal.ScriptContainer;
+import org.powerbot.script.methods.ClientFactory;
 import org.powerbot.script.util.Random;
 import org.powerbot.script.wrappers.Component;
 import org.powerbot.util.Tracker;
 
 @Manifest(name = "Bank Pin", authors = {"Timer"}, description = "Enters the stored bank pin")
-public class BankPin extends PollingScript implements RandomEvent {
+public class BankPin extends PollingPassive {
+	public BankPin(ClientFactory ctx, ScriptContainer container) {
+		super(ctx, container);
+	}
+
+	@Override
+	public boolean isValid() {
+		Component pinInterface = ctx.widgets.get(13, 0);
+		return pinInterface != null && pinInterface.isVisible();
+	}
+
 	@Override
 	public int poll() {
-		final Component pinInterface = ctx.widgets.get(13, 0);
+		Component pinInterface = ctx.widgets.get(13, 0);
 		if (pinInterface == null || !pinInterface.isVisible()) {
-			return 600;
+			return -1;
 		}
 		Tracker.getInstance().trackPage("randoms/BankPin/", "");
-		final String _pin = getPin();
+		String _pin = getPin();
 		if (_pin == null) {
 			getContainer().stop();
 			return -1;
 		}
-		final String pin = String.format(_pin);
-		final int value = Integer.valueOf(String.valueOf(pin.charAt(ctx.settings.get(163))));
-		if (ctx.widgets.get(13, value + 6).interact("Select")) {
-			Delay.sleep(Random.nextInt(700, 1200));
-		}
-		return 0;
+		String pin = String.format(_pin);
+		int value = Integer.valueOf(String.valueOf(pin.charAt(ctx.settings.get(163))));
+		ctx.widgets.get(13, value + 6).interact("Select");
+		return Random.nextInt(700, 1200);
 	}
 
 	private String getPin() {
