406573a87da9f7d8db16c8279fe35765c9b6510d
Joe Haddad <timer@powerbot.org>
2013-06-20 02:43:32 -0400

    TicketDestroy overrides isValid

--- src/org/powerbot/script/internal/randoms/TicketDestroy.java
+++ src/org/powerbot/script/internal/randoms/TicketDestroy.java
@@ -15,58 +15,62 @@ import org.powerbot.util.Tracker;
 @Manifest(name = "Spin ticket destroyer", authors = {"Timer"}, description = "Claims or destroys spin tickets")
 public class TicketDestroy extends PollingScript implements InternalScript {
 	private static final int[] ITEM_IDS = {24154, 24155};
-	private Item item;
+	private Component item;
 
 	@Override
-	public int poll() {
+	public boolean isValid() {
 		if (!ctx.game.isLoggedIn() || ctx.game.getCurrentTab() != Game.TAB_INVENTORY) {
-			return -1;
+			return false;
 		}
 
 		final Player player;
 		if ((player = ctx.players.getLocal()) == null ||
 				player.isInCombat() || player.getAnimation() != -1 || player.getInteracting() != null) {
-			return -1;
+			return false;
 		}
 
-		Component child = null;
+		this.item = null;
 		for (final Item item : ctx.inventory.select().id(ITEM_IDS).first()) {
-			child = item.getComponent();
+			this.item = item.getComponent();
+			return this.item.isValid();
 		}
+		return false;
+	}
 
-		if (child == null || !item.isValid()) {
-			return -1;
-		}
-
+	@Override
+	public int poll() {
 		Tracker.getInstance().trackPage("randoms/TicketDestroy/", "");
 
-		if (((ctx.settings.get(1448) & 0xFF00) >>> 8) < (child.getItemId() == ITEM_IDS[0] ? 10 : 9)) {
-			child.interact("Claim spin");
+		if (((ctx.settings.get(1448) & 0xFF00) >>> 8) < (item.getItemId() == ITEM_IDS[0] ? 10 : 9)) {
+			item.interact("Claim spin");
 			sleep(1000, 2000);
 		}
 
-		if (!child.interact("Destroy")) {
+		if (!item.interact("Destroy")) {
 			return Random.nextInt(1000, 2000);
 		}
 
+		Widget widget = ctx.widgets.get(1183);
 		final Timer timer = new Timer(Random.nextInt(4000, 6000));
-		while (timer.isRunning()) {
-			final Widget widget = ctx.widgets.get(1183);
-			if (widget != null && widget.isValid()) {
-				for (final Component c : widget.getComponents()) {
-					final String s;
-					if (c.isVisible() && (s = c.getTooltip()) != null && s.trim().equalsIgnoreCase("destroy")) {
-						if (c.interact("Destroy")) {
-							final Timer t = new Timer(Random.nextInt(1500, 2000));
-							while (t.isRunning() && child.getItemId() != -1) {
-								sleep(100, 250);
-							}
-						}
-					}
-				}
+		while (timer.isRunning() && !widget.isValid()) {
+			sleep(100, 200);
+		}
+		if (!widget.isValid()) {
+			return -1;
+		}
+		Component component = null;
+		for (final Component c : widget.getComponents()) {
+			if (c.isVisible() && c.getTooltip().trim().equalsIgnoreCase("destroy")) {
+				component = c;
+				break;
 			}
 		}
-
+		if (component != null && component.interact("Destroy")) {
+			final Timer t = new Timer(Random.nextInt(1500, 2000));
+			while (t.isRunning() && item.getItemId() != -1) {
+				sleep(100, 250);
+			}
+		}
 		return -1;
 	}
 
