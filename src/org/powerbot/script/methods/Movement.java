c001f9f6d5b7eada0cb6d5e434f62a341cbe8c2f
Joe Haddad <timer@powerbot.org>
2013-07-02 00:37:00 -0400

    pathDistance helper in movement

--- src/org/powerbot/script/methods/Movement.java
+++ src/org/powerbot/script/methods/Movement.java
@@ -1,6 +1,8 @@
 package org.powerbot.script.methods;
 
 import org.powerbot.client.Client;
+import org.powerbot.script.internal.wrappers.ClippingMap;
+import org.powerbot.script.internal.wrappers.ClippingValue;
 import org.powerbot.script.lang.Locatable;
 import org.powerbot.script.lang.Targetable;
 import org.powerbot.script.util.Delay;
@@ -110,5 +112,152 @@ public class Movement extends MethodProvider {
 				location.getY() + (int) (16d * Math.sin(angle)),
 				tile.getPlane()
 		);
+	}
+
+	public int pathDistance(Tile start, Tile end, final boolean findAdjacent) {
+		ClippingMap[] maps = ctx.map.getPlanes();
+		int plane = ctx.game.getPlane();
+		if (plane < 0 || plane >= maps.length) return -1;
+		ClippingMap map = maps[plane];
+		if (map == null) return -1;
+
+		Tile base = ctx.game.getMapBase();
+		if (base == Tile.NIL || start == Tile.NIL || end == Tile.NIL) return -1;
+		start = start.derive(-base.x, -base.y);
+		end = end.derive(-base.x, -base.y);
+
+		int startX = start.getX(), startY = start.getY();
+		int endX = end.getX(), endY = end.getY();
+		int xSize = map.getSizeX() - 6, ySize = map.getSizeY() - 6;
+		ClippingValue[][] values = new ClippingValue[xSize][ySize];
+		for (int mX = 0; mX < xSize; mX++) {
+			for (int mY = 0; mY < ySize; mY++) {
+				values[mX][mY] = map.getClippingValueAtLocal(mX, mY);
+			}
+		}
+		int[][] blocks = new int[xSize][ySize];
+		for (int mX = 0; mX < xSize; mX++) {
+			for (int mY = 0; mY < ySize; mY++) {
+				blocks[mX][mY] = values[mX][mY].getType();
+			}
+		}
+
+		final int[][] prev = new int[xSize][ySize];
+		final int[][] dist = new int[xSize][ySize];
+		for (int xx = 0; xx < xSize; xx++) {
+			for (int yy = 0; yy < ySize; yy++) {
+				prev[xx][yy] = 0;
+				dist[xx][yy] = Integer.MAX_VALUE;
+			}
+		}
+		final int[] path_x = new int[4000];
+		final int[] path_y = new int[4000];
+		int curr_x = startX;
+		int curr_y = startY;
+		prev[startX][startY] = 99;
+		dist[startX][startY] = 0;
+		int path_ptr = 0;
+		int step_ptr = 0;
+		path_x[path_ptr] = startX;
+		path_y[path_ptr++] = startY;
+		final int pathLength = path_x.length;
+		boolean foundPath = false;
+		while (step_ptr != path_ptr) {
+			curr_x = path_x[step_ptr];
+			curr_y = path_y[step_ptr];
+			if (Math.abs(curr_x - endX) + Math.abs(curr_y - endY) == (findAdjacent ? 1 : 0)) {
+				foundPath = true;
+				break;
+			}
+			step_ptr = (step_ptr + 1) % pathLength;
+			final int cost = dist[curr_x][curr_y] + 1;
+			// south
+			if (curr_y > 0 &&
+					prev[curr_x][curr_y - 1] == 0 &&
+					!values[curr_x][curr_y].hit(ClippingValue.SOUTH) &&
+					!values[curr_x][curr_y - 1].hit(ClippingValue.OBJECT_TILE.mark(ClippingValue.OBJECT_BLOCK))) {
+				path_x[path_ptr] = curr_x;
+				path_y[path_ptr] = curr_y - 1;
+				path_ptr = (path_ptr + 1) % pathLength;
+				prev[curr_x][curr_y - 1] = 1;
+				dist[curr_x][curr_y - 1] = cost;
+			}
+			// west
+			if (curr_x > 0 &&
+					prev[curr_x - 1][curr_y] == 0 &&
+					!values[curr_x][curr_y].hit(ClippingValue.WEST) &&
+					!values[curr_x - 1][curr_y].hit(ClippingValue.OBJECT_TILE.mark(ClippingValue.OBJECT_BLOCK))) {
+				path_x[path_ptr] = curr_x - 1;
+				path_y[path_ptr] = curr_y;
+				path_ptr = (path_ptr + 1) % pathLength;
+				prev[curr_x - 1][curr_y] = 2;
+				dist[curr_x - 1][curr_y] = cost;
+			}
+			// north
+			if (curr_y < 104 - 1 && prev[curr_x][curr_y + 1] == 0 &&
+					!values[curr_x][curr_y].hit(ClippingValue.NORTH) &&
+					!values[curr_x][curr_y + 1].hit(ClippingValue.OBJECT_TILE.mark(ClippingValue.OBJECT_BLOCK))) {
+				path_x[path_ptr] = curr_x;
+				path_y[path_ptr] = curr_y + 1;
+				path_ptr = (path_ptr + 1) % pathLength;
+				prev[curr_x][curr_y + 1] = 4;
+				dist[curr_x][curr_y + 1] = cost;
+			}
+			// east
+			if (curr_x < 104 - 1 && prev[curr_x + 1][curr_y] == 0 &&
+					!values[curr_x][curr_y].hit(ClippingValue.EAST) &&
+					!values[curr_x + 1][curr_y].hit(ClippingValue.OBJECT_TILE.mark(ClippingValue.OBJECT_BLOCK))) {
+				path_x[path_ptr] = curr_x + 1;
+				path_y[path_ptr] = curr_y;
+				path_ptr = (path_ptr + 1) % pathLength;
+				prev[curr_x + 1][curr_y] = 8;
+				dist[curr_x + 1][curr_y] = cost;
+			}
+			if (curr_x > 0 && curr_y > 0 && prev[curr_x - 1][curr_y - 1] == 0 &&
+					!values[curr_x][curr_y].hit(ClippingValue.SOUTHWEST.mark(ClippingValue.SOUTH.mark(ClippingValue.WEST))) &&
+					!values[curr_x - 1][curr_y - 1].hit(ClippingValue.OBJECT_TILE.mark(ClippingValue.OBJECT_BLOCK)) &&
+					!values[curr_x][curr_y - 1].hit(ClippingValue.WEST.mark(ClippingValue.OBJECT_TILE.mark(ClippingValue.OBJECT_BLOCK))) &&
+					!values[curr_x - 1][curr_y].hit(ClippingValue.SOUTH.mark(ClippingValue.OBJECT_TILE.mark(ClippingValue.OBJECT_BLOCK)))) {
+				path_x[path_ptr] = curr_x - 1;
+				path_y[path_ptr] = curr_y - 1;
+				path_ptr = (path_ptr + 1) % pathLength;
+				prev[curr_x - 1][curr_y - 1] = 3;
+				dist[curr_x - 1][curr_y - 1] = cost;
+			}
+			if (curr_x > 0 && curr_y < ySize - 1 && prev[curr_x - 1][curr_y + 1] == 0 &&
+					!values[curr_x][curr_y].hit(ClippingValue.NORTHWEST.mark(ClippingValue.NORTH.mark(ClippingValue.WEST))) &&
+					!values[curr_x - 1][curr_y + 1].hit(ClippingValue.OBJECT_TILE.mark(ClippingValue.OBJECT_BLOCK)) &&
+					!values[curr_x][curr_y + 1].hit(ClippingValue.WEST.mark(ClippingValue.OBJECT_TILE.mark(ClippingValue.OBJECT_BLOCK))) &&
+					!values[curr_x - 1][curr_y].hit(ClippingValue.NORTH.mark(ClippingValue.OBJECT_TILE.mark(ClippingValue.OBJECT_BLOCK)))) {
+				path_x[path_ptr] = curr_x - 1;
+				path_y[path_ptr] = curr_y + 1;
+				path_ptr = (path_ptr + 1) % pathLength;
+				prev[curr_x - 1][curr_y + 1] = 6;
+				dist[curr_x - 1][curr_y + 1] = cost;
+			}
+			if (curr_x < ySize - 1 && curr_y > 0 && prev[curr_x - 1][curr_y - 1] == 0 &&
+					!values[curr_x][curr_y].hit(ClippingValue.SOUTHEAST.mark(ClippingValue.SOUTH.mark(ClippingValue.EAST))) &&
+					!values[curr_x + 1][curr_y - 1].hit(ClippingValue.OBJECT_TILE.mark(ClippingValue.OBJECT_BLOCK)) &&
+					!values[curr_x][curr_y - 1].hit(ClippingValue.EAST.mark(ClippingValue.OBJECT_TILE.mark(ClippingValue.OBJECT_BLOCK))) &&
+					!values[curr_x + 1][curr_y].hit(ClippingValue.SOUTH.mark(ClippingValue.OBJECT_TILE.mark(ClippingValue.OBJECT_BLOCK)))) {
+				path_x[path_ptr] = curr_x + 1;
+				path_y[path_ptr] = curr_y - 1;
+				path_ptr = (path_ptr + 1) % pathLength;
+				prev[curr_x + 1][curr_y - 1] = 9;
+				dist[curr_x + 1][curr_y - 1] = cost;
+			}
+			if (curr_x < xSize - 1 && curr_y < ySize - 1 && prev[curr_x + 1][curr_y + 1] == 0 &&
+					!values[curr_x][curr_y].hit(ClippingValue.NORTHEAST.mark(ClippingValue.NORTH.mark(ClippingValue.WEST))) &&
+					!values[curr_x + 1][curr_y + 1].hit(ClippingValue.OBJECT_TILE.mark(ClippingValue.OBJECT_BLOCK)) &&
+					!values[curr_x][curr_y + 1].hit(ClippingValue.EAST.mark(ClippingValue.OBJECT_TILE.mark(ClippingValue.OBJECT_BLOCK))) &&
+					!values[curr_x + 1][curr_y].hit(ClippingValue.NORTH.mark(ClippingValue.OBJECT_TILE.mark(ClippingValue.OBJECT_BLOCK)))) {
+				path_x[path_ptr] = curr_x + 1;
+				path_y[path_ptr] = curr_y + 1;
+				path_ptr = (path_ptr + 1) % pathLength;
+				prev[curr_x + 1][curr_y + 1] = 12;
+				dist[curr_x + 1][curr_y + 1] = cost;
+			}
+		}
+		return foundPath ? dist[curr_x][curr_y] : -1;
 	}
 }
