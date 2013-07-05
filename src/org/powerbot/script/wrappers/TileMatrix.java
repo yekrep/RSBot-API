738f985babb7c3aff8ec729e274401c8a3d04807
Joe Haddad <timer@powerbot.org>
2013-07-05 16:31:52 -0400

    Add getBounds helper in Tile

--- src/org/powerbot/script/wrappers/TileMatrix.java
+++ src/org/powerbot/script/wrappers/TileMatrix.java
@@ -30,6 +30,18 @@ public final class TileMatrix extends Interactive implements Locatable, Drawable
 		return base != null ? ctx.game.groundToScreen((int) ((tile.x - base.x + modX) * 512d), (int) ((tile.y - base.y + modY) * 512d), tile.plane, height) : new Point(-1, -1);
 	}
 
+	public Polygon getBounds() {
+		Point tl = getPoint(0.0D, 0.0D, 0);
+		Point tr = getPoint(1.0D, 0.0D, 0);
+		Point br = getPoint(1.0D, 1.0D, 0);
+		Point bl = getPoint(0.0D, 1.0D, 0);
+		return new Polygon(
+				new int[]{tl.x, tr.x, br.x, bl.x},
+				new int[]{tl.y, tr.y, br.y, bl.y},
+				4
+		);
+	}
+
 	public Point getMapPoint() {
 		return ctx.game.tileToMap(tile.getX() + 0.5d, tile.getY() + 0.5d);
 	}
@@ -50,12 +62,16 @@ public final class TileMatrix extends Interactive implements Locatable, Drawable
 
 	@Override
 	public boolean isOnScreen() {
-		Point topLeft = getPoint(0.0D, 0.0D, 0);
-		Point topRight = getPoint(1.0D, 0.0D, 0);
-		Point bottomRight = getPoint(1.0D, 1.0D, 0);
-		Point bottomLeft = getPoint(0.0D, 1.0D, 0);
-		return ctx.game.isPointOnScreen(topLeft) && ctx.game.isPointOnScreen(topRight) &&
-				ctx.game.isPointOnScreen(bottomRight) && ctx.game.isPointOnScreen(bottomLeft);
+		return isPolygonOnScreen(getBounds());
+	}
+
+	private boolean isPolygonOnScreen(Polygon p) {
+		for (int i = 0; i < p.npoints; i++) {
+			if (!ctx.game.isPointOnScreen(p.xpoints[i], p.ypoints[i])) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 	@Override
@@ -77,20 +93,8 @@ public final class TileMatrix extends Interactive implements Locatable, Drawable
 
 	@Override
 	public boolean contains(final Point point) {
-		Point topLeft = getPoint(0.0D, 0.0D, 0);
-		Point topRight = getPoint(1.0D, 0.0D, 0);
-		Point bottomRight = getPoint(1.0D, 1.0D, 0);
-		Point bottomLeft = getPoint(0.0D, 1.0D, 0);
-		if (ctx.game.isPointOnScreen(topLeft) && ctx.game.isPointOnScreen(topRight) &&
-				ctx.game.isPointOnScreen(bottomRight) && ctx.game.isPointOnScreen(bottomLeft)) {
-			final Polygon p = new Polygon();
-			p.addPoint(topLeft.x, topLeft.y);
-			p.addPoint(topRight.x, topRight.y);
-			p.addPoint(bottomRight.x, bottomRight.y);
-			p.addPoint(bottomLeft.x, bottomLeft.y);
-			return p.contains(point);
-		}
-		return false;
+		Polygon p = getBounds();
+		return isPolygonOnScreen(p) && p.contains(point);
 	}
 
 	@Override
@@ -110,26 +114,19 @@ public final class TileMatrix extends Interactive implements Locatable, Drawable
 
 	@Override
 	public void draw(final Graphics render, final int alpha) {
+		Polygon p = getBounds();
+		if (!isPolygonOnScreen(p)) {
+			return;
+		}
+
 		Color c = Tile.TARGET_COLOR;
 		final int rgb = c.getRGB();
 		if (((rgb >> 24) & 0xff) != alpha) {
 			c = new Color((rgb >> 16) & 0xff, (rgb >> 8) & 0xff, rgb & 0xff, alpha);
 		}
 		render.setColor(c);
-		final Point topLeft = getPoint(0.0D, 0.0D, 0);
-		final Point topRight = getPoint(1.0D, 0.0D, 0);
-		final Point bottomRight = getPoint(1.0D, 1.0D, 0);
-		final Point bottomLeft = getPoint(0.0D, 1.0D, 0);
-		if (ctx.game.isPointOnScreen(topLeft) && ctx.game.isPointOnScreen(topRight) &&
-				ctx.game.isPointOnScreen(bottomRight) && ctx.game.isPointOnScreen(bottomLeft)) {
-			final Polygon p = new Polygon();
-			p.addPoint(topLeft.x, topLeft.y);
-			p.addPoint(topRight.x, topRight.y);
-			p.addPoint(bottomRight.x, bottomRight.y);
-			p.addPoint(bottomLeft.x, bottomLeft.y);
-			render.drawPolygon(p);
-			render.setColor(new Color(0, 0, 0, 20));
-			render.fillPolygon(p);
-		}
+		render.drawPolygon(p);
+		render.setColor(new Color(0, 0, 0, 20));
+		render.fillPolygon(p);
 	}
 }
