46912e1382183787a5ab2e961312650c9deb67e1
Joe Haddad <timer@powerbot.org>
2013-07-10 05:44:49 -0400

    Remove test code

--- src/org/powerbot/bot/nloader/ClassLoaderTransform.java
+++ src/org/powerbot/bot/nloader/ClassLoaderTransform.java
@@ -1,53 +1,23 @@
 package org.powerbot.bot.nloader;
 
-import java.util.ListIterator;
-
-import org.objectweb.asm.Opcodes;
-import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
-import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 
 public class ClassLoaderTransform implements Transform {
+	private final String super_;
 
-	/**
-	 * TARGET: defineClass
-	 * class extends ClassLoader
-	 * aload_0
-	 * [
-	 * aload (String name)
-	 * aload (byte[] b)
-	 * iload (int off)
-	 * iload (int len)
-	 * aload (ProtectionDomain protectionDomain)
-	 * ]
-	 * invokevirtual class/defineClass(Ljava/lang/String;[BIILjava/security/ProtectionDomain;)Ljava/lang/Class;
-	 * pop
-	 */
+	public ClassLoaderTransform() {
+		this.super_ = ClassLoader.class.getName().replace('.', '/');
+	}
+
 	@Override
 	public void accept(ClassNode node) {
-		//TODO: implement bytecode-modification
-		System.out.println(node.name + " (super " + node.superName + ")");
-		if (node.superName != null && node.superName.equals(ClassLoader.class.getName().replace('.', '/'))) {
-			System.out.println("Found our class!");
-			System.out.println("Methods dump: ");
-			for (MethodNode methodNode : node.methods) {
-				System.out.println(methodNode.name + methodNode.desc);
-				ListIterator<AbstractInsnNode> iterator = methodNode.instructions.iterator();
-				while (iterator.hasNext()) {
-					AbstractInsnNode abstractInsnNode = iterator.next();
-					if (abstractInsnNode instanceof MethodInsnNode) {
-						MethodInsnNode methodInsnNode = (MethodInsnNode) abstractInsnNode;
-						if (methodInsnNode.getOpcode() == Opcodes.INVOKEVIRTUAL &&
-								methodInsnNode.name.equals("defineClass")) {
-							System.out.println("=====");
-							System.out.println(methodInsnNode.name + methodInsnNode.desc);
-							System.out.println("=====");
-							break;
-						}
-					}
-				}
-			}
+		String super_ = node.superName;
+		if (super_ == null || !super_.equals(this.super_)) {
+			return;
+		}
+		for (MethodNode method : node.methods) {
+			//TODO find defineClass invoke and inject before it
 		}
 	}
 }
