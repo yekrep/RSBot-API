4a6effdad3e95779241524002e1c65cb543183f9
Joe Haddad <timer@powerbot.org>
2013-07-10 05:06:03 -0400

    Processor now requires Transform array

--- src/org/powerbot/bot/nloader/AbstractProcessor.java
+++ src/org/powerbot/bot/nloader/AbstractProcessor.java
@@ -5,9 +5,18 @@ import org.objectweb.asm.ClassWriter;
 import org.objectweb.asm.tree.ClassNode;
 
 public class AbstractProcessor implements Processor {
+	private Transform[] transforms;
+
+	public AbstractProcessor(Transform[] transforms) {
+		this.transforms = transforms;
+	}
+
 	@Override
 	public byte[] transform(byte[] b) {
 		ClassNode node = getNode(b);
+		for (Transform transform : transforms) {
+			transform.accept(node);
+		}
 		return getCode(node);
 	}
 
