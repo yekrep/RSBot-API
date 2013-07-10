a78b310be9d1a98b4b03cfdb702e645d8f59d829
Joe Haddad <timer@powerbot.org>
2013-07-10 05:06:40 -0400

    Remove old Hook class

--- src/org/powerbot/bot/nloader/ClassLoaderTransform.java
+++ src/org/powerbot/bot/nloader/ClassLoaderTransform.java
@@ -3,6 +3,21 @@ package org.powerbot.bot.nloader;
 import org.objectweb.asm.tree.ClassNode;
 
 public class ClassLoaderTransform implements Transform {
+
+	/**
+	 * TARGET: defineClass
+	 * class extends ClassLoader
+	 * aload_0
+	 * [
+	 * aload (String name)
+	 * aload (byte[] b)
+	 * iload (int off)
+	 * iload (int len)
+	 * aload (ProtectionDomain protectionDomain)
+	 * ]
+	 * invokevirtual class/defineClass(Ljava/lang/String;[BIILjava/security/ProtectionDomain;)Ljava/lang/Class;
+	 * pop
+	 */
 	@Override
 	public void accept(ClassNode node) {
 		//TODO: implement bytecode-modification
