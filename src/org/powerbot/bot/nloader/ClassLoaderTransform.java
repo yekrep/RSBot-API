3ca55ef7079f5b890a5de856637197f5a516d4f2
Joe Haddad <timer@powerbot.org>
2013-07-10 18:51:09 -0400

    Search methods for specific opcode instructions

--- src/org/powerbot/bot/nloader/ClassLoaderTransform.java
+++ src/org/powerbot/bot/nloader/ClassLoaderTransform.java
@@ -1,6 +1,9 @@
 package org.powerbot.bot.nloader;
 
+import org.objectweb.asm.Opcodes;
+import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
 
 public class ClassLoaderTransform implements Transform {
@@ -16,7 +19,19 @@ public class ClassLoaderTransform implements Transform {
 		if (super_ == null || !super_.equals(this.super_)) {
 			return;
 		}
+		final int[] ops = {
+				Opcodes.AALOAD, Opcodes.AALOAD, Opcodes.ILOAD, Opcodes.ILOAD, Opcodes.AALOAD,
+				Opcodes.INVOKEVIRTUAL
+		};
 		for (MethodNode method : node.methods) {
+			InsnSearcher searcher = new InsnSearcher(method);
+			while (searcher.getNext(ops) != null) {
+				AbstractInsnNode abstractInsnNode = searcher.current();
+				MethodInsnNode methodInsnNode = (MethodInsnNode) abstractInsnNode;
+				if (methodInsnNode.name.equals("defineClass")) {
+					System.out.println("Found defineClass" + methodInsnNode.desc);
+				}
+			}
 			//TODO find defineClass invoke and inject before it
 		}
 	}
