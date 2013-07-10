7f61127604e755bc9744c3a4e45ab31f94362591
Joe Haddad <timer@powerbot.org>
2013-07-10 04:57:01 -0400

    Barebones Processor and AbstractProcessor.

---
+++ src/org/powerbot/bot/nloader/AbstractProcessor.java
@@ -0,0 +1,26 @@
+package org.powerbot.bot.nloader;
+
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.ClassWriter;
+import org.objectweb.asm.tree.ClassNode;
+
+public class AbstractProcessor implements Processor {
+	@Override
+	public byte[] transform(byte[] b) {
+		ClassNode node = getNode(b);
+		return getCode(node);
+	}
+
+	private ClassNode getNode(byte[] bytes) {
+		ClassNode node = new ClassNode();
+		ClassReader reader = new ClassReader(bytes);
+		reader.accept(node, ClassReader.SKIP_FRAMES | ClassReader.SKIP_DEBUG);
+		return node;
+	}
+
+	private byte[] getCode(ClassNode node) {
+		ClassWriter writer = new ClassWriter(0);
+		node.accept(writer);
+		return writer.toByteArray();
+	}
+}
---
+++ src/org/powerbot/bot/nloader/Processor.java
@@ -0,0 +1,5 @@
+package org.powerbot.bot.nloader;
+
+public interface Processor {
+	public byte[] transform(byte[] b);
+}
