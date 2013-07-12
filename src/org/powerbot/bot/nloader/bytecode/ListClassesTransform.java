f6eacffdb8f245acf7fe6be81e219266323bea0e
Joe Haddad <timer@powerbot.org>
2013-07-12 11:00:45 -0400

    Require value to be [B in key,value map

--- src/org/powerbot/bot/nloader/bytecode/ListClassesTransform.java
+++ src/org/powerbot/bot/nloader/bytecode/ListClassesTransform.java
@@ -5,8 +5,11 @@ import org.objectweb.asm.tree.AbstractInsnNode;
 import org.objectweb.asm.tree.ClassNode;
 import org.objectweb.asm.tree.FieldInsnNode;
 import org.objectweb.asm.tree.InsnList;
+import org.objectweb.asm.tree.JumpInsnNode;
+import org.objectweb.asm.tree.LabelNode;
 import org.objectweb.asm.tree.MethodInsnNode;
 import org.objectweb.asm.tree.MethodNode;
+import org.objectweb.asm.tree.TypeInsnNode;
 import org.objectweb.asm.tree.VarInsnNode;
 import org.powerbot.bot.nloader.Bridge;
 
@@ -33,21 +36,30 @@ public class ListClassesTransform implements Transform {
 				MethodInsnNode methodInsnNode = (MethodInsnNode) abstractInsnNode;
 				if (methodInsnNode.name.equals(methodName) &&
 						methodInsnNode.desc.equals(desc)) {
-					method.instructions.insert(searcher.current(), createCallback(searcher.getPrevious().getPrevious()));
+					AbstractInsnNode second = searcher.getPrevious();
+					AbstractInsnNode first = searcher.getPrevious();
+					method.instructions.insert(searcher.current(), createCallback(first, second));
 				}
 			}
 		}
 	}
 
-	private InsnList createCallback(AbstractInsnNode byteLoad) {
+	private InsnList createCallback(AbstractInsnNode name, AbstractInsnNode value) {
 		InsnList insnList = new InsnList();
-		if (!(byteLoad instanceof VarInsnNode)) {
+		if (!(name instanceof VarInsnNode) ||
+				!(value instanceof VarInsnNode)) {
 			throw new RuntimeException();
 		}
-		int var = ((VarInsnNode) byteLoad).var;
+		int v = ((VarInsnNode) value).var;
+		insnList.add(new VarInsnNode(Opcodes.ALOAD, v));
+		insnList.add(new TypeInsnNode(Opcodes.INSTANCEOF, "[B"));
+		LabelNode label = new LabelNode();
+		insnList.add(new JumpInsnNode(Opcodes.IFEQ, label));
+		int n = ((VarInsnNode) name).var;
 		insnList.add(new FieldInsnNode(Opcodes.GETSTATIC, parent.getIdentified(), "accessor", "L" + Bridge.class.getName().replace('.', '/') + ";"));
-		insnList.add(new VarInsnNode(Opcodes.ALOAD, var));
+		insnList.add(new VarInsnNode(Opcodes.ALOAD, n));
 		insnList.add(new MethodInsnNode(Opcodes.INVOKEINTERFACE, Bridge.class.getName().replace('.', '/'), "entry", "(Ljava/lang/String;)V"));
+		insnList.add(label);
 		return insnList;
 	}
 }
