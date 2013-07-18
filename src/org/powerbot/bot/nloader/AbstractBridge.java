4f0b2db21c619a0e1c47d26e3dcfba2c00a045e9
Joe Haddad <timer@powerbot.org>
2013-07-18 00:26:15 -0400

    Update abstract bridge

--- src/org/powerbot/bot/nloader/AbstractBridge.java
+++ src/org/powerbot/bot/nloader/AbstractBridge.java
@@ -8,15 +8,18 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import org.objectweb.asm.ClassReader;
 import org.objectweb.asm.tree.ClassNode;
+import org.powerbot.bot.loader.transform.TransformSpec;
 
 public class AbstractBridge implements Bridge {
 	public final List<String> entries;
 	public final Map<String, byte[]> loaded;
+	private final TransformSpec transformSpec;
 	private ClassLoader loader;
 
-	public AbstractBridge() {
+	public AbstractBridge(TransformSpec transformSpec) {
 		this.entries = Collections.synchronizedList(new LinkedList<String>());
 		this.loaded = new ConcurrentHashMap<>();
+		this.transformSpec = transformSpec;
 	}
 
 	@Override
@@ -26,17 +29,22 @@ public class AbstractBridge implements Bridge {
 
 	@Override
 	public byte[] classDefined(byte[] bytes) {
+		if (transformSpec != null) {
+			return transformSpec.process(bytes);
+		}
+
 		ClassNode node = new ClassNode();
 		ClassReader reader = new ClassReader(bytes);
 		reader.accept(node, ClassReader.SKIP_DEBUG | ClassReader.SKIP_FRAMES);
-		if (loaded != null) {
-			loaded.put(node.name, bytes);
-		}
+		loaded.put(node.name, bytes);
 		return bytes;
 	}
 
 	@Override
 	public void entry(String name) {
+		if (transformSpec != null) {
+			return;
+		}
 		name = name.replace('/', '.');
 		if (!entries.contains(name)) {
 			entries.add(name);
@@ -45,10 +53,15 @@ public class AbstractBridge implements Bridge {
 
 	@Override
 	public void end() {
+		if (transformSpec != null) {
+			return;
+		}
+
 		//TODO: implement end
 	}
 
 	@Override
 	public void instance(Object object) {
+		//TODO: implement instance
 	}
 }
