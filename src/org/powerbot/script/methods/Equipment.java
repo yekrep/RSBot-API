f124b3fca4a53ac7f3ed4545b64d382ea6f2dc8d
Joe Haddad <timer@powerbot.org>
2013-07-23 04:05:48 -0400

    Update equipment api

--- src/org/powerbot/script/methods/Equipment.java
+++ src/org/powerbot/script/methods/Equipment.java
@@ -5,62 +5,44 @@ import org.powerbot.script.wrappers.Component;
 import org.powerbot.script.wrappers.Item;
 
 public class Equipment extends MethodProvider {
-	public static final int WIDGET = 387;
-	public static final int WIDGET_BANK = 667;
-	public static final int COMPONENT_BANK = 121;
+	public static final int WIDGET = 1464;
+	public static final int COMPONENT_CONTAINER = 28;
+	public static final int WIDGET_GEAR = 1462;
+	public static final int COMPONENT_GEAR_CONTAINER = 13;
 	public static final int NUM_SLOTS = 13;
-	public static final int NUM_APPEARANCE_SLOTS = 9;
 
 	public Equipment(MethodContext factory) {
 		super(factory);
 	}
 
 	public static enum Slot {
-		HEAD(0, 7, 0, 0, -1),
-		CAPE(1, 10, 1, 1, -1),
-		NECK(2, 13, 2, 2, -1),
-		MAIN_HAND(3, 16, 3, 3, 15),
-		TORSO(4, 19, 4, 4, -1),
-		OFF_HAND(5, 22, 5, 5, 16),
-		LEGS(7, 25, 7, 7, -1),
-		HANDS(9, 28, 9, 9, -1),
-		FEET(10, 31, 10, 10, -1),
-		RING(12, 34, 12, -1, -1),
-		QUIVER(13, 39, 13, -1, -1),
-		AURA(14, 48, 14, 14, -1),
-		POCKET(14, 70, 15, -1, -1);
-		private final int index;
+		HEAD(0, 0),
+		CAPE(1, 1),
+		NECK(2, 2),
+		MAIN_HAND(3, 3),
+		TORSO(4, 4),
+		OFF_HAND(5, 5),
+		LEGS(7, 7),
+		HANDS(9, 9),
+		FEET(10, 10),
+		RING(12, 12),
+		QUIVER(13, 13),
+		AURA(14, 14),
+		POCKET(14, 15);
+		private final int storageIndex;
 		private final int component;
-		private final int bank;
-		private final int appearance;
-		private final int sheathed;
 
-		Slot(int index, int component, int bank, int appearance, int sheathed) {
-			this.index = index;
+		Slot(int storageIndex, int component) {
+			this.storageIndex = storageIndex;
 			this.component = component;
-			this.bank = bank;
-			this.appearance = appearance;
-			this.sheathed = sheathed;
 		}
 
-		public int getIndex() {
-			return index;
+		public int getStorageIndex() {
+			return storageIndex;
 		}
 
 		public int getComponentIndex() {
 			return component;
-		}
-
-		public int getBankComponentIndex() {
-			return bank;
-		}
-
-		public int getAppearanceIndex() {
-			return appearance;
-		}
-
-		public int getSheathedIndex() {
-			return sheathed;
 		}
 	}
 
@@ -69,12 +51,12 @@ public class Equipment extends MethodProvider {
 		int[][] data = ctx.items.getItems(Items.INDEX_EQUIPMENT);
 		Item[] items = new Item[NUM_SLOTS];
 		for (Slot slot : Slot.values()) {
-			int index = slot.getIndex();
+			int index = slot.getStorageIndex();
 			Component c;
 			if (b) {
-				c = ctx.widgets.get(WIDGET_BANK, COMPONENT_BANK).getChild(slot.getBankComponentIndex());
+				c = ctx.widgets.get(WIDGET_GEAR, COMPONENT_GEAR_CONTAINER).getChild(slot.getComponentIndex());
 			} else {
-				c = ctx.widgets.get(WIDGET, slot.getComponentIndex());
+				c = ctx.widgets.get(WIDGET, COMPONENT_CONTAINER).getChild(slot.getComponentIndex());
 			}
 			if (index < 0 || index >= data.length || data[index][0] == -1) {
 				items[slot.ordinal()] = new Item(ctx, -1, -1, c);
@@ -85,17 +67,17 @@ public class Equipment extends MethodProvider {
 		return items;
 	}
 
-	public Item getItem(Slot slot) {
-		int index = slot.getIndex();
+	public Item getItemAt(Slot slot) {
+		int index = slot.getStorageIndex();
 		int[][] data = ctx.items.getItems(Items.INDEX_EQUIPMENT);
 		if (index < 0 || index >= data.length || data[index][0] == -1) {
 			return null;
 		}
 		Component c;
 		if (ctx.bank.isOpen()) {
-			c = ctx.widgets.get(WIDGET_BANK, COMPONENT_BANK).getChild(slot.getBankComponentIndex());
+			c = ctx.widgets.get(WIDGET_GEAR, COMPONENT_GEAR_CONTAINER).getChild(slot.getComponentIndex());
 		} else {
-			c = ctx.widgets.get(WIDGET, slot.getComponentIndex());
+			c = ctx.widgets.get(WIDGET, COMPONENT_CONTAINER).getChild(slot.getComponentIndex());
 		}
 		return new Item(ctx, data[index][0], data[index][1], c);
 	}
@@ -115,17 +97,5 @@ public class Equipment extends MethodProvider {
 			}
 		}
 		return true;
-	}
-
-	public boolean containsOneOf(int... ids) {
-		int[][] data = ctx.items.getItems(Items.INDEX_EQUIPMENT);
-		for (int id : ids) {
-			for (int i = 0; i < data.length; i++) {
-				if (data[i][0] == id) {
-					return true;
-				}
-			}
-		}
-		return false;
 	}
 }
