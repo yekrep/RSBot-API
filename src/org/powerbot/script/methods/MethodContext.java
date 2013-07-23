46a6823256d5443efc3619eeaa01ced07be8f1d8
Joe Haddad <timer@powerbot.org>
2013-07-23 04:25:43 -0400

    Remove summoning api

--- src/org/powerbot/script/methods/MethodContext.java
+++ src/org/powerbot/script/methods/MethodContext.java
@@ -36,7 +36,6 @@ public class MethodContext {
 	public Projectiles projectiles;
 	public Settings settings;
 	public Skills skills;
-	public Summoning summoning;
 	public Widgets widgets;
 
 	Items items;
@@ -71,7 +70,6 @@ public class MethodContext {
 		projectiles = new Projectiles(this);
 		settings = new Settings(this);
 		skills = new Skills(this);
-		summoning = new Summoning(this);
 		widgets = new Widgets(this);
 
 		items = new Items(this);
@@ -116,7 +114,6 @@ public class MethodContext {
 		projectiles = ctx.projectiles;
 		settings = ctx.settings;
 		skills = ctx.skills;
-		summoning = ctx.summoning;
 		widgets = ctx.widgets;
 
 		items = ctx.items;
