15437688701e0b6070313b7caf28067224e0e33a
Joe Haddad <timer@powerbot.org>
2013-07-24 04:29:27 -0400

    Prayer widgets for scripters

--- src/org/powerbot/script/methods/Powers.java
+++ src/org/powerbot/script/methods/Powers.java
@@ -1,6 +1,9 @@
 package org.powerbot.script.methods;
 
 public class Powers extends MethodProvider {
+	public static final int WIDGET_PRAYER = 1458;
+	public static final int COMPONENT_PRAYER_CONTAINER = 24;
+
 	public Powers(MethodContext factory) {
 		super(factory);
 	}
