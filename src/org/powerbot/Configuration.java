0592bc3249abca06179ddc5dd1e7e2356b418cb7
A. Ahmed <paris@powerbot.org>
2013-07-31 16:06:05 +0100

    Version 5005

--- src/org/powerbot/Configuration.java
+++ src/org/powerbot/Configuration.java
@@ -15,7 +15,7 @@ import org.powerbot.util.io.Resources;
 public class Configuration {
 	public static final String NAME = "RSBot";
 	public static final boolean FROMJAR, SERVEROS;
-	public static final int VERSION = 5004;
+	public static final int VERSION = 5005;
 	public static final boolean BETA = false;
 	public static final OperatingSystem OS;
 	public static final File HOME, TEMP;
