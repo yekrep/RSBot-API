9ed984c7aa03e31960717d48bdd89d3761d2bd1b
A. Ahmed <paris@powerbot.org>
2013-08-15 00:20:18 +0100

    Basis of antipatterns

---
+++ src/org/powerbot/script/golem/BankPattern.java
@@ -0,0 +1,11 @@
+package org.powerbot.script.golem;
+
+/**
+ * An {@link Antipattern} which makes random withdrawals, deposits and searches at a bank screen.
+ */
+public class BankPattern extends Antipattern {
+
+	@Override
+	public void run(final Preference preference) {
+	}
+}
---
+++ src/org/powerbot/script/golem/CameraPattern.java
@@ -0,0 +1,11 @@
+package org.powerbot.script.golem;
+
+/**
+ * An {@link Antipattern} for the camera which makes randomised screen rotations.
+ */
+public class CameraPattern extends Antipattern {
+
+	@Override
+	public void run(final Preference preference) {
+	}
+}
