d732e87103de74b28838619805792240869ce1c3
A. Ahmed <paris@powerbot.org>
2013-08-15 12:12:20 +0100

    Remove warning message

--- src/org/powerbot/gui/BotChrome.java
+++ src/org/powerbot/gui/BotChrome.java
@@ -110,16 +110,6 @@ public class BotChrome extends JFrame implements Closeable {
 		}
 		this.bot = bot;
 
-		SwingUtilities.invokeLater(new Runnable() {
-			@Override
-			public void run() {
-				JOptionPane.showMessageDialog(BotChrome.this,
-						"Ban rates are currently at medium-high levels.\nYou are advised to switch your scripts and accounts every few hours." +
-						"\n\nWe are working to mitigate the risks of bans every update.",
-						"Warning", JOptionPane.WARNING_MESSAGE);
-			}
-		});
-
 		System.gc();
 	}
 
