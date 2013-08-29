a1097c09bdd92f6d665540c6fed20df7eb7f9d31
Joe Haddad <timer@powerbot.org>
2013-08-29 17:19:56 -0400

    Humanize bank pin random event

--- src/org/powerbot/script/internal/randoms/BankPin.java
+++ src/org/powerbot/script/internal/randoms/BankPin.java
@@ -2,35 +2,34 @@ package org.powerbot.script.internal.randoms;
 
 import org.powerbot.script.PollingScript;
 import org.powerbot.script.internal.InternalScript;
+import org.powerbot.script.util.Random;
 
-/**
- * @author Paris
- * @author Timer
- */
 public class BankPin extends PollingScript implements InternalScript {
-	private final int PIN_INDEX = 163, PIN_WIDGET = 13, PIN_WIDGET_BASE = 0, PIN_WIDGET_OFFSET = 6;
+	private static final int SETTING_PIN_STEP = 163;
+	private static final int WIDGET = 13;
+	private static final int COMPONENT = 0;
+	private static final int COMPONENT_PIN_OFFSET = 6;
 
 	@Override
 	public int poll() {
 		final String pin = getPin();
 
-		while (ctx.widgets.get(PIN_WIDGET, PIN_WIDGET_BASE).isVisible()) {
-			if (pin == null) {
-				getController().stop();
-				return -1;
-			}
-
-			int i, v = Integer.valueOf(String.valueOf(pin.charAt(i = ctx.settings.get(PIN_INDEX))));
-			if (ctx.widgets.get(PIN_WIDGET, v + PIN_WIDGET_OFFSET).interact("Select")) {
-				while (i == ctx.settings.get(PIN_INDEX)) {
-					sleep(2400);
-				}
-			}
-
-			sleep(600);
+		if (pin == null) {
+			getController().stop();
+			return -1;
 		}
 
-		return -1;
+		if (ctx.widgets.get(WIDGET, COMPONENT).isVisible()) {
+			return -1;
+		}
+
+		int i, v = Integer.valueOf(String.valueOf(pin.charAt(i = ctx.settings.get(SETTING_PIN_STEP))));
+		if (ctx.widgets.get(WIDGET, v + COMPONENT_PIN_OFFSET).interact("Select")) {
+			for (int d = 0; d < 24 && i == ctx.settings.get(SETTING_PIN_STEP); d++) {
+				sleep(Random.nextInt(80, 100));
+			}
+		}
+		return i != ctx.settings.get(SETTING_PIN_STEP) ? Random.nextInt(600, 1800) : 100;
 	}
 
 	private String getPin() {
