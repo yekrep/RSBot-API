97ae03bae320f40287eba21a435a4873ec552ed8
Joe Haddad <timer@powerbot.org>
2013-09-07 20:39:01 -0400

    Do not return a poll value as means of delay

--- src/org/powerbot/script/internal/scripts/WidgetCloser.java
+++ src/org/powerbot/script/internal/scripts/WidgetCloser.java
@@ -13,20 +13,22 @@ import org.powerbot.script.wrappers.Component;
  */
 public class WidgetCloser extends PollingScript implements InternalScript {
 	private static final int[] COMPONENTS = {
-			1252 << 16 | 8, // Squeal of Fortune notification
 			1253 << 16 | 36, // Squeal of Fortune window
 			906 << 16 | 302, // validate email
 			1139 << 16 | 12, // Extras window
 			438 << 16 | 24,//recruit a friend
 			622 << 16 | 21,//member loyalty
 			204 << 16 | 3,//membership offer
+			1252 << 16 | 8, // Squeal of Fortune notification
 	};
 
 	private Component component;
+	private long time;
 	private int tries;
 
 	public WidgetCloser() {
 		priority.set(5);
+		time = 0;
 	}
 
 	@Override
@@ -38,17 +40,18 @@ public class WidgetCloser extends PollingScript implements InternalScript {
 		threshold.offer(priority.get());
 
 		final Component component = this.component;
-		if (component == null) {
+		if (component == null || System.currentTimeMillis() < time) {
 			return -1;
 		}
 
 		if (++tries > 3) {
+			time = System.currentTimeMillis() + Random.nextInt(30, 61) * 1000;
 			tries = 0;
-			return Random.nextInt(30, 61) * 1000;
+			return 0;
 		}
 
 		if (component.click(true)) {
-			if (!Condition.wait(new Callable<Boolean>() {
+			if (Condition.wait(new Callable<Boolean>() {
 				@Override
 				public Boolean call() {
 					return !component.isVisible();
