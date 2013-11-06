0a800eb37df373cad02890393137e23db29e1be0
A. Ahmed <paris@powerbot.org>
2013-11-06 22:59:52 +0000

    Don't use https since Apple Java 1.6 doesn't support EV SSL

--- src/org/powerbot/Configuration.java
+++ src/org/powerbot/Configuration.java
@@ -32,14 +32,13 @@ public class Configuration {
 		static final String DOMAIN_SITE_CDN = "cdn." + DOMAIN;
 		static final String DOMAIN_SITE_TESTING = DOMAIN + ".localdomain";
 		static final String DOMAIN_SITE = TESTING ? DOMAIN_SITE_TESTING : DOMAIN_SITE_LIVE;
-		static final String PROTOCOL = TESTING ? "http://" : "https://";
 
 		public static final String VERSION = "http://" + DOMAIN_SITE_CDN + "/rsbot/version.txt";
 
-		public static final String CLIENTPATCH = "https://" + DOMAIN_SITE_CDN + "/rsbot/ts/%s.ts";
+		public static final String CLIENTPATCH = "http://" + DOMAIN_SITE_CDN + "/rsbot/ts/%s.ts";
 		public static final String CLIENTBUCKET = "http://buckets." + DOMAIN + "/process/?hash=%s";
-		public static final String SCRIPTSCOLLECTION = PROTOCOL + DOMAIN_SITE + "/scripts/api/collection/?{POST}a=%s";
-		public static final String SIGNIN = PROTOCOL + DOMAIN_SITE + "/rsbot/login/?{POST}u=%s&p=%s&a=%s";
+		public static final String SCRIPTSCOLLECTION = "http://" + DOMAIN_SITE + "/scripts/api/collection/?{POST}a=%s";
+		public static final String SIGNIN = "http://" + DOMAIN_SITE + "/rsbot/login/?{POST}u=%s&p=%s&a=%s";
 
 		public static final String SITE = "http://" + DOMAIN_SITE + "/";
 		public static final String REGISTER = "http://" + DOMAIN_SITE_LIVE + "/go/register";
