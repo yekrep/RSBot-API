24b607d6598366369ca13749d5745de19d3043c9
Joe Haddad <timer@powerbot.org>
2013-11-10 01:58:08 -0500

    Update chat options.

--- src/org/powerbot/script/methods/Chat.java
+++ src/org/powerbot/script/methods/Chat.java
@@ -10,7 +10,7 @@ import org.powerbot.script.wrappers.Component;
 public class Chat extends TextQuery<ChatOption> {
 	public static final int WIDGET = 1188;
 	private static final int[] COMPONENT_CHAT_OPTIONS = {
-			11, 19, 24, 29, 34
+			12, 18, 23, 28, 33
 	};
 	private static final int[][] WIDGET_CONTINUE = {{1189, 11}, {1184, 11}, {1186, 6}, {1191, 11}};
 
