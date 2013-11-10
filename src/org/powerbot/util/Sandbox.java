3db66237414209cba0d2b4eab956132e25dec55b
A. Ahmed <paris@powerbot.org>
2013-11-10 07:53:01 +0000

    Revert "Change order to prevent stack overflow"
    This reverts commit a3aab00906ebebf26066b0fdb8fc48b0ceebc68d.

--- src/org/powerbot/util/Sandbox.java
+++ src/org/powerbot/util/Sandbox.java
@@ -61,8 +61,8 @@ public class Sandbox extends SecurityManager {
 		if (perm instanceof RuntimePermission) {
 			if (name.equals("setSecurityManager")) {
 				throw new SecurityException(name);
-			} else if ((name.equals("modifyThreadGroup") || name.equals("createClassLoader") || name.equals("setFactory") ||
-					name.startsWith("exitVM")) && isScriptThread()) {
+			} else if (isScriptThread() && (name.equals("modifyThreadGroup") || name.equals("createClassLoader") || name.equals("setFactory") ||
+					name.startsWith("exitVM"))) {
 				throw new SecurityException(name);
 			}
 		} else if (perm instanceof FilePermission) {
