4e052c7d7ab6a42b40f332a02b70da0b51d276de
A. Ahmed <paris@powerbot.org>
2013-11-10 07:53:27 +0000

    Revert "Simplify with permission checks"
    This reverts commit 136920f6c374e6c652f65bd4eca04512c15b37db.

--- src/org/powerbot/util/Sandbox.java
+++ src/org/powerbot/util/Sandbox.java
@@ -44,6 +44,22 @@ public class Sandbox extends SecurityManager {
 	}
 
 	@Override
+	public void checkExec(final String cmd) {
+		if (isScriptThread()) {
+			throw new SecurityException();
+		}
+		super.checkExec(cmd);
+	}
+
+	@Override
+	public void checkExit(final int status) {
+		if (isScriptThread()) {
+			throw new SecurityException();
+		}
+		super.checkExit(status);
+	}
+
+	@Override
 	public void checkMulticast(final InetAddress maddr) {
 		throw new SecurityException();
 	}
@@ -61,16 +77,14 @@ public class Sandbox extends SecurityManager {
 		if (perm instanceof RuntimePermission) {
 			if (name.equals("setSecurityManager")) {
 				throw new SecurityException(name);
-			} else if (isScriptThread() && (name.equals("modifyThreadGroup") || name.equals("createClassLoader") || name.equals("setFactory") ||
-					name.startsWith("exitVM"))) {
+			} else if (name.equals("modifyThreadGroup") && isScriptThread()) {
+				throw new SecurityException(name);
+			} else if (name.equals("createClassLoader") && isScriptThread()) {
 				throw new SecurityException(name);
 			}
 		} else if (perm instanceof FilePermission) {
 			final FilePermission fp = (FilePermission) perm;
 			final String a = fp.getActions();
-			if (a.equals("execute") && isScriptThread()) {
-				throw new SecurityException(a + ": " + fp.getName());
-			}
 			if (isCallingClass(Desktop.class)) {
 				return;
 			}
@@ -86,6 +100,14 @@ public class Sandbox extends SecurityManager {
 	@Override
 	public void checkPrintJobAccess() {
 		throw new SecurityException();
+	}
+
+	@Override
+	public void checkSetFactory() {
+		if (isScriptThread()) {
+			throw new SecurityException();
+		}
+		super.checkSetFactory();
 	}
 
 	@Override
