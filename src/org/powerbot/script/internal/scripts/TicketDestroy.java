e5db9572bf0b3f50e3d1486cffe145d3e82711a9
Joe Haddad <timer@powerbot.org>
2013-11-17 20:58:04 -0500

    Rewrite ticket destroy.

--- src/org/powerbot/script/internal/scripts/TicketDestroy.java
+++ src/org/powerbot/script/internal/scripts/TicketDestroy.java
@@ -9,7 +9,6 @@ import org.powerbot.script.util.Condition;
 import org.powerbot.script.util.Random;
 import org.powerbot.script.wrappers.Component;
 import org.powerbot.script.wrappers.Item;
-import org.powerbot.script.wrappers.Player;
 import org.powerbot.script.wrappers.Widget;
 
 /**
@@ -17,51 +16,27 @@ import org.powerbot.script.wrappers.Widget;
  */
 public class TicketDestroy extends PollingScript implements InternalScript {
 	private static final int[] ITEM_IDS = {24154, 24155};
-	private Component component;
 
 	public TicketDestroy() {
 		priority.set(3);
 	}
 
-	private boolean isValid() {
-		if (!ctx.game.isLoggedIn() || !ctx.hud.isVisible(Hud.Window.BACKPACK)) {
-			return false;
-		}
-
-		final Player player;
-		if ((player = ctx.players.local()) == null ||
-				player.isInCombat() || player.getAnimation() != -1 || player.getInteracting() != null) {
-			return false;
-		}
-
-		this.component = null;
-		final Item item = ctx.backpack.select().id(ITEM_IDS).poll();
-		if (item.isValid()) {
-			this.component = item.getComponent();
-			return this.component.isValid();
-		}
-		return false;
-	}
-
 	@Override
 	public int poll() {
-		if (!isValid()) {
+		final Item item = ctx.backpack.select().id(ITEM_IDS).poll();
+		if (!item.isValid() || !ctx.hud.isVisible(Hud.Window.BACKPACK) || !ctx.players.local().isIdle()) {
 			threshold.poll();
 			return 0;
 		}
 		threshold.offer(priority.get());
-
-		final Component item = this.component;
-		if (item == null || !ctx.backpack.scroll(item)) {
+		if (!ctx.backpack.scroll(item)) {
 			return -1;
 		}
 
-		if (((ctx.settings.get(1448) & 0xFF00) >>> 8) < (item.getItemId() == ITEM_IDS[0] ? 10 : 9)) {
+		if (((ctx.settings.get(1448) & 0xFF00) >>> 8) < (item.getId() == ITEM_IDS[0] ? 10 : 9)) {
 			item.interact("Claim spin");
-			sleep(1500);
-			return -1;
+			return 1500;
 		}
-
 		if (!item.interact("Destroy")) {
 			return Random.nextInt(1000, 2000);
 		}
@@ -87,7 +62,7 @@ public class TicketDestroy extends PollingScript implements InternalScript {
 			Condition.wait(new Callable<Boolean>() {
 				@Override
 				public Boolean call() throws Exception {
-					return item.getItemId() == -1;
+					return item.getComponent().getItemId() == -1;
 				}
 			}, Random.nextInt(150, 200), 10);
 		}
