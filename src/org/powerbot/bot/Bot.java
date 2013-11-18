3709ec1f4ae0c254aecc22be6c38c7738de33599
A. Ahmed <paris@powerbot.org>
2013-11-18 04:00:04 +0000

    No safe mode for OS X users running Java 1.6

--- src/org/powerbot/bot/Bot.java
+++ src/org/powerbot/bot/Bot.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.logging.Logger;
 
+import org.powerbot.Configuration;
 import org.powerbot.bot.loader.Crawler;
 import org.powerbot.bot.loader.GameLoader;
 import org.powerbot.bot.loader.GameStub;
@@ -249,7 +250,13 @@ public final class Bot implements Runnable, Stoppable {
 		this.ctx.setClient(client);
 		client.setCallback(new AbstractCallback(this));
 		constants = new Constants(spec.constants);
-		new Thread(threadGroup, new SafeMode(this)).start();
+
+		final String jvm = System.getProperty("java.version");
+		if (jvm != null && jvm.startsWith("1.6.") && Configuration.OS == Configuration.OperatingSystem.MAC) {
+		} else {
+			new Thread(threadGroup, new SafeMode(this)).start();
+		}
+
 		inputHandler = new InputHandler(applet, client);
 		ctx.menu.register();
 	}
