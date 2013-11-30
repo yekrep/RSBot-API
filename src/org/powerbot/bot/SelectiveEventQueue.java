90839ac55a36e40777200a6d6eb3d5b28c3a65b9
Joe Haddad <timer@powerbot.org>
2013-11-30 18:35:21 -0500

    Defocus helper method.

--- src/org/powerbot/bot/SelectiveEventQueue.java
+++ src/org/powerbot/bot/SelectiveEventQueue.java
@@ -63,6 +63,19 @@ public class SelectiveEventQueue extends EventQueue {
 		}
 	}
 
+	public void defocus() {
+		if (!isBlocking()) {
+			return;
+		}
+
+		final Component component = this.component.get();
+		if (component != null && component.isFocusOwner()) {
+			System.out.println("losing focus, purposely.");
+			postEvent(new RawAWTEvent(new FocusEvent(component, FocusEvent.FOCUS_LOST, false, null)));
+			postEvent(new RawAWTEvent(new FocusEvent(component, FocusEvent.FOCUS_LOST, false, null)));
+		}
+	}
+
 	@Override
 	protected final void dispatchEvent(AWTEvent event) {
 		if (event instanceof RawAWTEvent) {
