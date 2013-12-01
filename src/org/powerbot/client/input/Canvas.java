f88ef8c57be2ac38a622edda42c6cfda851f34be
Joe Haddad <timer@powerbot.org>
2013-12-01 17:11:34 -0500

    Clean up canvas.

--- src/org/powerbot/client/input/Canvas.java
+++ src/org/powerbot/client/input/Canvas.java
@@ -1,36 +1,28 @@
 package org.powerbot.client.input;
 
 import java.awt.AWTEvent;
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
 
-import org.powerbot.bot.Bot;
 import org.powerbot.bot.EventCallback;
 import org.powerbot.bot.SelectiveEventQueue;
 import org.powerbot.gui.BotChrome;
 
 public class Canvas extends java.awt.Canvas {
 	private static final long serialVersionUID = -8630774417757577975L;
-	private final Bot bot;
 
 	public Canvas() {
 		super();
-		final BotChrome chrome = BotChrome.getInstance();
 		final SelectiveEventQueue queue = SelectiveEventQueue.getInstance();
-		this.bot = chrome.getBot();
-
 		SelectiveEventQueue.pushSelectiveQueue();
+
 		queue.block(this, new EventCallback() {
 			@Override
 			public void execute(final AWTEvent event) {
 			}
 		});
-
-		if (SelectiveEventQueue.getInstance().isBlocking()) {
+		if (queue.isBlocking()) {
 			queue.focus();
 		}
 
-		bot.initiate();
-	}
+		BotChrome.getInstance().getBot().initiate();
 	}
 }
