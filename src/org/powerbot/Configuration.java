103511f797386936d6f9b0d327d5d64d7f754d3c
A. Ahmed <paris@powerbot.org>
2013-12-04 23:25:47 +0000

    Bring back bot ads

--- src/org/powerbot/Configuration.java
+++ src/org/powerbot/Configuration.java
@@ -48,6 +48,7 @@ public class Configuration {
 		public static final String SCRIPTSLIST = "http://" + DOMAIN_SITE_LIVE + "/go/scripts";
 
 		public static final String SCRIPTSICONS = "http://" + DOMAIN_SITE_CDN + "/assets/img/sprites/scripts.png";
+		public static final String ADS = "http://cdn.powerbot.org/rsbot/ads.txt";
 
 		public static final String GAME = "runescape.com";
 		public static final String GAME_SERVICES_DOMAIN = "services." + GAME;
--- src/org/powerbot/gui/component/BotPanel.java
+++ src/org/powerbot/gui/component/BotPanel.java
@@ -1,25 +1,44 @@
 package org.powerbot.gui.component;
 
 import java.awt.Color;
-import java.awt.Component;
+import java.awt.Cursor;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.net.URL;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.TimeZone;
 import java.util.logging.Logger;
 
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
+import org.powerbot.Configuration;
 import org.powerbot.gui.BotChrome;
+import org.powerbot.service.NetworkAccount;
+import org.powerbot.util.Ini;
+import org.powerbot.util.io.CryptFile;
+import org.powerbot.util.io.HttpClient;
 import org.powerbot.util.io.Resources;
 
 public class BotPanel extends JPanel {
 	private static final long serialVersionUID = -8983015619045562434L;
 
-	public BotPanel(final Component parent) {
+	public BotPanel(final BotChrome parent) {
 		final Dimension d = new Dimension(BotChrome.PANEL_MIN_WIDTH, BotChrome.PANEL_MIN_HEIGHT);
 		setSize(d);
 		setPreferredSize(d);
@@ -32,7 +51,8 @@ public class BotPanel extends JPanel {
 		final JPanel panel = new JPanel();
 		panel.setLayout(getLayout());
 		panel.setBackground(getBackground());
-		panel.add(new JLabel(new ImageIcon(Resources.getImage(Resources.Paths.ARROWS))), new GridBagConstraints());
+		final JLabel logo = new JLabel(new ImageIcon(Resources.getImage(Resources.Paths.ARROWS)));
+		panel.add(logo, new GridBagConstraints());
 		final GridBagConstraints c = new GridBagConstraints();
 		c.gridy = 1;
 		final JLabel status;
@@ -42,5 +62,95 @@ public class BotPanel extends JPanel {
 		status.setFont(new Font(f.getFamily(), f.getStyle(), f.getSize() + 1));
 		add(panel);
 		Logger.getLogger("").addHandler(new BotPanelLogHandler(status));
+
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				final String link;
+				final Image img;
+				try {
+					final CryptFile ads = new CryptFile("ads.1.txt", getClass());
+					final Ini.Member ini = new Ini().read(ads.download(new URL(Configuration.URLs.ADS))).get();
+
+					if (!ini.getBool("enabled", false) || !ini.get("image", "").startsWith("http") || !ini.get("link", "").startsWith("http") || !ini.has("expires")) {
+						return;
+					}
+
+					final long exp;
+					try {
+						final DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+						df.setCalendar(Calendar.getInstance(TimeZone.getTimeZone("UTC")));
+						exp = df.parse(ini.get("expires")).getTime();
+					} catch (final ParseException ignored) {
+						return;
+					}
+
+					if (System.currentTimeMillis() > exp) {
+						return;
+					}
+
+					final NetworkAccount n = NetworkAccount.getInstance();
+					if (!ini.getBool("vips", false) && n.isLoggedIn() && n.hasPermission(NetworkAccount.VIP)) {
+						return;
+					}
+
+					link = ini.get("link");
+					if (ini.getBool("popup", false)) {
+						BotChrome.openURL(link);
+					}
+
+					final File f = new File(Configuration.TEMP, CryptFile.getHashedName("advert.1.png"));
+					HttpClient.download(new URL(ini.get("image")), f);
+					if (!(f.isFile() && f.canRead())) {
+						return;
+					}
+
+					final BufferedImage bi = ImageIO.read(f);
+					final double w = bi.getWidth() / 768d, h = bi.getHeight() / 150d;
+					if (w > 1d || h > 1d) {
+						final double z = Math.max(w, h);
+						img = bi.getScaledInstance((int) (bi.getWidth() / z), (int) (bi.getHeight() / z), Image.SCALE_SMOOTH);
+					} else {
+						img = bi;
+					}
+				} catch (final IOException ignored) {
+					return;
+				}
+
+				if (Thread.interrupted()) {
+					return;
+				}
+
+				SwingUtilities.invokeLater(new Runnable() {
+					@Override
+					public void run() {
+						final JLabel label = new JLabel(new ImageIcon(img));
+						label.setBorder(BorderFactory.createEmptyBorder(175, 0, 0, 0));
+						label.setCursor(new Cursor(Cursor.HAND_CURSOR));
+						label.addMouseListener(new MouseAdapter() {
+							@Override
+							public void mouseClicked(final MouseEvent e) {
+								BotChrome.openURL(link);
+							}
+
+							@Override
+							public void mouseEntered(final MouseEvent e) {
+								parent.overlay.setCursor(label.getCursor());
+							}
+
+							@Override
+							public void mouseExited(final MouseEvent e) {
+								parent.overlay.setCursor(Cursor.getDefaultCursor());
+							}
+						});
+						logo.setBorder(label.getBorder());
+						final GridBagConstraints c = new GridBagConstraints();
+						c.gridy = 2;
+						panel.add(label, c);
+						panel.revalidate();
+					}
+				});
+			}
+		}).start();
 	}
 }
