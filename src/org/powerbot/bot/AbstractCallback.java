32b7875b1048de35af23181056588c38ee263bb3
Joe Haddad <timer@powerbot.org>
2013-12-06 02:13:40 -0500

    Speed up execution.

--- src/org/powerbot/bot/AbstractCallback.java
+++ src/org/powerbot/bot/AbstractCallback.java
@@ -4,35 +4,39 @@ import org.powerbot.client.Callback;
 import org.powerbot.client.RSInteractableLocation;
 import org.powerbot.client.RSObjectDef;
 import org.powerbot.client.Render;
+import org.powerbot.event.EventDispatcher;
 import org.powerbot.event.MessageEvent;
 import org.powerbot.script.methods.Camera;
+import org.powerbot.script.methods.MethodContext;
 import org.powerbot.util.math.Vector3f;
 
 public class AbstractCallback implements Callback {
-	private final Bot bot;
+	private final EventDispatcher eventDispatcher;
+	private final MethodContext ctx;
 
 	public AbstractCallback(final Bot bot) {
-		this.bot = bot;
+		this.eventDispatcher = bot.getEventDispatcher();
+		this.ctx = bot.getMethodContext();
 	}
 
 	@Override
 	public void updateRenderInfo(final Render render) {
-		bot.getMethodContext().game.updateToolkit(render);
+		ctx.game.updateToolkit(render);
 	}
 
 	@Override
 	public void notifyMessage(final int id, final String sender, final String message) {
-		bot.getEventDispatcher().dispatch(new MessageEvent(id, sender, message));
+		eventDispatcher.dispatch(new MessageEvent(id, sender, message));
 	}
 
 	@Override
 	public void notifyObjectDefinitionLoad(final RSObjectDef def) {
-		bot.getMethodContext().objects.setType(def.getID(), def.getClippingType());
+		ctx.objects.setType(def.getID(), def.getClippingType());
 	}
 
 	@Override
 	public void updateCamera(final RSInteractableLocation offset, final RSInteractableLocation center) {
-		final Camera camera = bot.getMethodContext().camera;
+		final Camera camera = ctx.camera;
 		camera.offset = new Vector3f(offset.getX(), offset.getY(), offset.getZ());
 		camera.center = new Vector3f(center.getX(), center.getY(), center.getZ());
 	}
