0725254bd7dea3ba4ea146421172e77b4d127350
A. Ahmed <paris@powerbot.org>
2013-12-06 18:27:32 +0000

    Suppress unusued warnings

--- src/org/powerbot/client/input/Canvas.java
+++ src/org/powerbot/client/input/Canvas.java
@@ -6,6 +6,7 @@ import org.powerbot.bot.EventCallback;
 import org.powerbot.bot.SelectiveEventQueue;
 import org.powerbot.gui.BotChrome;
 
+@SuppressWarnings("unused")
 public class Canvas extends java.awt.Canvas {
 	private static final long serialVersionUID = -2284879212465893870L;
 
