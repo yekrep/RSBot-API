0a4bbb8dd6a68e8ecdf58fe4dd0d5e1a93c98138
A. Ahmed <paris@powerbot.org>
2014-01-09 23:18:46 +0000

    Remove old warning

--- src/org/powerbot/script/lang/AbstractQuery.java
+++ src/org/powerbot/script/lang/AbstractQuery.java
@@ -216,8 +216,6 @@ public abstract class AbstractQuery<T extends AbstractQuery<T, K>, K> extends Me
 	/**
 	 * Retrieves and removes the first item in the query cache, or returns the value of {@link #getNil()} if it is empty.
 	 *
-	 * <b>Note: the use of this method is strongly discouraged.</b>
-	 *
 	 * @return the first item in the query cache, or the value of {@link #getNil()} if it is empty
 	 */
 	public K poll() {
