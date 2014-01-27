a8eb592d801fe79983bb0426088ec5ecd51e2907
A. Ahmed <paris@powerbot.org>
2014-01-27 07:55:21 +0000

    Scripts window stub

---
+++ src/org/powerbot/os/gui/BotScripts.java
@@ -0,0 +1,17 @@
+package org.powerbot.os.gui;
+
+import javax.swing.JDialog;
+
+/**
+ * @author Paris
+ */
+public class BotScripts extends JDialog {
+
+	public BotScripts(final BotChrome parent) {
+		super(parent, "Scripts");
+		setLayout(null);
+
+
+
+	}
+}
