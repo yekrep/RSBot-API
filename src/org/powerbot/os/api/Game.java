4f5c6fbbf8bb8bedeb6ed6ccbb3f85b90e166fae
Joe Haddad <timer@powerbot.org>
2014-01-31 15:43:31 -0500

    Fix game calculations.

--- src/org/powerbot/os/api/Game.java
+++ src/org/powerbot/os/api/Game.java
@@ -1,11 +1,11 @@
 package org.powerbot.os.api;
 
+import java.awt.Point;
+
 import org.powerbot.os.api.wrappers.HintArrow;
 import org.powerbot.os.api.wrappers.RelativePosition;
 import org.powerbot.os.api.wrappers.Tile;
 import org.powerbot.os.client.Client;
-
-import java.awt.*;
 
 public class Game extends MethodProvider {
 	private static final int[] ARRAY_SIN = new int[2048];
@@ -25,28 +25,31 @@ public class Game extends MethodProvider {
 	public HintArrow getHintArrow() {
 		final HintArrow r = new HintArrow();
 		final Client client = ctx.getClient();
-		if (client == null) return r;
+		if (client == null) {
+			return r;
+		}
 		return r;
 	}
 
-	private Point tileToMap(final Tile tile) {
-		final Point r = new Point(-1, -1);
+	public Point tileToMap(final Tile tile) {
 		final Client client = ctx.getClient();
-		if (client == null) return r;
+		if (client == null) {
+			return new Point(-1, -1);
+		}
 		final RelativePosition rel = ctx.players.getLocal().getRelativePosition();
 		final int angle = client.getMinimapScale() + client.getMinimapAngle() & 0x7ff;
-		final int[] d = {tile.x, tile.y, ARRAY_SIN[angle], ARRAY_COS[angle]};
+		final int[] d = {tile.x, tile.y, ARRAY_SIN[angle], ARRAY_COS[angle], -1, -1};
 		d[0] = (d[0] - client.getOffsetX()) * 4 + 2 - rel.x / 32;
 		d[1] = (d[1] - client.getOffsetY()) * 4 + 2 - rel.z / 32;
 		final int offset = client.getMinimapOffset();
-		d[2] = d[2] * 256 / (offset + 256);
-		d[3] = d[3] * 256 / (offset + 256);
-		d[0] = d[0] * d[3] + d[2] * d[1] >> 16;
-		d[1] = d[1] * d[3] - d[2] * d[0] >> 16;
-		return new Point(643 + d[0], 83 + d[1]);
+		d[2] = d[2] << 8 / (offset + 256);
+		d[3] = d[3] << 8 / (offset + 256);
+		d[4] = d[1] * d[2] + d[3] * d[0] >> 16;
+		d[5] = d[2] * d[0] - d[1] * d[3] >> 16;
+		return new Point(643 + d[4], 83 + d[5]);
 	}
 
-	private int getHeight(final int relativeX, final int relativeY, int floor) {
+	public int getHeight(final int relativeX, final int relativeY, int floor) {
 		final Client client = ctx.getClient();
 		int x = relativeX >> 7;
 		int y = relativeY >> 7;
@@ -55,19 +58,22 @@ public class Game extends MethodProvider {
 			return 0;
 		}
 		final byte[][][] meta = client.getLandscapeMeta();
-		if (meta == null) return 0;
+		final int[][][] heights = client.getTileHeights();
+		if (meta == null) {
+			return 0;
+		}
 		if (floor < 3 && (meta[1][x][y] & 0x2) == 2) {
 			floor++;
 		}
 
 		x &= 0x7f;
 		y &= 0x7f;
-		final int heightStart = x * meta[floor][(1 + x)][y] + meta[floor][x][y] * (128 - x) >> 7;
-		final int heightEnd = (128 - x) * meta[floor][x][(1 + y)] + x * meta[floor][(1 + x)][(y + 1)] >> 7;
+		final int heightStart = x * heights[floor][1 + x][y] + heights[floor][x][y] * (128 - x) >> 7;
+		final int heightEnd = (128 - x) * heights[floor][x][1 + y] + x * heights[floor][1 + x][y + 1] >> 7;
 		return y * heightEnd + heightStart * (128 - y) >> 7;
 	}
 
-	private Point worldToScreen(final int relativeX, final int relativeY, final int h) {
+	public Point worldToScreen(final int relativeX, final int relativeY, final int h) {
 		final Client client = ctx.getClient();
 		final Point r = new Point(-1, -1);
 		if (relativeX < 128 || relativeX > 13056 ||
@@ -75,7 +81,9 @@ public class Game extends MethodProvider {
 			return r;
 		}
 		final int floor = client.getFloor();
-		if (floor < 0) return r;
+		if (floor < 0) {
+			return r;
+		}
 		final int height = getHeight(relativeX, relativeY, floor) - h;
 		final int projectedX = relativeX - client.getCameraX(), projectedZ = relativeY - client.getCameraZ(),
 				projectedY = height - client.getCameraY();
