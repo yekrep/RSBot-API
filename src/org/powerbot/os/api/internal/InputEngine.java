43c3469d30d607a0931e284d2941515aa9560c56
Joe Haddad <timer@powerbot.org>
2014-02-12 16:06:28 -0500

    Generalize naming and update X, Y.

--- src/org/powerbot/os/api/internal/InputEngine.java
+++ src/org/powerbot/os/api/internal/InputEngine.java
@@ -6,42 +6,42 @@ import java.awt.event.MouseEvent;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 
-public class MouseEngine {
+public class InputEngine {//TODO: handle component focus!!!
 	private final Component component;
-	private final AtomicBoolean present;
-	private final AtomicBoolean[] pressed;
-	private final AtomicInteger x, y;
+	private final AtomicBoolean mousePresent;
+	private final AtomicBoolean[] mousePressed;
+	private final AtomicInteger mouseX, mouseY;
 
-	public MouseEngine(final Component component) {
+	public InputEngine(final Component component) {
 		this.component = component;
-		present = new AtomicBoolean(false);
-		pressed = new AtomicBoolean[]{null, new AtomicBoolean(false), new AtomicBoolean(false), new AtomicBoolean(false)};
-		x = new AtomicInteger(0);
-		y = new AtomicInteger(0);
+		mousePresent = new AtomicBoolean(false);
+		mousePressed = new AtomicBoolean[]{null, new AtomicBoolean(false), new AtomicBoolean(false), new AtomicBoolean(false)};
+		mouseX = new AtomicInteger(0);
+		mouseY = new AtomicInteger(0);
 	}
 
 	public void press(final int button) {
-		if (button < 1 || button >= pressed.length) {
+		if (button < 1 || button >= mousePressed.length) {
 			return;
 		}
-		if (!(present.get() || isDragging()) || pressed[button].get()) {
+		if (!(mousePresent.get() || isDragging()) || mousePressed[button].get()) {
 			return;
 		}
-		final MouseEvent e = new MouseEvent(component, MouseEvent.MOUSE_PRESSED, System.currentTimeMillis(), getMask(button), x.get(), y.get(), 1, false, button);
-		pressed[button].set(true);
+		final MouseEvent e = new MouseEvent(component, MouseEvent.MOUSE_PRESSED, System.currentTimeMillis(), getMask(button), mouseX.get(), mouseY.get(), 1, false, button);
+		mousePressed[button].set(true);
 		System.out.println(e.paramString());
 		//TODO: dispatch
 	}
 
 	public void release(final int button) {
-		if (button < 1 || button >= pressed.length) {
+		if (button < 1 || button >= mousePressed.length) {
 			return;
 		}
-		if (!pressed[button].get()) {
+		if (!mousePressed[button].get()) {
 			return;
 		}
-		final MouseEvent e = new MouseEvent(component, MouseEvent.MOUSE_RELEASED, System.currentTimeMillis(), getMask(button), x.get(), y.get(), 1, false, button);
-		pressed[button].set(false);
+		final MouseEvent e = new MouseEvent(component, MouseEvent.MOUSE_RELEASED, System.currentTimeMillis(), getMask(button), mouseX.get(), mouseY.get(), 1, false, button);
+		mousePressed[button].set(false);
 		System.out.println(e.paramString());
 		//TODO: dispatch
 	}
@@ -50,23 +50,29 @@ public class MouseEngine {
 		final boolean in = x >= 0 && y >= 0 && x < component.getWidth() && y < component.getHeight();
 		final int m = getMask();
 		if (in) {
-			if (present.get()) {
+			if (mousePresent.get()) {
 				if (!isDragging()) {
 					final MouseEvent e = new MouseEvent(component, MouseEvent.MOUSE_MOVED, System.currentTimeMillis(), m, x, y, 0, false);
+					mouseX.set(x);
+					mouseY.set(y);
 					System.out.println(e.paramString());
 					//TODO: dispatch
 				}
 				postDrag(x, y);
 			} else {
 				final MouseEvent e = new MouseEvent(component, MouseEvent.MOUSE_ENTERED, System.currentTimeMillis(), m, x, y, 0, false);
-				present.set(true);
+				mousePresent.set(true);
+				mouseX.set(x);
+				mouseY.set(y);
 				System.out.println(e.paramString());
 				//TODO: dispatch
 				postDrag(x, y);
 			}
-		} else if (present.get()) {
+		} else if (mousePresent.get()) {
 			final MouseEvent e = new MouseEvent(component, MouseEvent.MOUSE_EXITED, System.currentTimeMillis(), m, x, y, 0, false);
-			present.set(false);
+			mousePresent.set(false);
+			mouseX.set(x);
+			mouseY.set(y);
 			System.out.println(e.paramString());
 			//TODO: dispatch
 			postDrag(x, y);
@@ -81,17 +87,20 @@ public class MouseEngine {
 
 	private int getMask(final int button) {
 		int m = 0;
-		m |= pressed[MouseEvent.BUTTON1].get() || button == MouseEvent.BUTTON1 ? InputEvent.BUTTON1_DOWN_MASK : 0;
-		m |= pressed[MouseEvent.BUTTON2].get() || button == MouseEvent.BUTTON2 ? InputEvent.BUTTON2_DOWN_MASK | InputEvent.ALT_MASK : 0;
-		m |= pressed[MouseEvent.BUTTON3].get() || button == MouseEvent.BUTTON3 ? InputEvent.BUTTON3_DOWN_MASK | InputEvent.META_MASK : 0;
+		m |= mousePressed[MouseEvent.BUTTON1].get() || button == MouseEvent.BUTTON1 ? InputEvent.BUTTON1_DOWN_MASK : 0;
+		m |= mousePressed[MouseEvent.BUTTON2].get() || button == MouseEvent.BUTTON2 ? InputEvent.BUTTON2_DOWN_MASK | InputEvent.ALT_MASK : 0;
+		m |= mousePressed[MouseEvent.BUTTON3].get() || button == MouseEvent.BUTTON3 ? InputEvent.BUTTON3_DOWN_MASK | InputEvent.META_MASK : 0;
 		//TODO: account for keyboard alt, shift, ctrl
+		//TODO: InputEvent.ALT_DOWN_MASK;
+		//TODO: InputEvent.CTRL_DOWN_MASK;
+		//TODO: InputEvent.SHIFT_DOWN_MASK;
 		return m;
 	}
 
 	private boolean isDragging() {
 		final int[] arr = {MouseEvent.BUTTON1, MouseEvent.BUTTON2, MouseEvent.BUTTON3};
 		for (final int i : arr) {
-			if (pressed[i].get()) {
+			if (mousePressed[i].get()) {
 				return true;
 			}
 		}
@@ -103,6 +112,8 @@ public class MouseEngine {
 			return;
 		}
 		final MouseEvent e = new MouseEvent(component, MouseEvent.MOUSE_DRAGGED, System.currentTimeMillis(), getMask(), x, y, 0, false);
+		mouseX.set(x);
+		mouseY.set(y);
 		System.out.println(e.paramString());
 		//TODO: dispatch
 	}
