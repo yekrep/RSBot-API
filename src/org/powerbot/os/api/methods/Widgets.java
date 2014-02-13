5a981e623c10a8cc64c999d66b256be7ed7b0bff
Joe Haddad <timer@powerbot.org>
2014-02-13 13:28:29 -0500

    Implement widget sparse cache.

--- src/org/powerbot/os/api/methods/Widgets.java
+++ src/org/powerbot/os/api/methods/Widgets.java
@@ -1,13 +1,24 @@
 package org.powerbot.os.api.methods;
 
+import java.util.Arrays;
+
 import org.powerbot.os.api.wrappers.Widget;
 
 public class Widgets extends ClientAccessor {
+	private Widget[] sparseCache;
+
 	public Widgets(final ClientContext ctx) {
 		super(ctx);
 	}
 
-	public Widget get(final int index) {
-		return new Widget(ctx, index);
+	public synchronized Widget get(final int index) {
+		if (index < sparseCache.length && sparseCache[index] != null) {
+			return sparseCache[index];
+		}
+		final Widget c = new Widget(ctx, index);
+		if (index >= sparseCache.length) {
+			sparseCache = Arrays.copyOf(sparseCache, index + 1);
+		}
+		return sparseCache[index] = c;
 	}
 }
