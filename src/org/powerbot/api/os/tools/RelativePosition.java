6db3ff6e921e8dca23c76351a1c121c3c9a15901
Joe Haddad <timer@powerbot.org>
2014-02-13 22:32:47 -0500

    package relative position

--- src/org/powerbot/api/os/tools/RelativePosition.java
+++ src/org/powerbot/api/os/tools/RelativePosition.java
@@ -3,7 +3,7 @@ package org.powerbot.api.os.tools;
 public class RelativePosition {
 	public final int x, z;
 
-	public RelativePosition(final int x, final int z) {
+	RelativePosition(final int x, final int z) {
 		this.x = x;
 		this.z = z;
 	}
