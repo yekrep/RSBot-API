8bd347b2617b70e2037cd6c8663bcf944d711805
Joe Haddad <timer@powerbot.org>
2014-02-24 16:07:03 -0500

    fix

--- src/org/powerbot/script/methods/Lobby.java
+++ src/org/powerbot/script/methods/Lobby.java
@@ -25,7 +25,7 @@ public class Lobby extends MethodProvider {
 	public static final int LOGIN_DEFAULT_TIMEOUT = 30000;
 	public static final int WIDGET_MAIN_LOBBY = 906;
 	public static final int WIDGET_BUTTON_PLAY_GAME = 201;
-	public static final int WIDGET_BUTTON_LOGOUT = 225;
+	public static final int WIDGET_BUTTON_LOGOUT = 226;
 	public static final int WIDGET_LABEL_CURRENT_WORLD = 11;
 	public static final int WIDGET_WORLDS_TABLE = 64;
 	public static final int WIDGET_WORLDS_TABLE_SCROLLBAR = 86;
