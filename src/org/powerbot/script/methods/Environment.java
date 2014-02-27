3d84c5db601ff0c73c78c10e6741b4125d8a6e85
A. Ahmed <paris@powerbot.org>
2014-02-27 21:40:43 +0000

    Deprecation notes

--- src/org/powerbot/script/methods/Environment.java
+++ src/org/powerbot/script/methods/Environment.java
@@ -2,6 +2,11 @@ package org.powerbot.script.methods;
 
 import org.powerbot.misc.NetworkAccount;
 
+/**
+ * Provides basic details of the current user.
+ * This class is now deprecated in favour of the properties prefixed with <i>user</i>.
+ */
+@Deprecated
 public class Environment extends MethodProvider {
 	public Environment(final MethodContext factory) {
 		super(factory);
@@ -11,7 +16,10 @@ public class Environment extends MethodProvider {
 	 * Returns the logged in user's forum display name.
 	 *
 	 * @return the display name
+	 * @deprecated use the property <i>user.name</i>
+	 * @see org.powerbot.script.methods.MethodContext#properties
 	 */
+	@Deprecated
 	public static String getDisplayName() {
 		final NetworkAccount n = NetworkAccount.getInstance();
 		return n.isLoggedIn() ? n.getDisplayName() : null;
@@ -21,7 +29,10 @@ public class Environment extends MethodProvider {
 	 * Returns the logged in user's id.
 	 *
 	 * @return the id of the user
+	 * @deprecated use the property <i>user.id</i>
+	 * @see org.powerbot.script.methods.MethodContext#properties
 	 */
+	@Deprecated
 	public static int getUserId() {
 		return NetworkAccount.getInstance().getUID();
 	}
