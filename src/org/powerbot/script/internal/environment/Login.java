0097289798cb5177374e67e66b7d262300efa8f3
A. Ahmed <paris@powerbot.org>
2014-03-05 22:50:59 +0000

    Track banned messages

--- src/org/powerbot/script/internal/environment/Login.java
+++ src/org/powerbot/script/internal/environment/Login.java
@@ -3,6 +3,7 @@ package org.powerbot.script.internal.environment;
 import java.awt.Rectangle;
 
 import org.powerbot.misc.GameAccounts;
+import org.powerbot.misc.Tracker;
 import org.powerbot.script.PollingScript;
 import org.powerbot.script.internal.InternalScript;
 import org.powerbot.script.lang.Filter;
@@ -89,11 +90,24 @@ public class Login extends PollingScript implements InternalScript {
 		if (account != null && (state == Game.INDEX_LOGIN_SCREEN || state == Game.INDEX_LOGGING_IN)) {
 			final Component error = ctx.widgets.get(WIDGET, WIDGET_LOGIN_ERROR);
 			if (error.isVisible()) {
-				if (error.getText().toLowerCase().contains("password") ||
-						error.getText().toLowerCase().contains("ended")) {
+				final String pre = "scripts/0/login/", txt = error.getText().toLowerCase();
+				boolean stop = false;
+
+				if (txt.contains("your ban will be lifted in")) {
+					Tracker.getInstance().trackPage(pre + "ban", txt);
+					stop = true;
+				} else if (txt.contains("account has been disabled")) {
+					Tracker.getInstance().trackPage(pre + "disabled", txt);
+					stop = true;
+				} else if (txt.contains("password") || txt.contains("ended")) {
+					stop = true;
+				}
+
+				if (stop) {
 					getController().stop();
 					return -1;
 				}
+
 				ctx.widgets.get(WIDGET, WIDGET_LOGIN_TRY_AGAIN).click();
 				return -1;
 			}
