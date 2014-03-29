16e2e5294bbb6f67946d54436edca96d69ceafbb
Joe Haddad <timer@powerbot.org>
2014-03-28 22:23:45 -0400

    RT4 inventory query

---
+++ src/org/powerbot/script/rt4/Inventory.java
@@ -0,0 +1,29 @@
+package org.powerbot.script.rt4;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Inventory extends ItemQuery<Item> {
+	public Inventory(final ClientContext ctx) {
+		super(ctx);
+	}
+
+	@Override
+	protected List<Item> get() {
+		final List<Item> items = new ArrayList<Item>(28);
+		final Component c = ctx.widgets.get(149).component(0);
+		final int[] ids = c.itemIds(), stacks = c.itemStackSizes();
+		for (int i = 0; i < Math.min(ids != null ? ids.length : -1, stacks != null ? stacks.length : -1); i++) {
+			final int id = ids[i], stack = stacks[i];
+			if (id >= 1) {
+				items.add(new Item(ctx, c, i, id, stack));
+			}
+		}
+		return items;
+	}
+
+	@Override
+	public Item nil() {
+		return new Item(ctx, null, -1, -1, -1);
+	}
+}
