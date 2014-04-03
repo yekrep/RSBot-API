7717df7733ec03be3e9857da701f5b78b7f3691e
Joe Haddad <timer@powerbot.org>
2014-04-02 21:30:58 -0400

    Bank-inventory support

--- src/org/powerbot/script/rt4/Inventory.java
+++ src/org/powerbot/script/rt4/Inventory.java
@@ -8,9 +8,23 @@ public class Inventory extends ItemQuery<Item> {
 		super(ctx);
 	}
 
+	private static final int WIDGET_BANK = 15;
+	private static final int COMPONENT_BANK = 3;
+
 	@Override
 	protected List<Item> get() {
 		final List<Item> items = new ArrayList<Item>(28);
+		final Component sub = getComponent();
+		if (sub != null) {
+			for (final Component c : sub.components()) {
+				final int id = c.itemId();
+				if (id != -1) {
+					items.add(new Item(ctx, c, id, c.itemStackSize()));
+				}
+			}
+			return items;
+		}
+
 		final Component c = ctx.widgets.widget(149).component(0);
 		final int[] ids = c.itemIds(), stacks = c.itemStackSizes();
 		for (int i = 0; i < Math.min(ids != null ? ids.length : -1, stacks != null ? stacks.length : -1); i++) {
@@ -25,5 +39,13 @@ public class Inventory extends ItemQuery<Item> {
 	@Override
 	public Item nil() {
 		return new Item(ctx, null, -1, -1, -1);
+	}
+
+	private Component getComponent() {
+		Component component = ctx.widgets.widget(WIDGET_BANK).component(COMPONENT_BANK);
+		if (!component.visible()) {
+			component = null;
+		}
+		return component;
 	}
 }
