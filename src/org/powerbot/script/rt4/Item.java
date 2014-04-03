eec8fd63f70d80c507aa321c4d6e2614e42bb3b9
Joe Haddad <timer@powerbot.org>
2014-04-02 21:31:08 -0400

    Update item compatibility

--- src/org/powerbot/script/rt4/Item.java
+++ src/org/powerbot/script/rt4/Item.java
@@ -11,14 +11,18 @@ import org.powerbot.script.Stackable;
 public class Item extends Interactive implements Identifiable, Nameable, Stackable {
 	private static final int BASE_X = 560, BASE_Y = 210;
 	private static final int WIDTH = 42, HEIGHT = 36;
-	private final Component component;
-	private final int index, id;
-	private int stack;
+	final Component component;
+	private final int inventory_index, id;
+	private final int stack;
 
-	public Item(final ClientContext ctx, final Component component, final int index, final int id, final int stack) {
+	public Item(final ClientContext ctx, final Component component, final int id, final int stack) {
+		this(ctx, component, -1, id, stack);
+	}
+
+	public Item(final ClientContext ctx, final Component component, final int inventory_index, final int id, final int stack) {
 		super(ctx);
 		this.component = component;
-		this.index = index;
+		this.inventory_index = inventory_index;
 		this.id = id;
 		this.stack = stack;
 	}
@@ -30,8 +34,11 @@ public class Item extends Interactive implements Identifiable, Nameable, Stackab
 
 	@Override
 	public Point centerPoint() {
-		final int x = BASE_X + (index % 4) * WIDTH, y = BASE_Y + (index / 4) * HEIGHT;
-		return new Point(x + WIDTH / 2, y + HEIGHT / 2);
+		if (inventory_index != -1) {
+			final int x = BASE_X + (inventory_index % 4) * WIDTH, y = BASE_Y + (inventory_index / 4) * HEIGHT;
+			return new Point(x + WIDTH / 2, y + HEIGHT / 2);
+		}
+		return component.centerPoint();
 	}
 
 	@Override
@@ -46,13 +53,24 @@ public class Item extends Interactive implements Identifiable, Nameable, Stackab
 
 	@Override
 	public Point nextPoint() {
-		final Rectangle r = new Rectangle(BASE_X + (index % 4) * WIDTH, BASE_Y + (index / 4) * HEIGHT, WIDTH, HEIGHT);
-		return new Point(Random.nextInt(r.x, r.x + r.width), Random.nextInt(r.y, r.y + r.height));
+		if (inventory_index != -1) {
+			final Rectangle r = new Rectangle(BASE_X + (inventory_index % 4) * WIDTH, BASE_Y + (inventory_index / 4) * HEIGHT, WIDTH, HEIGHT);
+			return new Point(Random.nextInt(r.x, r.x + r.width), Random.nextInt(r.y, r.y + r.height));
+		}
+		return component.nextPoint();
 	}
 
 	@Override
 	public boolean contains(final Point point) {
-		final Rectangle r = new Rectangle(BASE_X + (index % 4) * WIDTH, BASE_Y + (index / 4) * HEIGHT, WIDTH, HEIGHT);
-		return r.contains(point);
+		if (inventory_index != -1) {
+			final Rectangle r = new Rectangle(BASE_X + (inventory_index % 4) * WIDTH, BASE_Y + (inventory_index / 4) * HEIGHT, WIDTH, HEIGHT);
+			return r.contains(point);
+		}
+		return component.contains(point);
+	}
+
+	@Override
+	public boolean valid() {
+		return component != null && component.visible() && id != -1;
 	}
 }
