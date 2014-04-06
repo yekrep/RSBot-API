79ae4caf7446a762ca6cd3bb0a5243a7d2583b5c
Joe Haddad <timer@powerbot.org>
2014-04-05 21:48:07 -0400

    Ensure full tile on screen rt4

--- src/org/powerbot/script/rt4/TileMatrix.java
+++ src/org/powerbot/script/rt4/TileMatrix.java
@@ -95,6 +95,9 @@ public final class TileMatrix extends Interactive implements Locatable {
 		if (model2 != null) {
 			return model2.nextPoint();
 		}
+		if (!inViewport()) {
+			return new Point(-1, -1);
+		}
 		final int x = Random.nextGaussian(0, 100, 5);
 		final int y = Random.nextGaussian(0, 100, 5);
 		return point(x / 100.0D, y / 100.0D, 0);
@@ -105,6 +108,9 @@ public final class TileMatrix extends Interactive implements Locatable {
 		final BoundingModel model2 = boundingModel.get();
 		if (model2 != null) {
 			return model2.centerPoint();
+		}
+		if (!inViewport()) {
+			return new Point(-1, -1);
 		}
 		return point(0);
 	}
