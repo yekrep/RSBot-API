1dd090966357c62b9dc7b87c8735f6c810032a52
A. Ahmed <paris@powerbot.org>
2014-04-07 19:00:50 +0100

    Version 6009

--- src/org/powerbot/Configuration.java
+++ src/org/powerbot/Configuration.java
@@ -11,7 +11,7 @@ import org.powerbot.util.StringUtils;
 
 public class Configuration {
 	public static final String NAME = "RSBot";
-	public static final int VERSION = 6008;
+	public static final int VERSION = 6009;
 
 	public static final boolean FROMJAR;
 	public static final OperatingSystem OS;
