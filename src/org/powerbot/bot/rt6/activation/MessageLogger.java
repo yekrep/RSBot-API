1eea23f70d254a650936a33552c079dc5ee35ca9
Joe Haddad <timer@powerbot.org>
2014-04-07 21:21:05 -0400

    Fix RT6 message debug

--- src/org/powerbot/bot/rt6/activation/MessageLogger.java
+++ src/org/powerbot/bot/rt6/activation/MessageLogger.java
@@ -4,9 +4,15 @@ import java.util.logging.Logger;
 
 import org.powerbot.script.MessageEvent;
 import org.powerbot.script.MessageListener;
+import org.powerbot.script.rt6.ClientContext;
 
 public class MessageLogger implements MessageListener {
 	private static final Logger log = Logger.getLogger("Messages");
+	private final ClientContext ctx;
+
+	public MessageLogger(final ClientContext ctx) {
+		this.ctx = ctx;
+	}
 
 	public void messaged(final MessageEvent e) {
 		if (e.getSender().equals("")) {
