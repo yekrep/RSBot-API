9ff0646391c4560d302f05e64c529098362bbf37
Joe Haddad <timer@powerbot.org>
2014-04-08 23:05:32 -0400

    Ignore null items

--- src/org/powerbot/script/rt4/Inventory.java
+++ src/org/powerbot/script/rt4/Inventory.java
@@ -18,7 +18,7 @@ public class Inventory extends ItemQuery<Item> {
 		if (sub != null) {
 			for (final Component c : sub.components()) {
 				final int id = c.itemId();
-				if (id <= 0) {
+				if (id <= 0 || id == 6512 || c.itemStackSize() <= 0) {
 					continue;
 				}
 				items.add(new Item(ctx, c, id, c.itemStackSize()));
