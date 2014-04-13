514d6aac91510d4e3b6cc2d5e2eba9e751bd3192
Joe Haddad <timer@powerbot.org>
2014-04-13 16:10:13 -0400

    Add requested inventory API

--- src/org/powerbot/script/rt4/Inventory.java
+++ src/org/powerbot/script/rt4/Inventory.java
@@ -3,6 +3,8 @@ package org.powerbot.script.rt4;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.powerbot.bot.rt4.client.Client;
+
 public class Inventory extends ItemQuery<Item> {
 	public Inventory(final ClientContext ctx) {
 		super(ctx);
@@ -34,9 +36,67 @@ public class Inventory extends ItemQuery<Item> {
 				continue;
 			}
 			items.add(new Item(ctx, c, i, id, stack));
-
 		}
 		return items;
+	}
+
+	public Item[] items() {
+		final Item[] items = new Item[28];
+		final Component sub = getComponent();
+		if (sub == null) {
+			final Component c = ctx.widgets.widget(149).component(0);
+			final int[] ids = c.itemIds(), stacks = c.itemStackSizes();
+			for (int i = 0; i < Math.min(ids != null ? ids.length : -1, stacks != null ? stacks.length : -1); i++) {
+				final int id = ids[i], stack = stacks[i];
+				if (id >= 1) {
+					items[i] = new Item(ctx, c, i, id, stack);
+				} else {
+					items[i] = nil();
+				}
+			}
+			return items;
+		}
+
+		final Component[] comps = sub.components();
+		final int len = comps.length;
+		for (int i = 0; i < 28; i++) {
+			if (i >= len) {
+				items[i] = nil();
+				continue;
+			}
+			final Component c = comps[i];
+			final int id = c.itemId();
+			if (id <= 0 || id == 6512 || c.itemStackSize() <= 0) {
+				items[i] = nil();
+				continue;
+			}
+			items[i] = new Item(ctx, c, id, c.itemStackSize());
+		}
+		return items;
+	}
+
+	public Item itemAt(final int index) {
+		return index >= 0 && index < 28 ? items()[index] : nil();
+	}
+
+	public int selectionType() {
+		final Client client = ctx.client();
+		return client != null ? client.getSelectionType() : 0;
+	}
+
+	public int selectedItemIndex() {
+		final Client client = ctx.client();
+		return client != null && selectionType() == 1 ? client.getSelectionIndex() : -1;
+	}
+
+	public Item selectedItem() {
+		final int index = selectedItemIndex();
+		return itemAt(index);
+	}
+
+	public Component component() {
+		final Component c = getComponent();
+		return c != null ? c : ctx.widgets.widget(149).component(0);
 	}
 
 	@Override
