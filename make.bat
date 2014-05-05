bf2abf1bd2af3a081420ba5133325b1de35c0853
A. Ahmed <paris@powerbot.org>
2014-05-05 19:25:41 +0100

    Update Windows make script

--- make.bat
+++ make.bat
@@ -12,7 +12,7 @@ GOTO :eof
 :setvars
 SET name=RSBot
 SET cc=javac
-SET cflags=-g:none
+SET cflags=-target 1.6 -source 1.6
 SET src=src
 SET lib=lib
 SET res=resources
@@ -21,7 +21,6 @@ SET dist=%lib%\%name%.jar
 SET lstf=temp.txt
 SET imgdir=%res%\images
 SET manifest=%res%\Manifest.txt
-SET csbat=codesigner.bat
 CALL "jdk.bat"
 GOTO :eof
 
@@ -33,9 +32,6 @@ ECHO Packing JAR
 CALL :pack
 ECHO Obfuscating
 CALL :obfuscate
-CALL :codesign
-CALL :rename
-CALL :docs
 CALL :end
 GOTO :eof
 
@@ -52,7 +48,7 @@ GOTO :eof
 IF EXIST "%dist%" DEL /F /Q "%dist%"
 IF EXIST "%lstf%" DEL /F /Q "%lstf%"
 COPY "%manifest%" "%lstf%" > NUL
-jar cfm "%dist%" "%lstf%" -C "%out%" . "license.txt" %imgdir%\*.png
+jar cfm "%dist%" "%lstf%" -C "%out%" . %imgdir%\*.png
 DEL /F /Q "%lstf%"
 GOTO :eof
 
@@ -60,20 +56,6 @@ GOTO :eof
 CD "%lib%"
 CALL java -jar allatori.jar allatori.xml
 CD ..
-GOTO :eof
-
-:codesign
-IF EXIST "%csbat%" CALL "%csbat%"
-GOTO :eof
-
-:rename
-FOR /F "delims=" %%G in ('java -jar "%dist%" -v') DO @SET version=%%G
-SET version=%version: =%
-MOVE /Y "%dist%" "%lib%\%name%-%version%.jar"
-GOTO :eof
-
-:docs
-CALL docs.bat
 GOTO :eof
 
 :end
