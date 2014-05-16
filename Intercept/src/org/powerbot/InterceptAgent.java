a481097f47da6a4914a13907427e24f01c7be0d9
A. Ahmed <paris@powerbot.org>
2014-05-16 17:18:09 +0100

    Basis of instrumentation engine

---
+++ Intercept/src/org/powerbot/InterceptAgent.java
@@ -0,0 +1,46 @@
+package org.powerbot;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.lang.instrument.Instrumentation;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.security.ProtectionDomain;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class InterceptAgent implements ClassFileTransformer {
+	private static final AtomicReference<ClassFileTransformer> obj = new AtomicReference<ClassFileTransformer>(null);
+	private static final AtomicReference<Method> proxy = new AtomicReference<Method>(null);
+
+	public static void premain(final String agentArgs, final Instrumentation instrumentation) {
+		try {
+			final Class<? extends ClassFileTransformer> c = Class.forName(InterceptAgent.class.getName() + "Proxy").asSubclass(ClassFileTransformer.class);
+			obj.set(c.newInstance());
+			proxy.set(c.getMethod("transform", new Class[]{ClassLoader.class, String.class, Class.class, ProtectionDomain.class, byte[].class}));
+			instrumentation.addTransformer(new InterceptAgent(), true);
+		} catch (final Exception e) {
+			throw new RuntimeException(e);
+		}
+	}
+
+	@SuppressWarnings("unused")
+	public static void agentmain(final String agentArgs, final Instrumentation instrumentation) {
+		premain(agentArgs, instrumentation);
+	}
+
+	@Override
+	public byte[] transform(final ClassLoader loader, final String className, final Class<?> classBeingRedefined,
+	                        final ProtectionDomain protectionDomain, final byte[] classfileBuffer) throws IllegalClassFormatException {
+		if (proxy.get() == null) {
+			return new byte[0];
+		}
+
+		try {
+			return (byte[]) proxy.get().invoke(obj.get(), loader, className, classBeingRedefined, protectionDomain, classfileBuffer);
+		} catch (final IllegalAccessException e) {
+			throw new RuntimeException(e);
+		} catch (final InvocationTargetException e) {
+			throw new RuntimeException(e);
+		}
+	}
+}
---
+++ src/org/powerbot/InterceptAgentProxy.java
@@ -0,0 +1,14 @@
+package org.powerbot;
+
+import java.lang.instrument.ClassFileTransformer;
+import java.lang.instrument.IllegalClassFormatException;
+import java.security.ProtectionDomain;
+
+public class InterceptAgentProxy implements ClassFileTransformer {
+
+	@Override
+	public byte[] transform(final ClassLoader loader, final String className, final Class<?> classBeingRedefined,
+	                        final ProtectionDomain protectionDomain, final byte[] classfileBuffer) throws IllegalClassFormatException {
+		return new byte[0];
+	}
+}
