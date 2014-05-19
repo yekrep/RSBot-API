e6809d457f0990b5ffbc270e1e9f535979a8ae54
A. Ahmed <paris@powerbot.org>
2014-05-19 20:55:51 +0100

    Move method to utility class

--- src/org/powerbot/Instrument.java
+++ src/org/powerbot/Instrument.java
@@ -1,6 +1,10 @@
 package org.powerbot;
 
 import java.lang.instrument.Instrumentation;
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicReference;
 
 public class Instrument {
@@ -8,5 +12,17 @@ public class Instrument {
 
 	public static Instrumentation get() {
 		return instance.get();
+	}
+
+	public static List<Field> getFields(final Class<?> cls) {
+		final List<Field> f = new ArrayList<Field>();
+		Collections.addAll(f, cls.getDeclaredFields());
+
+		final Class<?> p = cls.getSuperclass();
+		if (p != null && !p.equals(Object.class)) {
+			f.addAll(getFields(p));
+		}
+
+		return f;
 	}
 }
--- src/org/powerbot/bot/rt6/Bot.java
+++ src/org/powerbot/bot/rt6/Bot.java
@@ -4,13 +4,12 @@ import java.awt.Component;
 import java.awt.EventQueue;
 import java.lang.reflect.Field;
 import java.security.ProtectionDomain;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.Hashtable;
 import java.util.List;
 import java.util.concurrent.Callable;
 
 import org.powerbot.Configuration;
+import org.powerbot.Instrument;
 import org.powerbot.bot.SelectiveEventQueue;
 import org.powerbot.bot.reflect.LoaderUtils;
 import org.powerbot.bot.rt6.activation.EventDispatcher;
@@ -26,18 +25,6 @@ public final class Bot extends org.powerbot.script.Bot<ClientContext> {
 	@Override
 	protected ClientContext newContext() {
 		return ClientContext.newContext(this);
-	}
-
-	public List<Field> getFields(final Class<?> cls) {
-		final List<Field> f = new ArrayList<Field>();
-		Collections.addAll(f, cls.getDeclaredFields());
-
-		final Class<?> p = cls.getSuperclass();
-		if (p != null && !p.equals(Object.class)) {
-			f.addAll(getFields(p));
-		}
-
-		return f;
 	}
 
 	@Override
@@ -75,7 +62,7 @@ public final class Bot extends org.powerbot.script.Bot<ClientContext> {
 				Thread.yield();
 			}
 			f1.setAccessible(a1);
-			final List<Field> f1x = getFields(o1.getClass());
+			final List<Field> f1x = Instrument.getFields(o1.getClass());
 			Hashtable<String, byte[]> v0 = null;
 			Hashtable<String, Class<?>> v1 = null;
 			ProtectionDomain v2 = null;
