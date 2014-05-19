47b6e511666b8b35896db3151048d113160bc5d8
Joe Haddad <timer@powerbot.org>
2014-05-19 17:00:57 -0400

    Fix compiling

--- src/org/powerbot/bot/rt6/client/JavaRender.java
+++ src/org/powerbot/bot/rt6/client/JavaRender.java
@@ -1,4 +1,9 @@
 package org.powerbot.bot.rt6.client;
 
+import org.powerbot.bot.ReflectionEngine;
+
 public class JavaRender extends Render {
+	public JavaRender(final ReflectionEngine engine, final Object parent) {
+		super(engine, parent);
+	}
 }
