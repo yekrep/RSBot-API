ea4fb5bd90d73a7a5cc1df67193fdc4adfd3b9e1
Joe Haddad <timer@powerbot.org>
2014-05-23 23:34:29 -0400

    Properly create hash table

--- src/org/powerbot/script/rt6/GroundItems.java
+++ src/org/powerbot/script/rt6/GroundItems.java
@@ -45,8 +45,8 @@ public class GroundItems extends GroundItemQuery<GroundItem> {
 		for (int x = bx; x < bx + 104; x++) {
 			for (int y = by; y < by + 104; y++) {
 				id = x | y << 14 | plane << 28;
-				if (cache == null) {
 				cache = new NodeListCache(client.reflector, org.powerbot.bot.rt6.tools.HashTable.lookup(table, id));
+				if (cache.obj.get() == null) {
 					continue;
 				}
 				for (final RSItem item : NodeQueue.get(cache.getNodeList(), RSItem.class)) {
