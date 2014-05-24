d8b9f955ce3070a28f18ec06afa361d8442c47f8
Joe Haddad <timer@powerbot.org>
2014-05-24 01:31:09 -0400

    Instantiate reflector proxys

--- src/org/powerbot/bot/rt4/tools/NodeQueue.java
+++ src/org/powerbot/bot/rt4/tools/NodeQueue.java
@@ -1,8 +1,11 @@
 package org.powerbot.bot.rt4.tools;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.powerbot.bot.Reflector;
 import org.powerbot.bot.rt4.client.Node;
 import org.powerbot.bot.rt4.client.NodeDeque;
 
@@ -10,14 +13,24 @@ public class NodeQueue {
 	public static <E extends Node> List<E> get(final NodeDeque q, final Class<E> type) {
 		final List<E> list = new ArrayList<E>();
 		Node e;
-
+		final Constructor<E> c;
+		try {
+			c = type.getDeclaredConstructor(Reflector.class, Object.class);
+		} catch (final NoSuchMethodException ignored) {
+			return list;
+		}
 		if (q == null || (e = q.getSentinel()) == null) {
 			return list;
 		}
 		e = e.getNext();
 
-		for (; e != null && type.isInstance(e); e = e.getNext()) {
-			list.add(type.cast(e));
+		for (; !e.isNull() && e.isTypeOf(type); e = e.getNext()) {
+			try {
+				list.add(c.newInstance(q.reflector, e));
+			} catch (final InstantiationException ignored) {
+			} catch (final IllegalAccessException ignored) {
+			} catch (final InvocationTargetException ignored) {
+			}
 		}
 
 		return list;
