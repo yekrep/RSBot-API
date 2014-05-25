534926e3015f621b465156380662a8f147f247d5
Joe Haddad <timer@powerbot.org>
2014-05-24 22:19:23 -0400

    Remove old classes

--- src/org/powerbot/bot/rt6/client/DXRender.java
+++
@@ -1,9 +0,0 @@
-package org.powerbot.bot.rt6.client;
-
-import org.powerbot.bot.Reflector;
-
-public class DXRender extends Render {
-	public DXRender(final Reflector engine, final Object parent) {
-		super(engine, parent);
-	}
-}
--- src/org/powerbot/bot/rt6/client/GLRender.java
+++
@@ -1,9 +0,0 @@
-package org.powerbot.bot.rt6.client;
-
-import org.powerbot.bot.Reflector;
-
-public class GLRender extends Render {
-	public GLRender(final Reflector engine, final Object parent) {
-		super(engine, parent);
-	}
-}
