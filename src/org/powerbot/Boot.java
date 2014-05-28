0dcbdf82197c7d83ebde302fee26ce49da54964d
A. Ahmed <paris@powerbot.org>
2014-05-28 22:24:39 +0100

    Remove FROMJAR reference

--- src/org/powerbot/Boot.java
+++ src/org/powerbot/Boot.java
@@ -73,10 +73,8 @@ public class Boot implements Runnable {
 				std.print('[');
 				std.print(record.getLevel().getName());
 				std.print("] ");
-				if (!Configuration.FROMJAR) {
-					std.print(record.getLoggerName());
-					std.print(": ");
-				}
+				std.print(record.getLoggerName());
+				std.print(": ");
 				std.print(text);
 				final Throwable throwable = record.getThrown();
 				if (throwable != null) {
