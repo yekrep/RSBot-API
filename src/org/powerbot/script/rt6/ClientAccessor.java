5be7f691d1a7d90597a35213f222eb2383b82652
A. Ahmed <paris@powerbot.org>
2014-05-29 06:14:14 +0100

    Remove invalid javadocs

--- src/org/powerbot/script/rt6/ClientAccessor.java
+++ src/org/powerbot/script/rt6/ClientAccessor.java
@@ -1,12 +1,7 @@
 package org.powerbot.script.rt6;
 
-/**
- * {@inheritDoc}
- */
 public abstract class ClientAccessor extends org.powerbot.script.ClientAccessor<ClientContext> {
-	/**
-	 * {@inheritDoc}
-	 */
+
 	public ClientAccessor(final ClientContext ctx) {
 		super(ctx);
 	}
