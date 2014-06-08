99e7040a154a40e1b926dbbf812bfd2d75021304
A. Ahmed <paris@powerbot.org>
2014-06-08 15:38:43 +0100

    Simplify widget closer

--- src/org/powerbot/bot/rt6/WidgetCloser.java
+++ src/org/powerbot/bot/rt6/WidgetCloser.java
@@ -1,13 +1,7 @@
 package org.powerbot.bot.rt6;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.concurrent.atomic.AtomicLong;
-
 import org.powerbot.script.Condition;
 import org.powerbot.script.PollingScript;
-import org.powerbot.script.Random;
 import org.powerbot.script.rt6.ClientContext;
 import org.powerbot.script.rt6.Component;
 
@@ -27,67 +21,26 @@ public class WidgetCloser extends PollingScript<ClientContext> {
 			1223 << 16 | 18,//Achievement continue button
 	};
 
-	private volatile Component component;
-	private final AtomicLong time;
-	private final AtomicInteger tries;
-
 	public WidgetCloser() {
 		priority.set(5);
-		time = new AtomicLong(0L);
-		tries = new AtomicInteger();
 	}
 
 	@Override
 	public void poll() {
-		if (!isValid()) {
-			if (threshold.contains(this)) {
-				threshold.remove(this);
-			}
-			return;
-		}
-		if (!threshold.contains(this)) {
-			threshold.add(this);
-		}
-
-		final Component component = this.component;
-		if (component == null) {
+		if (ctx.properties.getProperty("widget.closer.disable", "").equals("true") || ctx.bank.opened()) {
 			return;
 		}
 
-		if (tries.incrementAndGet() >= 3) {
-			time.set(System.nanoTime() + TimeUnit.NANOSECONDS.toSeconds(Random.nextInt(30, 61)));
-			tries.set(0);
-			return;
-		}
-
-		if (component.click(true)) {
-			if (Condition.wait(new Callable<Boolean>() {
-				@Override
-				public Boolean call() {
-					return !component.visible();
-				}
-			}, 175)) {
-				tries.set(0);
+		for (final int id : COMPONENTS) {
+			final Component c = ctx.widgets.component(id >> 16, id & 0xffff);
+			if (c.visible() && c.click()) {
+				Condition.wait(new Condition.Check() {
+					@Override
+					public boolean poll() {
+						return !c.visible();
+					}
+				});
 			}
 		}
-	}
-
-	private boolean isValid() {
-		if (ctx.properties.getProperty("widget.closer.disable", "").equals("true")) {
-			return false;
-		}
-		if (System.nanoTime() < time.get() || ctx.bank.opened()) {
-			return false;
-		}
-
-		for (final int p : COMPONENTS) {
-			component = ctx.widgets.component(p >> 16, p & 0xffff);
-			if (component != null && component.visible()) {
-				break;
-			} else {
-				component = null;
-			}
-		}
-		return component != null;
 	}
 }
