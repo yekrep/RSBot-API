7f5d3053d95f4808e700cbdce38d41f549f4010a
Joe Haddad <timer@powerbot.org>
2014-09-20 18:35:22 -0400

    Fix buttons

--- src/org/powerbot/gui/BotPanel.java
+++ src/org/powerbot/gui/BotPanel.java
@@ -55,14 +55,8 @@ class BotPanel extends JPanel implements ActionListener {
 		mode.setBorder(BorderFactory.createEmptyBorder(10, 0, 0, 0));
 		mode.setBackground(getBackground());
 		rs3 = new JButton("RS3");
-		rs3.setBackground(getBackground());
-		rs3.setFocusable(false);
-		rs3.addActionListener(this);
 		mode.add(rs3, new GridBagConstraints());
 		os = new JButton("OS");
-		os.setBackground(getBackground());
-		os.setFocusable(false);
-		os.addActionListener(this);
 		mode.add(os, new GridBagConstraints());
 		final GridBagConstraints c = new GridBagConstraints();
 		c.gridy = 1;
