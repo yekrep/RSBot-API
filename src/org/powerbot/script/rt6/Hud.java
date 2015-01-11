7ed6c0473f885555f1d7ea5148f3a315f6992dee
Joe Haddad <timer@powerbot.org>
2015-01-11 14:39:17 -0500

    Update skill texture

--- src/org/powerbot/script/rt6/Hud.java
+++ src/org/powerbot/script/rt6/Hud.java
@@ -72,7 +72,7 @@ public class Hud extends ClientAccessor {
 		EMOTES(Menu.NONE, 18741, 18776, 590, 14, new LegacyTab(1830, "Emotes")),
 		MINIMAP(Menu.NONE, 18742, 0, 1465, 12, null),
 
-		SKILLS(Menu.HERO, 18738, 18775, 1466, 0, new LegacyTab(1818, "Skills")),
+		SKILLS(Menu.HERO, 18738, 24429, 1466, 0, new LegacyTab(1818, "Skills")),
 		ACTIVE_TASK(Menu.HERO, 18735, 18789, 1220, 0, new LegacyTab(1820, "Active Task")),
 		BACKPACK(Menu.GEAR, 18732, 18772, Constants.BACKPACK_WIDGET, Constants.BACKPACK_CONTAINER, new LegacyTab(1821, "Backpack")),
 		WORN_EQUIPMENT(Menu.GEAR, 18733, 18773, Constants.EQUIPMENT_WIDGET, 0, new LegacyTab(1822, "Worn Equipment")),
