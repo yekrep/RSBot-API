8a00b57947c44dd7a04ca4465ff2b7afdc27b09d
A. Ahmed <paris@powerbot.org>
2015-02-10 15:07:04 +0000

    Version 6055

--- src/org/powerbot/Configuration.java
+++ src/org/powerbot/Configuration.java
@@ -10,7 +10,7 @@ import org.powerbot.util.StringUtils;
 
 public class Configuration {
 	public static final String NAME = "RSBot";
-	public static final int VERSION = 6054;
+	public static final int VERSION = 6055;
 
 	public static final OperatingSystem OS;
 	public static final boolean JRE6;
