4064686b6940b5b03edf7232a6f4d55166b905e1
Joe Haddad <timer@powerbot.org>
2015-03-19 18:16:50 -0400

    Update lobby

--- src/org/powerbot/script/rt6/Constants.java
+++ src/org/powerbot/script/rt6/Constants.java
@@ -108,15 +108,15 @@ public final class Constants {
 	public static final int HUD_MENU_WINDOWS_LIST = 4;
 
 	public static final int LOBBY_WIDGET = 906;
-	public static final int LOBBY_PLAY = 152;
-	public static final int LOBBY_CURRENT_WORLD = 511;
-	public static final int LOBBY_CLOSE = 486;
+	public static final int LOBBY_PLAY = 153;
+	public static final int LOBBY_CURRENT_WORLD = 512;
+	public static final int LOBBY_CLOSE = 487;
 	public static final int LOBBY_CLOSE_SUB = 1;
-	public static final int LOBBY_TABS = 483;
+	public static final int LOBBY_TABS = 484;
 	public static final int LOBBY_TAB_START = 3;
 	public static final int LOBBY_TAB_LENGTH = 4;
 	public static final int LOBBY_TAB_CURRENT = 27;
-	public static final int LOBBY_ERROR = 471;
+	public static final int LOBBY_ERROR = 472;
 	public static final int LOBBY_WORLDS = 910;
 	public static final int LOBBY_WORLDS_VIEWPORT = 68;
 	public static final int LOBBY_WORLDS_BARS = 70;
