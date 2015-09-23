5f4f1dce6720bacae51f8ce63a2af74d28c148cd
Joe Haddad <timer@powerbot.org>
2015-09-22 23:39:06 -0400

    Npc count

--- rt4/Npc.java
+++ rt4/Npc.java
@@ -2,6 +2,7 @@ package org.powerbot.script.rt4;
 
 import java.awt.Color;
 import java.io.File;
+import java.util.Arrays;
 
 import org.powerbot.bot.cache.CacheWorker;
 import org.powerbot.bot.rt4.HashTable;
@@ -13,7 +14,7 @@ import org.powerbot.script.Actionable;
 import org.powerbot.script.Identifiable;
 
 public class Npc extends Actor implements Identifiable, Actionable {
-	private final CacheWorker CACHE_WORKER = new CacheWorker(new File(
+	private static final CacheWorker CACHE_WORKER = new CacheWorker(new File(
 			System.getProperty("user.home"), "jagexcache/oldschool/LIVE"
 	));
 	public static final Color TARGET_COLOR = new Color(255, 0, 255, 15);
@@ -59,29 +60,30 @@ public class Npc extends Actor implements Identifiable, Actionable {
 		if (client == null) {
 			return -1;
 		}
-		final NpcConfig config = npc != null ? npc.getConfig() : null;
-		if (config != null) {
-			final int varbit = config.getVarbit(), si = config.getVarpbitIndex();
-			int index = -1;
-			if (varbit != -1) {
-				final Cache cache = client.getVarbitCache();
-				final Varbit varBit = HashTable.lookup(cache.getTable(), varbit, Varbit.class);
-				if (varBit.obj.get() != null) {
-					final int mask = lookup[varBit.getEndBit() - varBit.getStartBit()];
-					index = ctx.varpbits.varpbit(varBit.getIndex()) >> varBit.getStartBit() & mask;
-				}
-			} else if (si != -1) {
-				index = ctx.varpbits.varpbit(si);
-			}
-			if (index >= 0) {
-				final int[] configs = config.getConfigs();
-				if (configs != null && index < configs.length && configs[index] != -1) {
-					return configs[index];
-				}
-			}
-			return config.getId();
+		final NpcConfig c = npc.getConfig();
+		if (c.isNull()) {
+			return -1;
 		}
-		return -1;
+		final int varbit = c.getVarbit(), si = c.getVarpbitIndex();
+		int index = -1;
+		if (varbit != -1) {
+			final Cache cache = client.getVarbitCache();
+			final Varbit varBit = HashTable.lookup(cache.getTable(), varbit, Varbit.class);
+			if (!varBit.isNull()) {
+				final int mask = lookup[varBit.getEndBit() - varBit.getStartBit()];
+				index = ctx.varpbits.varpbit(varBit.getIndex()) >> varBit.getStartBit() & mask;
+			}
+		} else if (si != -1) {
+			index = ctx.varpbits.varpbit(si);
+		}
+		if (index >= 0) {
+			final int[] configs = c.getConfigs();
+			System.out.println(Arrays.toString(configs) + " " + index);
+			if (index < configs.length && configs[index] != -1) {
+				return configs[index];
+			}
+		}
+		return c.getId();
 	}
 
 	@Override
