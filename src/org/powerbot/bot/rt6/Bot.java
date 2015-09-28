805c4709df97545dc92e147363d62eb864d14ca4
A. Ahmed <paris@powerbot.org>
2015-09-28 02:36:14 -0400

    Add code to grab loaded classes.

--- src/org/powerbot/bot/rt6/Bot.java
+++ src/org/powerbot/bot/rt6/Bot.java
@@ -1,6 +1,9 @@
 package org.powerbot.bot.rt6;
 
+import java.awt.Component;
+import java.lang.reflect.Field;
 import java.util.Hashtable;
+import java.util.List;
 import java.util.TimerTask;
 
 import org.powerbot.bot.AbstractBot;
@@ -26,6 +29,76 @@ public final class Bot extends AbstractBot<ClientContext> {
 
 	@Override
 	protected void reflect(final ReflectorSpec s) {
+		final Component o0 = chrome.target.get();
+
+		for (final Field f1 : o0.getClass().getDeclaredFields()) {
+			final boolean a1 = f1.isAccessible();
+			f1.setAccessible(true);
+			if (Class.class.equals(f1.getType())) {
+				Object o1 = null;
+				while (o1 == null) {
+					try {
+						o1 = f1.get(o0);
+					} catch (final IllegalAccessException e) {
+						throw new RuntimeException(e);
+					}
+				}
+			}
+			f1.setAccessible(a1);
+		}
+
+		for (final Field f1 : o0.getClass().getDeclaredFields()) {
+			final boolean a1 = f1.isAccessible();
+			f1.setAccessible(true);
+			final Object o1;
+			try {
+				o1 = f1.get(o0);
+			} catch (final IllegalAccessException e) {
+				throw new RuntimeException(e);
+			}
+			f1.setAccessible(a1);
+			if (o1 == null) {
+				continue;
+			}
+			final List<Field> f1x = Reflector.getFields(o1.getClass());
+			Hashtable<String, Class<?>> v1 = null;
+
+			for (final Field f2 : f1x) {
+				final Class<?> c2x = f2.getType();
+				if (!c2x.equals(Object.class)) {
+					continue;
+				}
+
+				final boolean a2 = f2.isAccessible();
+				f2.setAccessible(true);
+				Object o2 = null;
+				while (o2 == null) {
+					try {
+						o2 = f2.get(o1);
+					} catch (final IllegalAccessException e) {
+						throw new RuntimeException(e);
+					}
+				}
+				f2.setAccessible(a2);
+				final Class<?> c2 = o2.getClass();
+
+				if (c2.equals(Hashtable.class)) {
+					final Hashtable x = (Hashtable) o2;
+					if (x.keys().nextElement().getClass().equals(String.class)) {
+						final Class<?> c3 = x.elements().nextElement().getClass();
+						if (c3.getClass().equals(Class.class)) {
+							@SuppressWarnings("unchecked")
+							final Hashtable<String, Class<?>> xs = x;
+							v1 = xs;
+						}
+					}
+				}
+			}
+
+			if (v1 != null) {
+				loaded = v1;
+			}
+		}
 		final Reflector r = new Reflector(loaded.get("client").getClassLoader(), s);
 		ctx.client(new Client(r, null));
 		ctx.chat.register();
