12718bf929a989d9eb98c6d009e12a8b0433d1e2
Joe Haddad <timer@powerbot.org>
2015-09-28 22:43:29 -0400

    Fix rs3 loading

--- src/org/powerbot/bot/rt6/Bot.java
+++ src/org/powerbot/bot/rt6/Bot.java
@@ -1,10 +1,10 @@
 package org.powerbot.bot.rt6;
 
-import java.awt.Component;
 import java.io.File;
 import java.lang.reflect.Field;
-import java.util.Hashtable;
-import java.util.List;
+import java.lang.reflect.Modifier;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.TimerTask;
 
 import org.powerbot.bot.AbstractBot;
@@ -21,7 +21,6 @@ public final class Bot extends AbstractBot<ClientContext> {
 	public static final CacheWorker CACHE_WORKER = new CacheWorker(new File(
 			System.getProperty("user.home"), "jagexcache/runescape/LIVE"
 	));
-	private Hashtable<String, Class<?>> loaded;
 
 	public Bot(final BotChrome chrome) {
 		super(chrome, new EventDispatcher());
@@ -34,77 +33,35 @@ public final class Bot extends AbstractBot<ClientContext> {
 
 	@Override
 	protected void reflect(final ReflectorSpec s) {
-		final Component o0 = chrome.target.get();
-
-		for (final Field f1 : o0.getClass().getDeclaredFields()) {
-			final boolean a1 = f1.isAccessible();
-			f1.setAccessible(true);
-			if (Class.class.equals(f1.getType())) {
-				Object o1 = null;
-				while (o1 == null) {
-					try {
-						o1 = f1.get(o0);
-					} catch (final IllegalAccessException e) {
-						throw new RuntimeException(e);
-					}
-				}
+		final Class c = chrome.target.get().getClass();
+		final Queue<Field> q = new LinkedList<Field>();
+		for (final Field f : c.getDeclaredFields()) {
+			if (f.getType().getName().equals(Object.class.getName())) {
+				q.offer(f);
 			}
-			f1.setAccessible(a1);
 		}
-
-		for (final Field f1 : o0.getClass().getDeclaredFields()) {
-			final boolean a1 = f1.isAccessible();
-			f1.setAccessible(true);
-			final Object o1;
+		if (q.size() != 1) {
+			System.out.println("Failed to identify client object.");
+			return;
+		}
+		final Field f = q.poll();
+		f.setAccessible(true);
+		Object o = null;
+		for (int i = 0; o == null && i < 10; ++i) {
 			try {
-				o1 = f1.get(o0);
-			} catch (final IllegalAccessException e) {
-				throw new RuntimeException(e);
+				o = f.get((f.getModifiers() & Modifier.STATIC) == Modifier.STATIC ? null : chrome.target.get());
+			} catch (final IllegalAccessException ignored) {
 			}
-			f1.setAccessible(a1);
-			if (o1 == null) {
-				continue;
-			}
-			final List<Field> f1x = Reflector.getFields(o1.getClass());
-			Hashtable<String, Class<?>> v1 = null;
-
-			for (final Field f2 : f1x) {
-				final Class<?> c2x = f2.getType();
-				if (!c2x.equals(Object.class)) {
-					continue;
-				}
-
-				final boolean a2 = f2.isAccessible();
-				f2.setAccessible(true);
-				Object o2 = null;
-				while (o2 == null) {
-					try {
-						o2 = f2.get(o1);
-					} catch (final IllegalAccessException e) {
-						throw new RuntimeException(e);
-					}
-				}
-				f2.setAccessible(a2);
-				final Class<?> c2 = o2.getClass();
-
-				if (c2.equals(Hashtable.class)) {
-					final Hashtable x = (Hashtable) o2;
-					if (x.keys().nextElement().getClass().equals(String.class)) {
-						final Class<?> c3 = x.elements().nextElement().getClass();
-						if (c3.getClass().equals(Class.class)) {
-							@SuppressWarnings("unchecked")
-							final Hashtable<String, Class<?>> xs = x;
-							v1 = xs;
-						}
-					}
-				}
-			}
-
-			if (v1 != null) {
-				loaded = v1;
+			try {
+				Thread.sleep(500);
+			} catch (final InterruptedException ignored) {
 			}
 		}
-		final Reflector r = new Reflector(loaded.get("client").getClassLoader(), s);
+		if (o == null) {
+			System.out.println("Failed to get client object.");
+			return;
+		}
+		final Reflector r = new Reflector(o.getClass().getClassLoader(), s);
 		ctx.client(new Client(r, null));
 		ctx.chat.register();
 
