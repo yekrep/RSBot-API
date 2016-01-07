yekrep/RSBot-API

commit a7c50440402b74b2fb2a93a111899ea6bbb76445

Updated rt4 Equipment#itemAt(Slot), fixed small widgets bug, updated minimap component parsing

rt4/Widget.java
@@ -28,7 +28,7 @@ public class Widget extends ClientAccessor implements Identifiable, Validatable 
 	}
 
 	public synchronized Component component(final int index) {
-		if (index < 0) {
+		if (index < 1) {
 			return new Component(ctx, this, -1);
 		}
 		if (index < sparseCache.length && sparseCache[index] != null) {

rt4/Equipment.java
@@ -21,10 +21,10 @@ public class Equipment extends ItemQuery<Item> {
 		final int[] data = ctx.players.local().appearance();
 		for (final Slot slot : Slot.values()) {
 			final int index = slot.getIndex();
-			final Component c = ctx.widgets.widget(Constants.EQUIPMENT_WIDGET).component(slot.getComponentIndex()).component(1);
 			if (index < 0 || index >= data.length || data[index] < 0) {
 				continue;
 			}
+			final Component c = ctx.widgets.widget(Constants.EQUIPMENT_WIDGET).component(slot.getComponentIndex()).component(1);
 			items.add(new Item(ctx, c, data[index], c.visible() ? c.itemStackSize() : 1));
 		}
 		return items;
@@ -37,11 +37,13 @@ public class Equipment extends ItemQuery<Item> {
 	 * @return the {@link org.powerbot.script.rt4.Item} in the provided slot
 	 */
 	public Item itemAt(final Slot slot) {
-		final Component c = ctx.widgets.widget(Constants.EQUIPMENT_WIDGET).component(slot.getComponentIndex()).component(1);
-		if (c.itemId() < 0 || c.itemStackSize() < 0) {
+		final int index = slot.getIndex();
+		final int[] data = ctx.players.local().appearance();
+		if (index < 0 || index >= data.length || data[index] < 0) {
 			return nil();
 		}
-		return new Item(ctx, c, c.itemId(), c.itemStackSize());
+		final Component c = ctx.widgets.widget(Constants.EQUIPMENT_WIDGET).component(slot.getComponentIndex()).component(1);
+		return new Item(ctx, c, data[index], c.visible() ? c.itemStackSize() : 1);
 	}
 
 	/**

rt4/Game.java
@@ -150,8 +150,19 @@ public class Game extends ClientAccessor {
 		d[4] = d[1] * d[2] + d[3] * d[0] >> 16;
 		d[5] = d[2] * d[0] - d[1] * d[3] >> 16;
 		final boolean r = ctx.game.resizable();
-		final Point centre = r ? ctx.widgets.component(161, 17).centerPoint() : ctx.widgets.component(548, 6).centerPoint();
+		final Point centre = minimapComponent(r ? 161 : 548).centerPoint();
 		return new Point(centre.x + d[4], centre.y + d[5]);
+	}
+
+	private Component minimapComponent(int widgetId) {
+		final Widget widget = ctx.widgets.widget(widgetId);
+		for (Component comp : widget.components()) {
+			//Content type specific to minimap
+			if (comp.contentType() == 1338) {
+				return comp;
+			}
+		}
+		return new Component(ctx, widget, -1);
 	}
 
 	public int tileHeight(final int relativeX, final int relativeZ) {

commit 98815d4ffdeab57981130f0d47db0bbda600e0ce

Fix bug introduced in a7c50440402b74b2fb2a93a111899ea6bbb76445

rt4/Widget.java
@@ -28,7 +28,7 @@ public class Widget extends ClientAccessor implements Identifiable, Validatable 
 	}
 
 	public synchronized Component component(final int index) {
-		if (index < 1) {
+		if (index < 0) {
 			return new Component(ctx, this, -1);
 		}
 		if (index < sparseCache.length && sparseCache[index] != null) {