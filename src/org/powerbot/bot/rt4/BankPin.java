45b993d2ab3a279bfd3bafd2d3947987f8387288
Joe Haddad <timer@powerbot.org>
2016-07-18 18:57:29 -0400

    Clean up code

--- src/org/powerbot/bot/rt4/BankPin.java
+++ src/org/powerbot/bot/rt4/BankPin.java
@@ -32,16 +32,17 @@ public class BankPin extends PollingScript<ClientContext> {
 			return;
 		}
 
-		for (final Component c : ctx.widgets.widget(Constants.BANKPIN_WIDGET)
-				.components()) {
-			final Component child = c.component(1);
-			if (child == null || !child.visible()) {
+		for (final Component c : ctx.widgets.widget(Constants.BANKPIN_WIDGET).components()) {
+			if (c.textColor() != 0 || c.width() != 64 || c.height() != 64 || c.componentCount() != 2 || !c.visible()) {
 				continue;
 			}
+			final Component child = c.component(1);
+			if (!child.visible()) {
+				continue;
+			}
+			//TODO: re-evaluate this to get rid of count; or fail out
 			final String text = child.text();
-			if (c.visible() && c.textColor() == 0 && c.width() == 64 &&
-					c.height() == 64 && c.componentCount() == 2 &&
-					text.equals(pin.charAt(count % 4) + "")) {
+			if (text.equals(Integer.toString(pin.charAt(count % 4)))) {
 				if (c.click()) {
 					count++;
 					Condition.wait(new Condition.Check() {
