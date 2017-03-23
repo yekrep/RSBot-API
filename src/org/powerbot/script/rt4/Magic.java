bd6eb68139faeec1eaeaf8ec03187919cc28118f
Grant <MrFreezerburnt@users.noreply.github.com>
2017-03-22 22:18:28 -0500

    Update Magic.java

--- src/org/powerbot/script/rt4/Magic.java
+++ src/org/powerbot/script/rt4/Magic.java
@@ -52,7 +52,7 @@ public class Magic extends ClientAccessor {
 			return false;
 		}
 		final Component c = component(spell);
-		return c.visible() && c.click("Cast");
+		return c.visible() && (c.click("Cast") || c.click("Reanimate"));
 	}
 
 	/**
