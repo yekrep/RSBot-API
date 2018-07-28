46a5cf89caf24b6c3e180e7a399adb0ee454e68a
Chris B <sponsor@powerbot.org>
2018-07-29 00:01:46 +0100

    update inventory (verified bank and deposit box hasn't changed) (#1915)
    update inventory (verified bank and deposit box hasn't changed)

--- src/main/java/org/powerbot/script/rt6/Constants.java
+++ src/main/java/org/powerbot/script/rt6/Constants.java
@@ -195,10 +195,10 @@ public final class Constants {
 	public static final int PLAYER_INVENTORY_ITEMS = 31;
 
 	public static final int BACKPACK_WIDGET = 1473;
-	public static final int BACKPACK_SCROLLBAR = 4;
-	public static final int BACKPACK_VIEW = 5;
-	public static final int BACKPACK_CONTAINER = 5;
-	public static final int BACKPACK_BANK = 762 << 16 | 27;
+	public static final int BACKPACK_SCROLLBAR = 3;
+	public static final int BACKPACK_VIEW = 7;
+	public static final int BACKPACK_CONTAINER = 7;
+	public static final int BACKPACK_BANK = 762 << 16 | 28;
 	public static final int BACKPACK_DEPOSITBOX = 11 << 16 | 1;
 	public static final int BACKPACK_GEAR = 1474 << 16 | 13;
 	public static final int FAMILIAR_BACKPACK = FAMILIAR_INVENTORY_WIDGET << 16 | PLAYER_INVENTORY_ITEMS;
