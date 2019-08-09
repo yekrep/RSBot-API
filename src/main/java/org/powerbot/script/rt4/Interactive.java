00eb0bc8eb006391dc084df2575aa3ca5c35a533
Coma <coma@powerbot.org>
2019-08-09 11:45:40 -0400

    Revert "temp hotfix for continue chat widget issue"
    This reverts commit 9f7e9fce283de863e7a8ab8b7a1544eddf2c73c4.

--- src/main/java/org/powerbot/script/rt4/Interactive.java
+++ src/main/java/org/powerbot/script/rt4/Interactive.java
@@ -66,26 +66,17 @@ public abstract class Interactive extends ClientAccessor implements org.powerbot
 		});
 	}
 
-	public final boolean click(int offsetX, int offsetY, boolean left){
-        return valid() && ctx.input.apply(this, new Filter<Point>() {
-            @Override
-            public boolean accept(final Point point) {
-                if(offsetX>0 || offsetY>0) {//temp
-                    point.x += offsetX;
-                    point.y += offsetY;
-                    ctx.input.move(point);
-                }
-                return ctx.input.click(left);
-            }
-        });
-    }
-
 	/**
 	 * {@inheritDoc}
 	 */
 	@Override
 	public final boolean click() {
-		return click(0, 0, true);
+		return valid() && ctx.input.apply(this, new Filter<Point>() {
+			@Override
+			public boolean accept(final Point point) {
+				return ctx.input.click(true);
+			}
+		});
 	}
 
 	/**
@@ -93,7 +84,12 @@ public abstract class Interactive extends ClientAccessor implements org.powerbot
 	 */
 	@Override
 	public final boolean click(final boolean left) {
-		return click(0, 0, left);
+		return valid() && ctx.input.apply(this, new Filter<Point>() {
+			@Override
+			public boolean accept(final Point point) {
+				return ctx.input.click(left);
+			}
+		});
 	}
 
 	/**
