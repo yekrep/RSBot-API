ae009f6a925026acb6a738098c63bd8acf5a5d2e
Coma <coma@powerbot.org>
2019-08-09 09:23:54 -0400

    added viewport widget id for Bottom Line Tabs mode

--- src/main/java/org/powerbot/script/rt4/Constants.java
+++ src/main/java/org/powerbot/script/rt4/Constants.java
@@ -126,8 +126,9 @@ public final class Constants {
 	public static final int SKILLS_CONSTRUCTION = 22;
 
 	public static final int VIEWPORT_WIDGET = 548 << 16 | 12;
-	public static final int RESIZABLE_VIEWPORT_WIDGET = 164;
-	public static final int RESIZABLE_VIEWPORT_COMPONENT = 0;
+	public static final int RESIZABLE_VIEWPORT_WIDGET = 161;
+	public static final int RESIZABLE_VIEWPORT_BOTTOM_LINE_WIDGET = 164;
+	public static final int RESIZABLE_VIEWPORT_COMPONENT = 13;
 
 	@Deprecated
 	public static final int CHAT_NPC = 231;
