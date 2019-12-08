0216d26a3126ce4f57cb9d2ee7d6b70ebddfcffb
Coma <coma@powerbot.org>
2019-12-08 02:29:42 -0500

    added getPrevious

--- src/main/java/org/powerbot/bot/rt4/client/Node.java
+++ src/main/java/org/powerbot/bot/rt4/client/Node.java
@@ -1,10 +1,12 @@
 package org.powerbot.bot.rt4.client;
 
-import org.powerbot.bot.*;
+import org.powerbot.bot.ReflectProxy;
+import org.powerbot.bot.Reflector;
 
 public class Node extends ReflectProxy {
 	private static final Reflector.FieldCache a = new Reflector.FieldCache(),
-			b = new Reflector.FieldCache();
+			b = new Reflector.FieldCache(),
+			c = new Reflector.FieldCache();
 
 	public Node(final Reflector engine, final Object parent) {
 		super(engine, parent);
@@ -12,6 +14,10 @@ public class Node extends ReflectProxy {
 
 	public Node getNext() {
 		return new Node(reflector, reflector.access(this, a));
+	}
+
+	public Node getPrevious() {
+		return new Node(reflector, reflector.access(this, c));
 	}
 
 	public long getId() {
