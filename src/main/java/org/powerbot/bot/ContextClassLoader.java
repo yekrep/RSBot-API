dcea83569e781c803bba3f1e6eaa8f8200c7571e
A. Ahmed <paris@powerbot.org>
2019-12-08 08:41:20 +0000

    Use parent class loader and move in context

--- src/main/java/org/powerbot/bot/ContextClassLoader.java
+++ src/main/java/org/powerbot/bot/ContextClassLoader.java
@@ -5,6 +5,14 @@ import org.powerbot.script.*;
 @Script.Manifest(name = "RSBot", description = "powerbot.org")
 public abstract class ContextClassLoader extends ClassLoader {
 	public static final int VERSION = 7120;
+	private final ClientContext ctx;
 
-	public abstract ClientContext ctx();
+	public ContextClassLoader(final ClientContext ctx, final ClassLoader parent) {
+		super(parent);
+		this.ctx = ctx;
+	}
+
+	public ClientContext ctx() {
+		return ctx;
+	}
 }
